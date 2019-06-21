68d689752c7b91f271c99a583be389d138939423
Scott Morken <smorken@nrcan.gc.ca>
2019-06-21 16:30:57 -0700

    add is_simulated field to landclass

--- libcbm/configuration/cbm_defaults.py
+++ libcbm/configuration/cbm_defaults.py
@@ -64,7 +64,7 @@ queries = {
     """,
 
     "land_classes": """
-        select land_class.id, land_class.is_forest,
+        select land_class.id, land_class.is_forest, land_class.is_simulated
         land_class.transitional_period, land_class.transition_id
         from land_class;
     """,
