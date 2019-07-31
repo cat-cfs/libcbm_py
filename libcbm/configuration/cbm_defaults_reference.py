635ec64a974b7ec4d87f779331d4307097c06285
Scott Morken <smorken@nrcan.gc.ca>
2019-07-31 15:32:14 -0700

    renamed query file, refactoring, documenting pep8 for test_case_simulator.py

--- libcbm/configuration/cbm_defaults_reference.py
+++ libcbm/configuration/cbm_defaults_reference.py
@@ -8,6 +8,8 @@ import libcbm.configuration.cbm_defaults_queries as queries
 # queries for species name/species id associations
 species_reference_query = queries.get_query("species_name_ref.sql")
 
+disturbance_reference_query = queries.get_query("disturbance_type_ref.sql")
+
 # queries for spatial unit id, admin boundary name, eco boundary name
 # associations
 spatial_unit_reference_query = queries.get_query("spatial_units_name_ref.sql")
@@ -59,9 +61,56 @@ class CBMDefaultsReference:
 
     def __init__(self, sqlite_path, locale_code="en-CA"):
 
-        self.sqlite_path = sqlite_path
-        self.locale_code = locale_code
+        self.species_ref = load_data(
+            sqlite_path, species_reference_query, locale_code)
+        self.species_by_name = {x["name"]: x for x in self.species_ref}
 
-        self.species_ref = load_data(sqlite_path, species_reference_query, locale_code)
+        self.disturbance_type_ref = load_data(
+            sqlite_path, disturbance_reference_query, locale_code)
+        self.disturbance_type_by_name = {
+            x["name"]: x for x in self.disturbance_type_ref}
 
-    def get_species_id(species_name):
+        self.spatial_unit_ref =  load_data(
+            sqlite_path, spatial_unit_reference_query, locale_code)
+        self.spatial_unit_by_admin_eco_names = {
+            (x["admin_boundary_name"], x["eco_boundary_name"]): x
+            for x in self.spatial_unit_ref}
+
+
+
+    def get_species_id(self, species_name):
+        """Get the species id associated with the specified species name.
+
+        Arguments:
+            species_name {str} -- a species name
+
+        Returns:
+            int -- the id associated with the specified name
+        """
+        return self.species_by_name[species_name]["id"]
+
+    def get_disturbance_type_id(self, disturbance_type_name):
+        """Get the disturbance type id associated with the specified
+        disturbance type name
+
+        Arguments:
+            disturbance_type_name {str} -- a disturbance type name
+
+        Returns:
+            int -- disturbance type id
+        """
+        return self.disturbance_type_by_name[disturbance_type_name]["id"]
+
+    def get_spatial_unit_id(self, admin_boundary_name, eco_boundary_name):
+        """Get the spatial unit id associated with the specified
+        admin-boundary-name, eco-boundary-name combination
+
+        Arguments:
+            admin_boundary_name {str} -- an admin boundary name
+            eco_boundary_name {[type]} -- an eco boundary name
+
+        Returns:
+            int -- the spatial unit id
+        """
+        return self.spatial_unit_by_admin_eco_names[
+            (admin_boundary_name, eco_boundary_name)]
--- libcbm/test/test_case_simulator.py
+++ libcbm/test/test_case_simulator.py
@@ -72,7 +72,7 @@ def run_libCBM(dll_path, db_path, cases, n_steps, spinup_debug=False):
 
     pooldef = cbm_defaults.load_cbm_pools(db_path)
     flux_ind = cbm_defaults.load_flux_indicators(db_path)
-    flux_indicator_names = cbm_defaults.get_flux_indicator_names(db_path)
+
 
     dll.Initialize(libcbmconfig.to_string(
         {
@@ -87,13 +87,13 @@ def run_libCBM(dll_path, db_path, cases, n_steps, spinup_debug=False):
             for c in cases
         ])
     ])
-
-    species_reference = cbm_defaults.load_species_reference(db_path, "en-CA")
-    spatial_unit_reference = cbm_defaults.get_spatial_unit_ids_by_admin_eco_name(db_path, "en-CA")
-    disturbance_types_reference = cbm_defaults.get_disturbance_type_ids_by_name(db_path, "en-CA")
-    afforestation_pre_types = cbm_defaults.get_afforestation_types_by_name(db_path, "en-CA")
-    land_class_ref = cbm_defaults.get_land_class_reference(db_path, "en-CA")
-    land_classes_by_code = {x["land_class_code"]: x for x in land_class_ref}
+    # flux_indicator_names = cbm_defaults.get_flux_indicator_names(db_path)
+    # species_reference = cbm_defaults.load_species_reference(db_path, "en-CA")
+    # spatial_unit_reference = cbm_defaults.get_spatial_unit_ids_by_admin_eco_name(db_path, "en-CA")
+    # disturbance_types_reference = cbm_defaults.get_disturbance_type_ids_by_name(db_path, "en-CA")
+    # afforestation_pre_types = cbm_defaults.get_afforestation_types_by_name(db_path, "en-CA")
+    # land_class_ref = cbm_defaults.get_land_class_reference(db_path, "en-CA")
+    # land_classes_by_code = {x["land_class_code"]: x for x in land_class_ref}
 
     curves = []
     for c in cases:
@@ -101,7 +101,7 @@ def run_libCBM(dll_path, db_path, cases, n_steps, spinup_debug=False):
         merch_volumes = []
         for component in c["components"]:
             merch_volumes.append({
-                "species_id": species_reference[component["species"]]["species_id"],
+                "species_id": ref.get_species_id([component["species"]]),
                 "age_volume_pairs": component["age_volume_pairs"]
             })
 
@@ -121,26 +121,30 @@ def run_libCBM(dll_path, db_path, cases, n_steps, spinup_debug=False):
         "transitions": []
     }))
 
-    nstands = len(cases)
+    n_stands = len(cases)
 
     inventory_age = np.array([c["age"] for c in cases], dtype=np.int32)
 
     historic_disturbance_type = np.array(
-        [disturbance_types_reference[c["historic_disturbance"]]
-            for c in cases], dtype=np.int32)
+        [
+            ref.get_disturbance_type_id(c["historic_disturbance"])
+            for c in cases],
+        dtype=np.int32)
+
     last_pass_disturbance_type = np.array(
-        [disturbance_types_reference[c["last_pass_disturbance"]]
-            for c in cases], dtype=np.int32)
+        [ref.get_disturbance_type_id(c["last_pass_disturbance"]) for c in cases],
+        dtype=np.int32)
 
     delay = np.array([c["delay"] for c in cases], dtype=np.int32)
 
-    classifiers = np.zeros((nstands,1),dtype=np.int32)
+    classifiers = np.zeros((n_stands, 1), dtype=np.int32)
+
     classifiers[:,0]=[classifiers_config["classifier_index"][0] \
         [casegeneration.get_classifier_name(c["id"])] for c in cases]
 
     spatial_units = np.array(
-        [spatial_unit_reference[(c["admin_boundary"],c["eco_boundary"])]
-            for c in cases],dtype=np.int32)
+        [ref.get_spatial_unit_id(c["admin_boundary"], c["eco_boundary"])
+            for c in cases], dtype=np.int32)
 
     afforestation_pre_type_ids = []
     for c in cases:
@@ -152,22 +156,23 @@ def run_libCBM(dll_path, db_path, cases, n_steps, spinup_debug=False):
 
     afforestation_pre_type_id = np.array(afforestation_pre_type_ids, dtype=np.int32)
 
-    land_class = np.ones(nstands, dtype=np.int32)
-    land_class[afforestation_pre_type_id>0] = land_classes_by_code["UNFCCC_CL_R_CL"]["land_class_id"]
-    last_disturbance_type = np.zeros(nstands, dtype=np.int32)
-    time_since_last_disturbance = np.zeros(nstands, dtype=np.int32)
-    time_since_land_class_change = np.zeros(nstands, dtype=np.int32)
-    growth_enabled = np.zeros(nstands, dtype=np.int32)
+    land_class = np.ones(n_stands, dtype=np.int32)
+    land_class[afforestation_pre_type_id > 0] = \
+        land_classes_by_code["UNFCCC_CL_R_CL"]["land_class_id"]
+    last_disturbance_type = np.zeros(n_stands, dtype=np.int32)
+    time_since_last_disturbance = np.zeros(n_stands, dtype=np.int32)
+    time_since_land_class_change = np.zeros(n_stands, dtype=np.int32)
+    growth_enabled = np.zeros(n_stands, dtype=np.int32)
 
-    age = np.zeros(nstands, dtype=np.int32)
-    growth_multipliers = np.ones(nstands, dtype=np.float)
-    regeneration_delay = np.zeros(nstands, dtype=np.int32)
-    disturbance_types = np.zeros(nstands, dtype=np.int32)
-    transition_rules = np.zeros(nstands, dtype=np.int32)
+    age = np.zeros(n_stands, dtype=np.int32)
+    growth_multipliers = np.ones(n_stands, dtype=np.float)
+    regeneration_delay = np.zeros(n_stands, dtype=np.int32)
+    disturbance_types = np.zeros(n_stands, dtype=np.int32)
+    transition_rules = np.zeros(n_stands, dtype=np.int32)
 
-    pools = np.zeros((nstands,len(pooldef)))
-    flux = np.zeros((nstands, len(flux_ind)))
-    enabled = np.ones(nstands, dtype=np.int32)
+    pools = np.zeros((n_stands,len(pooldef)))
+    flux = np.zeros((n_stands, len(flux_ind)))
+    enabled = np.ones(n_stands, dtype=np.int32)
 
     disturbances = {}
     for i_c, c in enumerate(cases):
@@ -214,9 +219,9 @@ def run_libCBM(dll_path, db_path, cases, n_steps, spinup_debug=False):
         land_class=land_class,
         age=age)
 
-    pool_result = append_pools_data(pool_result, nstands, 0, pools, pooldef)
+    pool_result = append_pools_data(pool_result, n_stands, 0, pools, pooldef)
     state_variable_result = pd.DataFrame(data={
-        "identifier": [casegeneration.get_classifier_name(x) for x in range(1, nstands+1)],
+        "identifier": [casegeneration.get_classifier_name(x) for x in range(1, n_stands+1)],
         "timestep": 0,
         "age": age,
         "land_class": land_class,
@@ -253,10 +258,10 @@ def run_libCBM(dll_path, db_path, cases, n_steps, spinup_debug=False):
             land_class=land_class,
             growth_multiplier=growth_multipliers,
             regeneration_delay=regeneration_delay)
-        pool_result = append_pools_data(pool_result, nstands, t, pools, pooldef)
-        flux_result = append_flux_data(flux_result, nstands, t, flux, flux_indicator_names)
+        pool_result = append_pools_data(pool_result, n_stands, t, pools, pooldef)
+        flux_result = append_flux_data(flux_result, n_stands, t, flux, flux_indicator_names)
         state_variable_result = state_variable_result.append(pd.DataFrame(data = {
-            "identifier": [casegeneration.get_classifier_name(x) for x in range(1,nstands+1)],
+            "identifier": [casegeneration.get_classifier_name(x) for x in range(1,n_stands+1)],
             "timestep": t,
             "age": age,
             "land_class": land_class,
