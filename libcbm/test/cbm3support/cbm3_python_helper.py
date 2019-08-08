ca65387dada96d29f7ca67163b0d04ee4d664536
Scott Morken <smorken@nrcan.gc.ca>
2019-08-08 11:00:07 -0700

    code docs, removed one unused parameter from import_cbm3_project function

--- libcbm/test/cbm3support/cbm3_python_helper.py
+++ libcbm/test/cbm3support/cbm3_python_helper.py
@@ -3,20 +3,24 @@
 
 import git
 import os,sys
+
 def load_cbm3_python(branch="master"):
     '''
-    Clone the cbm3_python repository from Github if it does not exist locally already.  
-    If it does already exist locally do a git pull to get the latest revisions
+    Clone the cbm3_python repository from Github if it does not exist locally
+    already.  If it does already exist locally do a git pull to get the latest
+    revisions
     '''
-    cbm3_python_dir = os.path.abspath(os.path.join(".","cbm3_python_git"))
+    cbm3_python_dir = os.path.abspath(os.path.join(".", "cbm3_python_git"))
     if not os.path.exists(cbm3_python_dir):
-        git.Repo.clone_from('https://github.com/cat-cfs/cbm3_python.git', cbm3_python_dir, branch=branch)
+        git.Repo.clone_from(
+            'https://github.com/cat-cfs/cbm3_python.git', cbm3_python_dir,
+            branch=branch)
     else:
         g = git.cmd.Git(cbm3_python_dir)
         g.pull()
         if branch:
             g.checkout(branch)
-    
+
     if not cbm3_python_dir in sys.path:
         sys.path.append(os.path.abspath(cbm3_python_dir))
 
