1ad8a084b7228f26e7809c6095a06f3819777936
Scott Morken <smorken@nrcan.gc.ca>
2019-08-09 10:26:04 -0700

    removed some code duplication

--- libcbm/wrapper/libcbm_matrix.py
+++ libcbm/wrapper/libcbm_matrix.py
@@ -2,23 +2,19 @@ import ctypes
 import numpy as np
 
 
-class LibCBM_Matrix(ctypes.Structure):
-    """Wrapper for low level C/C++ LibCBM structure of the same name.
-    Used to pass a 2 dimensional numpy matrix or single valued array as raw
-    float64 memory to LibCBM.
+class LibCBM_Matrix_Base(ctypes.Structure):
+    """Wrapper for low level C/C++ LibCBM structures LibCBM_Matrix and
+    LibCBM_Matrix_Int. Used to pass a 2 dimensional numpy matrix or single
+    valued array as raw memory to LibCBM.
 
     Args:
-    matrix (numpy.ndarray): a 2 dimensional numpy array, or single value
+        matrix (numpy.ndarray): a 2 dimensional numpy array, or single value
 
     Raises:
         ValueError: matrix must have either 2 dimensions or be a scalar
-        ValueError: matrix must be C_CONTIGUOUS and of type np.double
+        ValueError: matrix must be C_CONTIGUOUS and of the correct type
     """
-    _fields_ = [('rows', ctypes.c_ssize_t),
-                ('cols', ctypes.c_ssize_t),
-                ('values', ctypes.POINTER(ctypes.c_double))]
-
-    def __init__(self, matrix):
+    def __init__(self, matrix, matrix_np_type, matrix_c_type):
         if len(matrix.shape) == 1 and matrix.shape[0] == 1:
             self.rows = 1
             self.cols = 1
@@ -30,40 +26,47 @@ class LibCBM_Matrix(ctypes.Structure):
                 "matrix must have either 2 dimensions or be a single cell "
                 "matrix")
         if not matrix.flags["C_CONTIGUOUS"] or \
-           not matrix.dtype == np.double:
+           not matrix.dtype == matrix_np_type:
             raise ValueError(
                 "matrix must be c contiguous and of type np.double")
-        self.values = matrix.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        self.values = matrix.ctypes.data_as(ctypes.POINTER(matrix_c_type))
 
 
-class LibCBM_Matrix_Int(ctypes.Structure):
-    """Wrapper for low level C/C++ LibCBM structure of the same name.
-    Used to pass a 2 dimensional numpy matrix or single valued array as raw
-    int32 memory to LibCBM.
+class LibCBM_Matrix(LibCBM_Matrix_Base):
+    """64 bit float specialization of
+    :py:class:`libcbm.wrapper.libcbm_matrix.LibCBM_Matrix_Base`
 
     Args:
-    matrix (numpy.ndarray): a 2 dimensional numpy array, or single value
-
-    Raises:
-        ValueError: matrix must have either 2 dimensions or be a scalar
-        ValueError: matrix must be C_CONTIGUOUS and of type np.int32
+        matrix (numpy.ndarray): a 2 dimensional numpy array, or single value
     """
+    _matrix_c_type = ctypes.c_double
+    _matrix_np_type = np.double
+
     _fields_ = [('rows', ctypes.c_ssize_t),
                 ('cols', ctypes.c_ssize_t),
-                ('values', ctypes.POINTER(ctypes.c_int))]
+                ('values', ctypes.POINTER(_matrix_c_type))]
 
     def __init__(self, matrix):
-        if len(matrix.shape) == 1 and matrix.shape[0] == 1:
-            self.rows = 1
-            self.cols = 1
-        elif len(matrix.shape) == 2:
-            self.rows = matrix.shape[0]
-            self.cols = matrix.shape[1]
-        else:
-            raise ValueError(
-                "matrix must have either 2 dimensions or be a single cell "
-                "matrix")
-        if not matrix.flags["C_CONTIGUOUS"] or not matrix.dtype == np.int32:
-            raise ValueError(
-                "matrix must be c contiguous and of type np.int32")
-        self.values = matrix.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
+        LibCBM_Matrix_Base.__init__(
+            self, matrix, LibCBM_Matrix._matrix_np_type,
+            LibCBM_Matrix._matrix_c_type)
+
+
+class LibCBM_Matrix_Int(LibCBM_Matrix_Base):
+    """32 bit int specialization of
+    :py:class:`libcbm.wrapper.libcbm_matrix.LibCBM_Matrix_Base`
+
+    Args:
+        matrix (numpy.ndarray): a 2 dimensional numpy array, or single value
+    """
+    _matrix_c_type = ctypes.c_int
+    _matrix_np_type = np.int32
+
+    _fields_ = [('rows', ctypes.c_ssize_t),
+                ('cols', ctypes.c_ssize_t),
+                ('values', ctypes.POINTER(_matrix_c_type))]
+
+    def __init__(self, matrix):
+        LibCBM_Matrix_Base.__init__(
+            self, matrix, LibCBM_Matrix_Int._matrix_np_type,
+            LibCBM_Matrix_Int._matrix_c_type)
