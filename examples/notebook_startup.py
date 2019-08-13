0bb9c6947b43d79e82c18769e5deed2bf0105a90
Scott Morken <smorken@nrcan.gc.ca>
2019-08-13 10:59:42 -0700

    docs

--- examples/notebook_startup.py
+++ examples/notebook_startup.py
@@ -4,11 +4,28 @@ import json
 
 # This is required so that the notebooks can work with the libcbm package
 # directory structure.
-sys.path.insert(0, os.path.abspath('..'))
+package_dir = os.path.abspath('..')
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
 
 
 def load_settings():
+    """Load or create a settings file which is a json dictionary with the
+    following string variables:
 
+        - cbm3_exe_path: directory containing the CBM-CFS3
+                         model executables: cbm.exe and makelist.exe
+        - toolbox_path: directory of the CBM-CFS3 application installation
+        - archive_index_db_path: path to a CBM-CFS3 archive index database
+        - cbm_defaults_db_path: path to a cbm_defaults database. See:
+                                https://github.com/cat-cfs/cbm_defaults
+        - libcbm_path: path to a compiled libcbm dll/so file.
+
+    If the file does not already exist an empty one is created.
+
+    Returns:
+        dict: a dictionary with settings
+    """
     local_dir = os.path.dirname(os.path.realpath(__file__))
     local_settings_path = os.path.join(
         local_dir, "notebook_settings.json")
