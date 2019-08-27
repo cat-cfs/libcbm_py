354a3831bb8e54134ab9e48813802f290466e0a6
Scott Morken <smorken@nrcan.gc.ca>
2019-08-27 11:32:59 -0700

    development and code docs for SIT format

--- libcbm/input/sit/sit_format.py
+++ libcbm/input/sit/sit_format.py
@@ -4,16 +4,34 @@ import numpy as np
 classifier_keyword = "_CLASSIFIER"
 
 
-def get_classifier_format():
-    return [
+def get_classifier_format(n_columns):
+    """Gets a list of dictionaries describing the CBM SIT classifier columns
+
+    Returns:
+        list: a list of dictionaries that describe the CBM SIT classifier
+            columns
+    """
+    classifier_format = [
         {"name": "id", "index": 0},
         {"name": "name", "index": 1},
         {"name": "description", "index": 2},
-        {"name": "classifier_aggregates", "column_range": [3, -1]}
+        # {"name": "classifier_aggregates", "column_range": [3, -1]}
     ]
+
+    if n_columns < 3:
+        raise ValueError(
+            "specified number of columns invalid.  Expected at least 3.")
+    return classifier_format
 
 
 def get_disturbance_type_format():
+    """Gets a list of dictionaries describing the CBM SIT disturbance type
+    columns
+
+    Returns:
+        list: a list of dictionaries that describe the CBM SIT disturbance
+            type columns
+    """
     return [
         {"name": "id", "index": 0},
         {"name": "name", "index": 1},
@@ -22,20 +40,32 @@ def get_disturbance_type_format():
 
 
 def get_age_class_format():
+    """Gets a list of dictionaries describing the CBM SIT age class columns
+
+    Returns:
+        list: a list of dictionaries that describe the  CBM SIT age class
+            columns
+    """
     return [
         {"name": "id", "index": 0},
         {"name": "size", "index": 1, "type": np.int},
     ]
 
 
-def get_yield_format(classifier_names):
+def get_yield_format(classifier_names, n_columns):
+    """Gets a list of dictionaries describing the CBM SIT age class columns
+
+    Returns:
+        list: a list of dictionaries that describe the  CBM SIT age class
+            columns
+    """
     n_classifiers = len(classifier_names)
     classifier_values = [
         {"name": c, "index": i} for i, c in enumerate(classifier_names)]
     leading_species_col = [{
         "name": "leading_species", "index": n_classifiers}]
     volumes = [{
-        "name": "volumes", "column_range": [n_classifiers+1, -1],
+        "name": "volumes", "column_range": [n_classifiers + 1, -1],
         "min_value": 0, "type": np.float}]
     return classifier_values + leading_species_col + volumes
 
@@ -53,8 +83,7 @@ def get_transition_rules_format(classifier_names, n_columns):
         in the data.
 
     Raises:
-        ValueError: n_columns did not match the possible number of columns
-            in the SIT transitions format.
+        ValueError: n_columns was not valid for the sit transitions format
 
     Returns:
         list: a list of dictionaries describing the SIT transition rule columns
@@ -92,13 +121,19 @@ def get_transition_rules_format(classifier_names, n_columns):
     result.extend(disturbance_type)
     result.extend(classifier_set)  # destination classifier set
     result.extend(post_transition)
+    if n_columns < len(result):
+        raise ValueError(
+            "specified number of columns invalid.  Expected at least "
+            "{}".format(len(result)))
     if n_columns == len(result):
         return result
     elif n_columns == len(result) + 1:
         result.extend(spatial_reference)
         return result
     else:
-        raise ValueError("incorrect number of columns in transition rules")
+        raise ValueError(
+            "incorrect number of columns in transition rules. "
+            "Expected at most {}".format(len(result) + 1))
 
 
 def get_inventory_format(classifier_names, n_columns):
@@ -166,12 +201,37 @@ def get_disturbance_event_format(classifier_names, n_columns):
         {"name": "MaxSWStemSnagC", "index": index + 12, "type": np.float},
         {"name": "MinHWStemSnagC", "index": index + 13, "type": np.float},
         {"name": "MaxHWStemSnagC", "index": index + 14, "type": np.float},
-        {"name": "MinTotalStemSnagMerchC", "index": index + 15, "type": np.float},
-        {"name": "MaxTotalStemSnagMerchC", "index": index + 16, "type": np.float},
+        {"name": "MinTotalStemSnagMerchC", "index": index + 15,
+            "type": np.float},
+        {"name": "MaxTotalStemSnagMerchC", "index": index + 16,
+            "type": np.float},
         {"name": "MinSWMerchStemSnagC", "index": index + 17, "type": np.float},
         {"name": "MaxSWMerchStemSnagC", "index": index + 18, "type": np.float},
         {"name": "MinHWMerchStemSnagC", "index": index + 19, "type": np.float},
         {"name": "MaxHWMerchStemSnagC", "index": index + 20, "type": np.float}
     ]
-
-    event_target = []
+    n_eligibility_fields = len(disturbance_eligibility)
+    index = n_classifiers + n_age_fields + n_eligibility_fields
+    event_target = [
+        {"name": "efficiency", "index": index, "type": np.float,
+            "min_value": 0, "max_value": 1},
+        {"name": "sort_type", "index": index + 1},
+        {"name": "target_type", "index": index + 2},
+        {"name": "target", "index": index + 3, "type": np.float},
+        {"name": "disturbance_type", "index": index + 4},
+        {"name": "disturbance_year", "index": index + 5, "type": np.int},
+    ]
+    if n_columns < index + 6:
+        raise ValueError(
+            "specified number of columns invalid.  Expected at least "
+            "{}".format(len(index + 6)))
+    if n_columns == index + 7:
+        event_target.append(
+            {"name": "spatial", "index": index + 6}
+        )
+    if n_columns > index + 7:
+        raise ValueError(
+            "specified number of columns invalid.  Expected at most "
+            "{}".format(len(index + 7)))
+    return classifier_set + disturbance_age_eligibility + \
+        disturbance_eligibility + event_target
