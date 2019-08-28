a70bc1fe550fef85dd26ab9f9b704a2b23c12e81
Scott Morken <smorken@nrcan.gc.ca>
2019-08-28 15:34:02 -0700

    layout the rest of the parser module/test files

---
+++ libcbm/test/unit/input/sit/sit_disturbance_type_parser_test.py
@@ -0,0 +1,6 @@
+import unittest
+from libcbm.input.sit import sit_disturbance_type_parser
+
+
+class SITDisturbanceTypeParserTest(unittest.TestCase):
+    pass
---
+++ libcbm/test/unit/input/sit/sit_inventory_parser_test.py
@@ -0,0 +1,6 @@
+import unittest
+from libcbm.input.sit import sit_inventory_parser
+
+
+class SITInventoryParserTest(unittest.TestCase):
+    pass
