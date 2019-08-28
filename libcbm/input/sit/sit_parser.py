a4220b1b41609dc701dbe5e00144d04a5a8220a5
Scott Morken <smorken@nrcan.gc.ca>
2019-08-28 14:41:16 -0700

    add helper functions to parse integer and boolean from irregular string or integer input

--- libcbm/input/sit/sit_parser.py
+++ libcbm/input/sit/sit_parser.py
@@ -32,3 +32,42 @@ def unpack_table(table, column_descriptions, table_name):
     return pd.DataFrame(columns=cols, data=data)
 
 
+def try_get_int(s):
+    """
+    Checks if the specified value is an integer, and returns a result
+
+    Args:
+        s (any): a value to test
+
+    Returns:
+        tuple: (int(s), True) if the value can be converted to an integer,
+            and otherwise (None, False)
+    """
+    try:
+        i = int(s)
+        return (i, True)
+    except ValueError:
+        return (None, False)
+
+
+def get_parse_bool_func(table_name, colname):
+    def parse_bool(x):
+        if isinstance(x, bool):
+            return x
+        elif isinstance(x, int):
+            return x != 0
+        else:
+            str_x = str(x).lower()
+            int_x, success = try_get_int(str_x)
+            if(success):
+                return int_x > 0
+            if str_x in ["true", "t", "y"]:
+                return True
+            elif str_x in ["false", "f", "n"]:
+                return False
+            else:
+                raise ValueError(
+                    f"{table_name}: cannot parse value: '{x}' in "
+                    f"column: '{colname}' as a boolean")
+
+    return parse_bool
