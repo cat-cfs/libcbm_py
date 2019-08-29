d6be0648975826eb47b42fece7fe912edcc1d08b
Scott Morken <smorken@nrcan.gc.ca>
2019-08-29 10:18:51 -0700

    age class parser tests

--- libcbm/test/unit/input/sit/sit_age_class_parser_test.py
+++ libcbm/test/unit/input/sit/sit_age_class_parser_test.py
@@ -1,27 +1,36 @@
 import unittest
+import pandas as pd
 from libcbm.input.sit import sit_age_class_parser
 
 
 class SITAgeClassParserTest(unittest.TestCase):
 
-    def non_zero_initial_size_error(self):
+    def test_non_zero_initial_size_error(self):
         """Check that a non zero size for the first age class row results in
         error
         """
-        self.assertTrue(False)
+        with self.assertRaises(ValueError):
+            sit_age_class_parser.parse_age_classes(
+                pd.DataFrame([(0, 1), (1, 1)]))
 
-    def zero_size_error(self):
+    def test_zero_size_error(self):
         """Check that a zero size for any but the first age class row results
         in error
         """
-        self.assertTrue(False)
+        with self.assertRaises(ValueError):
+            sit_age_class_parser.parse_age_classes(
+                pd.DataFrame([(0, 0), (1, 0)]))
 
-    def non_numeric_size_error(self):
+    def test_non_numeric_size_error(self):
         """Check that a non numeric value for the size column results in error
         """
-        self.assertTrue(False)
+        with self.assertRaises(ValueError):
+            sit_age_class_parser.parse_age_classes(
+                pd.DataFrame([(0, 0), (1, "a")]))
 
-    def duplicate_id_error(self):
+    def test_duplicate_id_error(self):
         """Check that a duplicate value for the id column results in error
         """
-        self.assertTrue(False)
+        with self.assertRaises(ValueError):
+            sit_age_class_parser.parse_age_classes(
+                pd.DataFrame([(0, 0), (1, 1), (1, 1)]))
