a78e4e694b823574735de2fc6398ba34733feef7
Scott Morken <smorken@nrcan.gc.ca>
2019-08-29 11:31:13 -0700

    classifier parser tests

--- libcbm/test/unit/input/sit/sit_classifier_parser_test.py
+++ libcbm/test/unit/input/sit/sit_classifier_parser_test.py
@@ -1,4 +1,6 @@
 import unittest
+import numpy as np
+import pandas as pd
 from libcbm.input.sit import sit_classifier_parser
 
 
@@ -8,21 +10,146 @@ class SITClassifierParserTest(unittest.TestCase):
         """checks that an error is raised when any 2 classifiers have the
         same name
         """
-        self.assertTrue(False)
+        sit_classifiers_table = pd.DataFrame(
+            data=[
+                ("1", "_CLASSIFIER", "SAME_NAME", np.nan, np.nan),
+                (1, "a", "a", np.nan, np.nan),
+                (1, "b", "b", np.nan, np.nan),
+                (1, "agg1", "agg2", "a", "b"),
+                (1, "agg2", "agg2", "a", "b"),
+                (2, "_CLASSIFIER", "SAME_NAME", np.nan, np.nan),
+                (2, "a", "a", np.nan, np.nan),
+                (2, "agg1", "agg1", "a", np.nan)])
+
+        with self.assertRaises(ValueError):
+            sit_classifier_parser.parse_classifiers(sit_classifiers_table)
 
     def test_duplicate_classifier_value_name_value_error(self):
         """checks that an error is raise if any 2 classifier value names are
         identical for a given classifier
         """
-        self.assertTrue(False)
+        sit_classifiers_table = pd.DataFrame(
+            data=[
+                ("1", "_CLASSIFIER", "classifier1", np.nan, np.nan),
+                (1, "SAME_NAME", "a", np.nan, np.nan),
+                (1, "SAME_NAME", "b", np.nan, np.nan),
+                (1, "agg1", "agg2", "a", "b"),
+                (1, "agg2", "agg2", "a", "b"),
+                (2, "_CLASSIFIER", "classifier2", np.nan, np.nan),
+                (2, "a", "a", np.nan, np.nan),
+                (2, "agg1", "agg1", "a", np.nan)])
+        with self.assertRaises(ValueError):
+            sit_classifier_parser.parse_classifiers(sit_classifiers_table)
 
     def test_classifier_aggregate_validation_errors(self):
         """checks that the function validates classifier aggregates
         """
-        self.assertTrue(False)
+
+        with self.assertRaises(ValueError):
+            # no two aggregates can have the same name
+            sit_classifier_parser.parse_classifiers(
+                pd.DataFrame(data=[
+                    ("1", "_CLASSIFIER", "classifier1", np.nan, np.nan),
+                    (1, "a", "a", np.nan, np.nan),
+                    (1, "b", "b", np.nan, np.nan),
+                    (1, "SAME_NAME", "agg2", "a", "b"),
+                    (1, "SAME_NAME", "agg2", "a", "b"),
+                    (2, "_CLASSIFIER", "classifier2", np.nan, np.nan),
+                    (2, "a", "a", np.nan, np.nan),
+                    (2, "agg1", "agg1", "a", np.nan)]))
+
+        with self.assertRaises(ValueError):
+            # error when a value in the aggregate is not a defined classifier
+            # value name for the classifier
+            sit_classifier_parser.parse_classifiers(
+                pd.DataFrame(data=[
+                    ("1", "_CLASSIFIER", "classifier1", np.nan, np.nan),
+                    (1, "a", "a", np.nan, np.nan),
+                    (1, "b", "b", np.nan, np.nan),
+                    (1, "agg1", "agg2", "MISSING", "b"),
+                    (1, "agg2", "agg2", "a", "b"),
+                    (2, "_CLASSIFIER", "classifier2", np.nan, np.nan),
+                    (2, "a", "a", np.nan, np.nan),
+                    (2, "agg1", "agg1", "a", np.nan)]))
+
+        with self.assertRaises(ValueError):
+            # error when a value in the aggregate is duplicated
+            sit_classifier_parser.parse_classifiers(
+                pd.DataFrame(data=[
+                    ("1", "_CLASSIFIER", "classifier1", np.nan, np.nan),
+                    (1, "DUPLICATE", "a", np.nan, np.nan),
+                    (1, "b", "b", np.nan, np.nan),
+                    (1, "agg1", "agg2", "DUPLICATE", "DUPLICATE"),
+                    (1, "agg2", "agg2", "a", "b"),
+                    (2, "_CLASSIFIER", "classifier2", np.nan, np.nan),
+                    (2, "a", "a", np.nan, np.nan),
+                    (2, "agg1", "agg1", "a", np.nan)]))
 
     def test_multiple_classifier_per_id_column_error(self):
         """checks if an error is raise with multiple classifiers defined for a
         single id
         """
-        self.assertTrue(False)
+
+        # in the following data (1, _CLASSIFIER) appears on 2 different rows
+        sit_classifiers_table = pd.DataFrame(
+            data=[
+                ("1", "_CLASSIFIER", "classifier1", np.nan, np.nan),
+                (1, "_CLASSIFIER", "a", np.nan, np.nan),
+                (1, "b", "b", np.nan, np.nan),
+                (1, "agg1", "agg2", "a", "b"),
+                (1, "agg2", "agg2", "a", "b"),
+                (2, "_CLASSIFIER", "classifier2", np.nan, np.nan),
+                (2, "a", "a", np.nan, np.nan),
+                (2, "agg1", "agg1", "a", np.nan)])
+
+        with self.assertRaises(ValueError):
+            sit_classifier_parser.parse_classifiers(sit_classifiers_table)
+
+    def test_expected_result(self):
+        """checks if an error is raise with multiple classifiers defined for a
+        single id
+        """
+
+        # in the following data (1, _CLASSIFIER) appears on 2 different rows
+        sit_classifiers_table = pd.DataFrame(
+            data=[
+                ("1", "_CLASSIFIER", "classifier1", np.nan, np.nan),
+                (1, "a", "a", np.nan, np.nan),
+                (1, "b", "b", np.nan, np.nan),
+                (1, "agg1", "agg1", "a", "b"),
+                (1, "agg2", "agg2", "a", "b"),
+                (2, "_CLASSIFIER", "classifier2", np.nan, np.nan),
+                (2, "a", "a", np.nan, np.nan),
+                (2, "agg1", "agg1", "a", np.nan)])
+
+        classifiers, classifier_values, classifier_aggregates = \
+            sit_classifier_parser.parse_classifiers(sit_classifiers_table)
+
+        self.assertTrue(list(classifiers.id) == [1, 2])
+        self.assertTrue(list(classifiers.name) == ["classifier1", "classifier2"])
+
+        self.assertTrue(list(classifier_values.classifier_id) == [1, 1, 2])
+        self.assertTrue(list(classifier_values.name) == ["a", "b", "a"])
+        self.assertTrue(list(classifier_values.description) == ["a", "b", "a"])
+
+        self.assertTrue(len(classifier_aggregates) == 3)
+        self.assertTrue(
+            classifier_aggregates[0] == {
+                'classifier_id': 1,
+                'name': 'agg1',
+                'description': 'agg1',
+                'classifier_values': ['a', 'b']})
+
+        self.assertTrue(
+            classifier_aggregates[1] == {
+                'classifier_id': 1,
+                'name': 'agg2',
+                'description': 'agg2',
+                'classifier_values': ['a', 'b']})
+
+        self.assertTrue(
+            classifier_aggregates[2] == {
+                'classifier_id': 2,
+                'name': 'agg1',
+                'description': 'agg1',
+                'classifier_values': ['a']})
