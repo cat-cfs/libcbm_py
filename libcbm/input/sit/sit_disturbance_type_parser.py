0b51098aff8e4835ce4a8b16baecd509847c0c47
Scott Morken <smorken@nrcan.gc.ca>
2019-08-29 12:19:43 -0700

    disturbance_type_parser and tests

--- libcbm/input/sit/sit_disturbance_type_parser.py
+++ libcbm/input/sit/sit_disturbance_type_parser.py
@@ -4,8 +4,15 @@ from libcbm.input.sit import sit_parser
 
 
 def parse_disturbance_types(disturbance_types_table):
-    return sit_parser.unpack_table(
+    result = sit_parser.unpack_table(
         disturbance_types_table,
         sit_format.get_disturbance_type_format(
             len(disturbance_types_table.columns)),
         "disturbance types")
+
+    duplicates = result.groupby("id").size()
+    duplicates = list(duplicates[duplicates > 1].index)
+    if len(duplicates) > 0:
+        raise ValueError(
+            f"duplicate ids detected in disturbance types {duplicates}")
+    return result
--- libcbm/test/unit/input/sit/sit_disturbance_type_parser_test.py
+++ libcbm/test/unit/input/sit/sit_disturbance_type_parser_test.py
@@ -1,4 +1,5 @@
 import unittest
+import pandas as pd
 from libcbm.input.sit import sit_disturbance_type_parser
 
 
@@ -6,4 +7,25 @@ class SITDisturbanceTypeParserTest(unittest.TestCase):
 
     def test_check_expected_result(self):
         """check a valid parsed result"""
-        self.assertTrue(False)
+        result = sit_disturbance_type_parser.parse_disturbance_types(
+            pd.DataFrame(data=[
+                ("1", "fire"),
+                (2, "clearcut"),
+                ("3", "clearcut")
+            ]))
+
+        self.assertTrue(list(result.id) == ["1", "2", "3"])
+        self.assertTrue(list(result.name) == ["fire", "clearcut", "clearcut"])
+
+    def test_duplicate_id_error(self):
+        """check if an error is raise on duplicate ids
+        """
+        with self.assertRaises(ValueError):
+            sit_disturbance_type_parser.parse_disturbance_types(
+                pd.DataFrame(data=[
+                    ("1", "fire"),
+                    (1, "clearcut"),
+                    ("3", "clearcut")
+                ]))
+
+
