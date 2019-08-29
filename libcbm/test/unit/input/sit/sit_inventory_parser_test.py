9354bf6656819efe71346767511a5645d92b1986
Scott Morken <smorken@nrcan.gc.ca>
2019-08-29 12:19:59 -0700

    inventory parser tests

--- libcbm/test/unit/input/sit/sit_inventory_parser_test.py
+++ libcbm/test/unit/input/sit/sit_inventory_parser_test.py
@@ -1,14 +1,78 @@
 import unittest
+import pandas as pd
 from libcbm.input.sit import sit_inventory_parser
 
 
 class SITInventoryParserTest(unittest.TestCase):
 
-    def test_expected_result_with_using_age_class(self):
+    def get_mock_classifiers(self):
+        classifiers = pd.DataFrame(
+            data=[
+                (1, "classifier1"),
+                (2, "classifier2")
+            ],
+            columns=["id", "name"]
+        )
+        classifier_values = pd.DataFrame(
+            data=[
+                (1, "a", "a"),
+                (1, "b", "b"),
+                (2, "a", "a"),
+            ],
+            columns=["classifier_id", "name", "description"]
+        )
+        return classifiers, classifier_values
+
+    def get_mock_age_classes(self):
+        return pd.DataFrame(
+            data=[
+                ("0", 0, 0, 0),
+                ("1", 2, 1, 2)],
+            columns=["name", "class_size", "start_year", "end_year"]
+        )
+
+    def get_mock_disturbance_types(self):
+        return pd.DataFrame(
+            data=[
+                ("dist1", "fire"),
+                ("dist2", "clearcut")
+            ],
+            columns=["id", "name"]
+        )
+
+    def test_expected_result_with_using_non_zero_age_class(self):
         """Checks the age class expansion feature "using_age_class"
-        (including the 0th age class)
         """
-        self.assertTrue(False)
+        inventory_table = pd.DataFrame(
+            data=[
+                ("b", "a", "TRUE", "1", 1, 0, 0),
+                ("a", "a", False, 100, 1, 0, 0),
+                ("a", "a", "-1", 4, 1, 0, 0)])
+
+        classifiers, classifier_values = self.get_mock_classifiers()
+        age_classes = self.get_mock_age_classes()
+        result = sit_inventory_parser.parse_inventory(
+            inventory_table, classifiers, classifier_values, None, age_classes)
+        self.assertTrue(result.shape[0] == len(inventory_table) + 1)
+        self.assertTrue(result.area.sum() == 3)
+        self.assertTrue(set(result.age) == {100, 4, 1, 2})
+
+    def test_expected_result_with_using_zeroth_age_class(self):
+        """Checks the age class expansion feature "using_age_class"
+        """
+        inventory_table = pd.DataFrame(
+            data=[
+                ("b", "a", "TRUE", "0", 1, 0, 0),
+                ("a", "a", False, 100, 1, 0, 0),
+                ("a", "a", "-1", 4, 1, 0, 0)])
+
+        classifiers, classifier_values = self.get_mock_classifiers()
+        age_classes = self.get_mock_age_classes()
+        result = sit_inventory_parser.parse_inventory(
+            inventory_table, classifiers, classifier_values, None, age_classes)
+        self.assertTrue(result.shape[0] == len(inventory_table))
+        self.assertTrue(result.area.sum() == 3)
+        self.assertTrue(set(result.age) == {100, 4, 0})
 
     def test_non_numeric_or_negative_values_raise_exception(self):
         """checks that an error is raised when a negative or non-numeric
@@ -18,7 +82,23 @@ class SITInventoryParserTest(unittest.TestCase):
          - delay
          - spatial reference (negatives allowed here)
         """
-        self.assertTrue(False)
+
+        classifiers, classifier_values = self.get_mock_classifiers()
+        disturbance_types = self.get_mock_disturbance_types()
+        for data in [
+                ("b", "a", "F", "x", 1, 0, 0),
+                ("b", "a", "F", -1, 1, 0, 0),
+                ("b", "a", "F", 0, "x", 0, 0),
+                ("b", "a", "F", 0, -1, 0, 0),
+                ("b", "a", "F", 0, 0, "x", 0),
+                ("b", "a", "F", 0, 0, -1, 0),
+                ("b", "a", "F", 0, 0, -1, 0, "dist1", "dist1", "x")]:
+
+            inventory_table = pd.DataFrame([data])
+            with self.assertRaises(ValueError):
+                sit_inventory_parser.parse_inventory(
+                    inventory_table, classifiers, classifier_values, disturbance_types, None)
+
 
     def test_missing_disturbance_type_id_raises_exception(self):
         """checks that an error is raised if a disturbance type id is
