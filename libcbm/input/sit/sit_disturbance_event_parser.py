cdb2d44401ceccb3af1cb69f6f932ebdc9553bf8
Scott Morken <smorken@nrcan.gc.ca>
2019-08-30 15:18:30 -0700

    method stubs for disturbance event parser

--- libcbm/input/sit/sit_disturbance_event_parser.py
+++ libcbm/input/sit/sit_disturbance_event_parser.py
@@ -11,9 +11,23 @@ def parse(disturbance_events, classifiers, classifier_values,
     unpacked_table = sit_parser.unpack_table(
         disturbance_events, disturbance_event_format, "disturbance events")
 
+    # check that the correct number of classifiers are present
 
+    # check that each value in disturbance events classifier sets is defined in
+    # classifier values, classifier aggregates or is a wildcard
+
+    # if age classes are used substitute the age critera based on the age
+    # class id, and raise an error if the id is not defined, and drop
+    # using_age_class from output
+
+    # check that all age criteria are identical between SW and HW (since CBM
+    # has only a stand age)
+
+    # translate criteria into a more useful format
+
+    # validate sort type
+
+    # validate target type
+
+    # validate disturbance type according to specified disturbance types
     return unpacked_table
-
-
-
-
--- libcbm/test/unit/input/sit/sit_disturbance_event_parser_test.py
+++ libcbm/test/unit/input/sit/sit_disturbance_event_parser_test.py
@@ -4,4 +4,58 @@ from libcbm.input.sit import sit_disturbance_event_parser
 
 class SITDisturbanceEventParserTest(unittest.TestCase):
 
-    pass
+    def test_incorrect_number_of_classifiers_error(self):
+        """checks that the format has the correct number of columns
+        according to the defined classifiers
+        """
+        self.fail()
+
+    def test_undefined_classifier_value_error(self):
+        """checks that the format has values that are either wildcards or
+        classifier sets drawn from the set of defined classifiers values
+        and aggregates
+        """
+        self.fail()
+
+    def test_differing_hw_sw_age_criteria_error(self):
+        """check that an error is raised if hw age and sw age criteria
+        differ (CBM only has stand age)
+        """
+        self.fail()
+
+    def test_undefined_age_class_error(self):
+        """check that an error is raised if the using_age_class is set
+        to true and any of the age class ids are not valid
+        """
+        self.fail()
+
+    def test_undefined_sort_type_error(self):
+        """check if an error is raised when an invalid sort type is specified
+        """
+        self.fail()
+
+    def test_undefined_target_type_error(self):
+        """check if an error is raised when an invalid target type is
+        specified
+        """
+        self.fail()
+
+    def test_invalid_target_value_error(self):
+        """check if an error is raised when an invalid target value is
+        specified
+        """
+        self.fail()
+
+    def test_invalid_disturbance_year_value_error(self):
+        """check if an error is raised when an invalid target value is
+        specified
+        """
+        self.fail()
+
+    def test_undefined_disturbance_type_value_error(self):
+        """check if an error is raised when an undefined disturbance type
+        value is specified
+        """
+        self.fail()
+
+
