9ca654bab33ff4e15f82f61da965552ff6158322
Scott Morken <smorken@nrcan.gc.ca>
2019-08-30 14:25:03 -0700

    stubbing out methods and tests

--- libcbm/input/sit/sit_yield_parser.py
+++ libcbm/input/sit/sit_yield_parser.py
@@ -1,4 +1,23 @@
+from libcbm.input.sit import sit_parser
+from libcbm.input.sit import sit_format
 
 
-def parse(yield_table, classifiers, classifier_values):
-    pass
+def parse(yield_table, classifiers, classifier_values, age_classes,
+          species_map):
+
+    yield_format = sit_format.get_yield_format(
+        classifiers.name, len(yield_table.columns))
+
+    unpacked_table = sit_parser.unpack_table(
+        yield_table, yield_format, "yield")
+
+    # check that the number of volumes is equal to the number of age classes
+
+    # check that the leading species column is mapped
+
+    # check that the correct number of classifiers are present
+
+    # check that each value in yield table classifier sets is defined in
+    # classifier values
+
+    return unpacked_table
--- libcbm/test/unit/input/sit/sit_yield_parser_test.py
+++ libcbm/test/unit/input/sit/sit_yield_parser_test.py
@@ -3,4 +3,26 @@ from libcbm.input.sit import sit_yield_parser
 
 
 class SITYieldParserTest(unittest.TestCase):
-    pass
+
+    def test_incorrect_number_of_volumes_error(self):
+        """checks that the number of volumes is equal to the number of
+        specified age classes
+        """
+        self.fail()
+
+    def test_undefined_leading_species_value_error(self):
+        """checks if all values are mapped in the species map parameter
+        """
+        self.fail()
+
+    def test_incorrect_number_of_classifiers_error(self):
+        """checks that the format has the correct number of columns
+        according to the defined classifiers
+        """
+        self.fail()
+
+    def test_non_numeric_or_negative_volume_error(self):
+        """checks that an error is raised if any volume is non-numeric or
+        is a negative number.
+        """
+        self.fail()
