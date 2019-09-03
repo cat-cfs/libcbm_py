56077c53b506413ba1161a2953a0f15f4911d854
Scott Morken <smorken@nrcan.gc.ca>
2019-09-03 16:54:33 -0700

    disturbance event parser and tests

--- libcbm/input/sit/sit_disturbance_event_parser.py
+++ libcbm/input/sit/sit_disturbance_event_parser.py
@@ -1,5 +1,29 @@
+import numpy as np
+from libcbm.input.sit import sit_classifier_parser
 from libcbm.input.sit import sit_parser
 from libcbm.input.sit import sit_format
+
+
+def get_sort_types():
+    return {
+        1: "PROPORTION_OF_EVERY_RECORD",
+        2: "MERCHCSORT_TOTAL",
+        3: "SORT_BY_SW_AGE",
+        5: "SVOID ",
+        6: "RANDOMSORT",
+        7: "TOTALSTEMSNAG",
+        8: "SWSTEMSNAG",
+        9: "HWSTEMSNAG",
+        10: "MERCHCSORT_SW",
+        11: "MERCHCSORT_HW",
+        12: "SORT_BY_HW_AGE"}
+
+
+def get_target_types():
+    return {
+        "A": "Area",
+        "P": "Proportion",
+        "M": "Merchantable"}
 
 
 def parse(disturbance_events, classifiers, classifier_values,
@@ -8,24 +32,79 @@ def parse(disturbance_events, classifiers, classifier_values,
     disturbance_event_format = sit_format.get_disturbance_event_format(
           classifiers.name, len(disturbance_events.columns))
 
-    unpacked_table = sit_parser.unpack_table(
+    events = sit_parser.unpack_table(
         disturbance_events, disturbance_event_format, "disturbance events")
 
-    # check that the correct number of classifiers are present
-
-    # check that each value in disturbance events classifier sets is defined in
+    # check that the correct number of classifiers are present, and check
+    # that each value in disturbance events classifier sets is defined in
     # classifier values, classifier aggregates or is a wildcard
+    for row in classifiers.itertuples():
+        event_classifiers = events[row.name].unique()
+
+        defined_classifiers = classifier_values[
+            classifier_values["classifier_id"] == row.id]["name"].unique()
+
+        aggregates = np.array(
+            [x["name"] for x in
+             classifier_aggregates if x["classifier_id"] == row.id])
+        wildcard = np.array([sit_classifier_parser.get_wildcard_keyword()])
+        valid_classifiers = np.concatenate(
+            [defined_classifiers, aggregates, wildcard])
+
+        diff_classifiers = np.setdiff1d(
+            event_classifiers, valid_classifiers)
+        if len(diff_classifiers) > 0:
+            raise ValueError(
+                "Undefined classifier values detected: "
+                f"classifier: '{row.name}', values: {diff_classifiers}")
 
     # if age classes are used substitute the age critera based on the age
     # class id, and raise an error if the id is not defined, and drop
     # using_age_class from output
-
-    # check that all age criteria are identical between SW and HW (since CBM
-    # has only a stand age)
+    parse_bool_func = sit_parser.get_parse_bool_func(
+        "events", "using_age_class")
+    events = sit_parser.substitute_using_age_class_rows(
+        events, parse_bool_func, age_classes)
 
     # validate sort type
+    valid_sort_types = get_sort_types().keys()
+    int_sort_type = events.sort_type.astype(np.int)
+    sort_type_diff = set(int_sort_type.unique()) \
+        .difference(set(valid_sort_types))
+    if len(sort_type_diff) > 0:
+        raise ValueError(
+            f"specified sort types are not valid: {sort_type_diff}")
+    events.sort_type = int_sort_type.map(get_sort_types())
 
     # validate target type
+    valid_target_types = get_target_types().keys()
+    target_type_diff = set(events.target_type.unique()) \
+        .difference(set(valid_target_types))
+    if len(target_type_diff) > 0:
+        raise ValueError(
+            f"specified target types are not valid: {target_type_diff}")
+    events.target_type = events.target_type.map(get_target_types())
 
     # validate disturbance type according to specified disturbance types
-    return unpacked_table
+    undefined_disturbances = np.setdiff1d(
+        events.disturbance_type.unique(),
+        disturbance_types.id.unique()
+    )
+    if len(undefined_disturbances) > 0:
+        raise ValueError(
+            "Undefined disturbance type ids (as defined in sit "
+            f"disturbance types) detected: {undefined_disturbances}"
+        )
+    disturbance_join = events.merge(
+        disturbance_types, left_on="disturbance_type",
+        right_on="id")
+    events.disturbance_type = disturbance_join.name
+
+    events = events.rename(
+        columns={
+            "min_softwood_age": "min_age",
+            "max_softwood_age": "max_age"})
+
+    events = events.drop(
+        columns=["using_age_class", "min_hardwood_age", "max_hardwood_age"])
+    return events
--- libcbm/test/unit/input/sit/sit_disturbance_event_parser_test.py
+++ libcbm/test/unit/input/sit/sit_disturbance_event_parser_test.py
@@ -1,61 +1,242 @@
 import unittest
+import pandas as pd
 from libcbm.input.sit import sit_disturbance_event_parser
-
+from libcbm.input.sit import sit_format
 
 class SITDisturbanceEventParserTest(unittest.TestCase):
+
+    def get_mock_classifiers(self):
+        classifiers = pd.DataFrame(
+            data=[
+                (1, "classifier1"),
+                (2, "classifier2")
+            ],
+            columns=["id", "name"]
+        )
+        classifier_values = pd.DataFrame(
+            data=[
+                (1, "a", "a"),
+                (1, "b", "b"),
+                (2, "a", "a")
+            ],
+            columns=["classifier_id", "name", "description"]
+        )
+        aggregates = [
+            {'classifier_id': 1,
+             'name': 'agg1',
+             'description': 'agg2',
+             'classifier_values': ['a', 'b']},
+            {'classifier_id': 1,
+             'name': 'agg2',
+             'description': 'agg2',
+             'classifier_values': ['a', 'b']},
+            {'classifier_id': 2,
+             'name': 'agg1',
+             'description': 'agg1',
+             'classifier_values': ['a']}]
+
+        return classifiers, classifier_values, aggregates
+
+    def get_mock_age_classes(self):
+        return pd.DataFrame(
+            data=[
+                ("age0", 0, 0, 0),
+                ("age1", 2, 1, 2),
+                ("age2", 2, 3, 4)],
+            columns=["name", "class_size", "start_year", "end_year"]
+        )
+
+    def get_mock_disturbance_types(self):
+        return pd.DataFrame(
+            data=[
+                ("dist1", "fire"),
+                ("dist2", "clearcut")
+            ],
+            columns=["id", "name"]
+        )
+
+    def assemble_disturbance_events_table(self, events):
+        return pd.DataFrame([
+            event["classifier_set"] +
+            event["age_eligibility"] +
+            event["eligibility"] +
+            event["target"]
+            for event in events
+        ])
+
+    def get_num_eligibility_cols(self):
+        num_eligibility_cols = len(
+            sit_format.get_disturbance_eligibility_columns(0))
+        return num_eligibility_cols
 
     def test_incorrect_number_of_classifiers_error(self):
         """checks that the format has the correct number of columns
         according to the defined classifiers
         """
-        self.fail()
+        event = {
+            "classifier_set": ["a", "?", "EXTRA"],
+            "age_eligibility": ["False", -1, -1, -1, -1],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "1", "A", 100, "dist1", 2, 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(), self.get_mock_age_classes())
 
     def test_undefined_classifier_value_error(self):
         """checks that the format has values that are either wildcards or
         classifier sets drawn from the set of defined classifiers values
         and aggregates
         """
-        self.fail()
+        event = {
+            "classifier_set": ["UNDEFINED", "?"],
+            "age_eligibility": ["False", -1, -1, -1, -1],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "1", "A", 100, "dist1", 2, 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(),
+                self.get_mock_age_classes())
 
     def test_differing_hw_sw_age_criteria_error(self):
         """check that an error is raised if hw age and sw age criteria
         differ (CBM only has stand age)
         """
-        self.fail()
+        event = {
+            "classifier_set": ["a", "?"],
+            "age_eligibility": ["False", -1, 10, -1, 20],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "1", "A", 100, "dist1", 2, 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(),
+                self.get_mock_age_classes())
 
     def test_undefined_age_class_error(self):
         """check that an error is raised if the using_age_class is set
         to true and any of the age class ids are not valid
         """
-        self.fail()
+        event = {
+            "classifier_set": ["a", "?"],
+            "age_eligibility": ["True", -1, -1, -1, "UNDEFINED"],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "1", "A", 100, "dist1", 2, 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(),
+                self.get_mock_age_classes())
 
     def test_undefined_sort_type_error(self):
         """check if an error is raised when an invalid sort type is specified
         """
-        self.fail()
+        event = {
+            "classifier_set": ["a", "?"],
+            "age_eligibility": ["True", -1, -1, -1, -1],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "UNDEFINED", "A", 100, "dist1", 2, 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(),
+                self.get_mock_age_classes())
 
     def test_undefined_target_type_error(self):
         """check if an error is raised when an invalid target type is
         specified
         """
-        self.fail()
+        event = {
+            "classifier_set": ["a", "?"],
+            "age_eligibility": ["True", -1, -1, -1, -1],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "1", "UNDEFINED", 100, "dist1", 2, 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(),
+                self.get_mock_age_classes())
 
     def test_invalid_target_value_error(self):
         """check if an error is raised when an invalid target value is
         specified
         """
-        self.fail()
+        event = {
+            "classifier_set": ["a", "?"],
+            "age_eligibility": ["True", -1, -1, -1, -1],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "1", "A", "invalid", "dist1", 2, 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(),
+                self.get_mock_age_classes())
 
     def test_invalid_disturbance_year_value_error(self):
         """check if an error is raised when an invalid target value is
         specified
         """
-        self.fail()
+        event = {
+            "classifier_set": ["a", "?"],
+            "age_eligibility": ["True", -1, -1, -1, -1],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "1", "A", "1", "dist1", "invalid", 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(),
+                self.get_mock_age_classes())
 
     def test_undefined_disturbance_type_value_error(self):
         """check if an error is raised when an undefined disturbance type
         value is specified
         """
-        self.fail()
+        event = {
+            "classifier_set": ["a", "?"],
+            "age_eligibility": ["True", -1, -1, -1, -1],
+            "eligibility": [-1] * self.get_num_eligibility_cols(),
+            "target": [1.0, "1", "A", "1", "UNDEFINED_DIST", "1", 100]}
+
+        classifiers, classifier_values, aggregates = \
+            self.get_mock_classifiers()
+        with self.assertRaises(ValueError):
+            e = self.assemble_disturbance_events_table([event])
+            sit_disturbance_event_parser.parse(
+                e, classifiers, classifier_values, aggregates,
+                self.get_mock_disturbance_types(),
+                self.get_mock_age_classes())
 
 
