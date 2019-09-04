68d9c1a050eb7827aa2c3ec893e123786c147c44
Scott Morken <smorken@nrcan.gc.ca>
2019-09-04 15:56:29 -0700

    code docs

--- libcbm/input/sit/sit_cbm_factory.py
+++ libcbm/input/sit/sit_cbm_factory.py
@@ -6,6 +6,19 @@ from libcbm.model.cbm.cbm_defaults_reference import CBMDefaultsReference
 
 
 def get_classifiers(classifiers, classifier_values):
+    """Create classifier input for initializing the CBM class based on CBM
+    Standard import tool formatted data.
+
+    Args:
+        classifiers (pandas.DataFrame): the parsed SIT classifiers output
+            of :py:func:`libcbm.input.sit.sit_classifier_parser.parse`
+        classifier_values (pandas.DataFrame): the parsed SIT classifier values
+            output of :py:func:`libcbm.input.sit.sit_classifier_parser.parse`
+
+    Returns:
+        dict: configuration dictionary for CBM. See:
+            :py:func:`libcbm.model.cbm.cbm_config.classifier_config`
+    """
     classifiers_config = []
     for _, row in classifiers.iterrows():
         values = classifier_values[
@@ -22,7 +35,24 @@ def get_classifiers(classifiers, classifier_values):
 
 def get_merch_volumes(yield_table, classifiers, age_classes,
                       cbm_defaults_ref):
+    """Create merchantable volume input for initializing the CBM class
+    based on CBM Standard import tool formatted data.
 
+    Args:
+        yield_table (pandas.DataFrame): the parsed SIT yield output
+            of :py:func:`libcbm.input.sit.sit_yield_parser.parse`
+        classifiers (pandas.DataFrame): the parsed SIT classifiers output
+            of :py:func:`libcbm.input.sit.sit_classifier_parser.parse`
+        age_classes (pandas.DataFrame): the parsed SIT age classes
+            output of :py:func:`libcbm.input.sit.sit_age_class_parser.parse`
+        cbm_defaults_ref (CBMDefaultsReference): instance of
+            :py:class:`libcbm.model.cbm.cbm_defaults_reference.CBMDefaultsReference`
+            for fetching species information
+
+    Returns:
+        dict: configuration dictionary for CBM. See:
+            :py:func:`libcbm.model.cbm.cbm_config.classifier_config`
+    """
     default_species_map = {
         x["species_name"]: x["species_id"]
         for x in cbm_defaults_ref.get_species()}
@@ -61,7 +91,24 @@ def get_merch_volumes(yield_table, classifiers, age_classes,
 
 def initialize_cbm(db_path, dll_path, yield_table, classifiers,
                    classifier_values, age_classes):
+    """Create an initialized instance of
+        :py:class:`libcbm.model.cbm.cbm_model.CBM` based on SIT input
 
+    Args:
+        db_path (str): path to a cbm_defaults database
+        dll_path (str): path to the libcbm compiled library
+        yield_table (pandas.DataFrame): the parsed SIT yield output
+            of :py:func:`libcbm.input.sit.sit_yield_parser.parse`
+        classifiers (pandas.DataFrame): the parsed SIT classifiers output
+            of :py:func:`libcbm.input.sit.sit_classifier_parser.parse`
+        classifier_values (pandas.DataFrame): the parsed SIT classifier values
+            output of :py:func:`libcbm.input.sit.sit_classifier_parser.parse`
+        age_classes (pandas.DataFrame): the parsed SIT age classes
+            output of :py:func:`libcbm.input.sit.sit_age_class_parser.parse`
+
+    Returns:
+        libcbm.model.cbm.cbm_model.CBM: an initialized CBM instance
+    """
     cbm_defaults_ref = CBMDefaultsReference(db_path)
 
     cbm = cbm_factory.create(
