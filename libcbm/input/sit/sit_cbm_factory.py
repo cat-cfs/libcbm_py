aa5eb618cdbec9b60549c752610b07a89361c53b
Scott Morken <smorken@nrcan.gc.ca>
2019-09-11 16:30:39 -0700

    refactor: consolidated parameters into objects

--- libcbm/input/sit/sit_cbm_factory.py
+++ libcbm/input/sit/sit_cbm_factory.py
@@ -81,10 +81,10 @@ def get_merch_volumes(yield_table, classifiers, classifier_values, age_classes,
     return output
 
 
-def initialize_inventory(inventory, classifiers, classifier_values,
-                         sit_mapping):
+def initialize_inventory(sit_data, sit_mapping):
 
-    classifier_config = get_classifiers(classifiers, classifier_values)
+    classifier_config = get_classifiers(
+        sit_data.classifiers, sit_data.classifier_values)
     classifier_ids = [
         (x["id"], x["name"]) for x in classifier_config["classifiers"]]
     classifier_value_id_lookups = {}
@@ -96,54 +96,49 @@ def initialize_inventory(inventory, classifiers, classifier_values,
             if x["classifier_id"] == identifier}
 
     classifiers_data = np.column_stack([
-        inventory[name].map(classifier_value_id_lookups[name])
-        for name in list(classifiers.name)
+        sit_data.inventory[name].map(classifier_value_id_lookups[name])
+        for name in list(sit_data.classifiers.name)
     ])
 
     classifiers_data = np.ascontiguousarray(classifiers_data)
     classifiers_result = pd.DataFrame(
         data=classifiers_data,
-        columns=list(classifiers.name))
+        columns=list(sit_data.classifiers.name))
 
     inventory_result = pd.DataFrame(
         data={
-            "age": inventory.age,
+            "age": sit_data.inventory.age,
             "spatial_unit": sit_mapping.get_spatial_unit(
-                inventory, classifiers, classifier_values),
+                sit_data.inventory, sit_data.classifiers,
+                sit_data.classifier_values),
             "afforestation_pre_type_id": sit_mapping.get_nonforest_cover_ids(
-                inventory, classifiers, classifier_values),
-            "area": inventory.area,
-            "delay": inventory.delay,
-            "land_class": sit_mapping.get_land_class_id(inventory.land_class),
+                sit_data.inventory, sit_data.classifiers,
+                sit_data.classifier_values),
+            "area": sit_data.inventory.area,
+            "delay": sit_data.inventory.delay,
+            "land_class": sit_mapping.get_land_class_id(
+                sit_data.inventory.land_class),
             "historical_disturbance_type":
                 sit_mapping.get_disturbance_type_id(
-                    inventory.historical_disturbance_type),
+                    sit_data.inventory.historical_disturbance_type),
             "last_pass_disturbance_type":
                 sit_mapping.get_disturbance_type_id(
-                    inventory.last_pass_disturbance_type),
+                    sit_data.inventory.last_pass_disturbance_type),
         })
     return classifiers_result, inventory_result
 
 
-def initialize_cbm(db_path, dll_path, yield_table, classifiers,
-                   classifier_values, age_classes, sit_mapping):
+def initialize_cbm(db_path, dll_path, sit_data, sit_mapping):
     """Create an initialized instance of
         :py:class:`libcbm.model.cbm.cbm_model.CBM` based on SIT input
 
     Args:
         db_path (str): path to a cbm_defaults database
         dll_path (str): path to the libcbm compiled library
-        yield_table (pandas.DataFrame): the parsed SIT yield output
-            of :py:func:`libcbm.input.sit.sit_yield_parser.parse`
-        classifiers (pandas.DataFrame): the parsed SIT classifiers output
-            of :py:func:`libcbm.input.sit.sit_classifier_parser.parse`
-        classifier_values (pandas.DataFrame): the parsed SIT classifier values
-            output of :py:func:`libcbm.input.sit.sit_classifier_parser.parse`
-        age_classes (pandas.DataFrame): the parsed SIT age classes
-            output of :py:func:`libcbm.input.sit.sit_age_class_parser.parse`
-        sit_mapping (libcbm.input.sit.sit_mapping.SITMapping): instance of
-            SITMapping used to validate species classifier and fetch species id
-            for merch volume curves
+        sit_data (object): a standard import dataset as returned by:
+            :py:func:`libcbm.input.sit.sit_reader.read`
+        sit_mapping (libcbm.input.sit.sit_mapping.SITMapping): class used to
+            link sit data to default parameters
 
     Returns:
         libcbm.model.cbm.cbm_model.CBM: an initialized CBM instance
@@ -158,9 +153,10 @@ def initialize_cbm(db_path, dll_path, yield_table, classifiers,
             cbm_config.merch_volume_to_biomass_config(
                 db_path=db_path,
                 merch_volume_curves=get_merch_volumes(
-                    yield_table, classifiers, classifier_values,
-                    age_classes, sit_mapping)),
+                    sit_data.yield_table, sit_data.classifiers,
+                    sit_data.classifier_values, sit_data.age_classes,
+                    sit_mapping)),
         classifiers_factory=lambda: get_classifiers(
-            classifiers, classifier_values))
+            sit_data.classifiers, sit_data.classifier_values))
 
     return cbm
