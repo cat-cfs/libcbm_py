459fb24cd2fffcaf0332ded833deab43257449fa
Scott Morken <smorken@nrcan.gc.ca>
2019-09-30 16:32:43 -0700

    docs

--- libcbm/resources/__init__.py
+++ libcbm/resources/__init__.py
@@ -5,15 +5,34 @@ import warnings
 
 
 def get_local_dir():
+    """Gets the directory containing this script
+
+    Returns:
+        str: full path to the the script's directory
+    """
     return os.path.dirname(os.path.realpath(__file__))
 
 
 def get_cbm_defaults_path():
+    """Gets the path to the packaged cbm defaults sqlite database
+
+    Returns:
+        str: path to the bundled database
+    """
     return os.path.join(
         get_local_dir(), "cbm_defaults_db", "cbm_defaults_2019.db")
 
 
 def get_libcbm_bin_path():
+    """Returns bundled, OS specific compiled libcbm C/C++ library
+
+    Raises:
+        RuntimeError: 32 bit systems are not supported
+        RuntimeError: unknown platform.system() value
+
+    Returns:
+        str: path to the bundled, compiled dll or so file
+    """
     local_dir = get_local_dir()
     if sys.maxsize <= 2**32:
         raise RuntimeError("32 bit python not supported")
