04810cdbe9a7d7b30789d0798563c6ae71d4c6db
Scott Morken <smorken@nrcan.gc.ca>
2019-10-08 13:52:10 -0700

    fixed for real this time

--- libcbm/model/cbm/cbm_defaults_queries/spinup_parameter.sql
+++ libcbm/model/cbm/cbm_defaults_queries/spinup_parameter.sql
@@ -3,6 +3,6 @@ spatial_unit.id as spatial_unit_id,
 spinup_parameter.return_interval,
 spinup_parameter.historic_mean_temperature,
 spinup_parameter.min_rotations,
-spinup_parameter.max_rotations,
+spinup_parameter.max_rotations
 from spinup_parameter inner join spatial_unit on
 spatial_unit.spinup_parameter_id == spinup_parameter.id
