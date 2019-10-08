2960cd04a1624fc198a4709e81ff9ac4d3f57b68
Scott Morken <smorken@nrcan.gc.ca>
2019-10-08 13:50:27 -0700

    bugfix, extra column in query

--- libcbm/model/cbm/cbm_defaults_queries/spinup_parameter.sql
+++ libcbm/model/cbm/cbm_defaults_queries/spinup_parameter.sql
@@ -4,6 +4,5 @@ spinup_parameter.return_interval,
 spinup_parameter.historic_mean_temperature,
 spinup_parameter.min_rotations,
 spinup_parameter.max_rotations,
-spinup_parameter.return_interval
 from spinup_parameter inner join spatial_unit on
 spatial_unit.spinup_parameter_id == spinup_parameter.id
