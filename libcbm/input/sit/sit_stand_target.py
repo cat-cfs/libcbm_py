8dc300d7151738f8ec4f2af34a0b49b182df2a80
Scott Morken <smorken@nrcan.gc.ca>
2019-10-09 16:35:31 -0700

    function to decide if it's necessary to compute production via disturbance matrix

--- libcbm/input/sit/sit_stand_target.py
+++ libcbm/input/sit/sit_stand_target.py
@@ -2,6 +2,23 @@ from libcbm.model.cbm.rule_based import rule_target
 from libcbm.input.sit import sit_disturbance_event_parser
 
 
+def is_production_based(sit_event_row):
+    """Returns true if the specified disturbance event requires computation
+    of production via disturbance matrix
+
+    Args:
+        sit_event_row (dict): a row dict from parsed SIT disturbance events
+    """
+    production_sorts = ["MERCHCSORT_TOTAL", "MERCHCSORT_SW", "MERCHCSORT_HW"]
+    if sit_event_row["sort_type"] in production_sorts:
+        # sorted by the production
+        return True
+    if sit_event_row["target"] == sit_disturbance_event_parser.get_target_types("M"):
+        # the production is the target variable
+        return True
+    return False
+
+
 def get_production_sort_value(sort_type, pools, production):
     if sort_type == "MERCHCSORT_TOTAL":
         return production.Total
@@ -15,12 +32,20 @@ def get_production_sort_value(sort_type, pools, production):
         raise ValueError(
             f"specified sort_type '{sort_type}' is not a production sort")
 
+
 def get_sort_value(sort_type, pools, state_variables):
-    if sort_type == "SORT_BY_SW_AGE":
+    if sort_type == "SORT_BY_SW_AGE" or sort_type == "SORT_BY_HW_AGE":
         return state_variables.age
     elif sort_type == "TOTALSTEMSNAG":
         return pools.SoftwoodStemSnag + pools.HardwoodStemSnag
-    elif sort_type ==
+    elif sort_type == "SWSTEMSNAG":
+        return pools.SoftwoodStemSnag
+    elif sort_type == "HWSTEMSNAG":
+        return pools.HardwoodStemSnag
+    else:
+        raise ValueError(
+            f"specified sort_type '{sort_type}' is not sort value")
+
 
 def create_sit_event_target(sit_event_row, inventory):
 
