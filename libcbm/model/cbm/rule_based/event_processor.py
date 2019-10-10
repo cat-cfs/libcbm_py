65399757120524419a011f1bf6a564df5b581bdc
Scott Morken <smorken@nrcan.gc.ca>
2019-10-10 16:24:47 -0700

    function layout

--- libcbm/model/cbm/rule_based/event_processor.py
+++ libcbm/model/cbm/rule_based/event_processor.py
@@ -13,9 +13,27 @@ def select_rule_based_events(rule_based_events, time_step):
         #     , classifier_values, state_variables, pools)
 
 
+def process_event(filter_factory, classifiers_filter_factory, filter_data,
+                  undisturbed, target_func, pools, state_variables,
+                  classifiers, inventory):
+
+    filter_result = apply_filter(
+        filter_factory, classifiers_filter_factory, filter_data,
+        undisturbed, pools, state_variables, classifiers)
+
+    filtered_inventory = inventory[filter_result]
+    filtered_state_variables = state_variables[filter_result]
+    filtered_pools = pools[filter_result]
+
+    target = apply_rule_based_target(
+        target_func, filtered_pools, filtered_inventory,
+        filtered_state_variables)
+
+    return apply_rule_based_event(
+        target, classifiers, inventory, pools, state_variables)
+
 def apply_filter(filter_factory, classifiers_filter_factory, filter_data,
-                 undisturbed, pools, state_variables, classifiers,
-                 inventory):
+                 undisturbed, pools, state_variables, classifiers):
 
     classifier_filter = classifiers_filter_factory(
         filter_data.classifier_set, classifiers)
@@ -30,24 +48,29 @@ def apply_filter(filter_factory, classifiers_filter_factory, filter_data,
             data=state_variables,
             columns=filter_data.state_filter_columns),
         classifier_filter)
-    filtered = filter_factory.evaluate_filter(merged_filter)
+    filter_result = filter_factory.evaluate_filter(merged_filter)
 
-    # remove those stands affected by a previous disturbance from eligibility
-    filtered = np.logical_and(undisturbed, filtered)
+    # set to false those stands affected by a previous disturbance from
+    # eligibility
+    filter_result = np.logical_and(undisturbed, filter_result)
 
-    return pools[filtered], state_variables[filtered], inventory[filtered]
+    return filter_result
 
 
-def apply_rule_based_event(pools, classifiers, inventory, state_variables,
-                           target_func):
+def apply_rule_based_target(target_func, pools, inventory, state_variables):
+    return target_func(pools, inventory, state_variables)
 
-    target = target_func(pools, inventory, state_variables)
+
+def apply_rule_based_event(target, classifiers, inventory, pools,
+                           state_variables):
+
     target_index = target["disturbed_indices"]
     target_area_proportions = target["area_proportions"]
 
     updated_inventory = inventory.copy()
     updated_classifiers = classifiers.copy()
     updated_state_variables = state_variables.copy()
+    updated_pools = pools.copy()
 
     split_index = target_index[target_area_proportions < 1.0]
     split_inventory = updated_inventory.iloc[split_index].copy()
@@ -63,13 +86,14 @@ def apply_rule_based_event(pools, classifiers, inventory, state_variables,
     # create the updated inventory by appending the split records
     updated_inventory = updated_inventory.append(split_inventory)
 
-    # since classifiers and state variables are not altered here (this is done
-    # in the model) splitting is just a matter of adding a copy of the split
-    # values
+    # since classifiers, pools, and state variables are not altered here (this
+    # is done in the model) splitting is just a matter of adding a copy of the
+    # split values
     updated_classifiers = updated_classifiers.append(
         updated_classifiers.iloc[split_index].copy())
     updated_state_variables = updated_state_variables.append(
         updated_state_variables[split_index].copy())
-
+    updated_pools = updated_pools.append(
+        updated_pools[split_index].copy())
     return (target_index, updated_inventory, updated_classifiers,
             updated_state_variables)
