964c8d4b9edb7980c01637e30f7b3b6c61bc7d67
Scott Morken <smorken@nrcan.gc.ca>
2019-10-11 16:58:06 -0700

    event_iterator function started

--- libcbm/model/cbm/rule_based/sit/sit_event_processor.py
+++ libcbm/model/cbm/rule_based/sit/sit_event_processor.py
@@ -1,1 +1,12 @@
 
+
+def event_iterator(time_step, sit_events):
+
+    # TODO: In CBM-CFS3 events are sorted by default disturbance type id
+    # (ascending) In libcbm, sort order needs to be explicitly defined in
+    # cbm_defaults (or other place)
+    time_step_events = sit_events[
+        sit_events.time_step == time_step]
+    for _, time_step_event in time_step_events.itterows():
+        yield dict(time_step_event)
+
