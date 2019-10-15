b9ba9888292d530d54b1595d013c8b2cc4b1311b
Scott Morken <smorken@nrcan.gc.ca>
2019-10-15 16:33:21 -0700

    docs

--- libcbm/model/cbm/rule_based/event_processor.py
+++ libcbm/model/cbm/rule_based/event_processor.py
@@ -4,7 +4,49 @@ import numpy as np
 def process_event(filter_factory, classifiers_filter_factory, filter_data,
                   undisturbed, target_func, pools, state_variables,
                   classifiers, inventory):
+    """Computes a CBM rule based event by filtering and targeting a subset of
+    the specified inventory.  In the case of merchantable or area targets
+    splits may occur to meet a disturbance target exactly.
 
+    Args:
+        filter_factory (object): collection of methods to create and apply
+            filters for CBM stands.
+            See :py:mod:`libcbm.model.cbm.rule_based.rule_filter`
+        classifiers_filter_factory (object): collection of methods to filter
+            stands by classifier values using classifier sets.
+            See: :py:class:`libcbm.model.cbm.rule_based.classifier_filter.ClassifierFilter`
+        filter_data (object): object containing pool and state variable filter
+            expressions. See:
+            :py:func:`libcbm.model.cbm.rule_based.rule_filter.create_filter`
+            for creation methods. Has the following properties:
+
+            - pool_filter_expression (str): a boolean formatted expression in
+                terms of CBM pools
+            - pool_filter_columns (list): list of the string names of pools
+                referenced in the expression.
+            - state_filter_expression (str): a boolean formatted expression in
+                terms of CBM state variables
+            - state_filter_columns (list): list of the string names of state
+                variables referenced in the expression.
+            - classifier_set (list): a classifier set, which is a list of
+                classifier values, aggregates or wildcards used to filter
+                stands for eligibility.
+
+        undisturbed (pandas.Series): a boolean value series indicating each
+            specified index is eligible (True) or ineligible (False) for
+            disturbance.
+        target_func (func): a function for creating a disturbance target.
+            See: :py:mod:`libcbm.model.cbm.rule_based.rule_target`
+        pools (pandas.DataFrame): CBM simulation pools
+        state_variables (pandas.DataFrame): CBM simulation state variables
+        classifiers (pandas.DataFrame): CBM classifier values
+        inventory (pandas.DataFrame): CBM inventory
+
+    Returns:
+        tuple: The computed disturbance index and pools, state variables,
+            classifiers and inventory which can be modified when splitting
+            occurs. See the return value of :py:func:`apply_rule_based_event`
+    """
     filter_result = apply_filter(
         filter_factory, classifiers_filter_factory, filter_data,
         undisturbed, pools, state_variables, classifiers)
