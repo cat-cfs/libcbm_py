7cf6380505f56aca79f0753839d5cd8f10d2fe57
Scott Morken <smorken@nrcan.gc.ca>
2019-10-16 10:31:32 -0700

    refactor

--- libcbm/model/cbm/rule_based/event_processor.py
+++ libcbm/model/cbm/rule_based/event_processor.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def process_event(filter_factory, classifiers_filter_factory, filter_data,
-                  undisturbed, target_func, pools, state_variables,
-                  classifiers, inventory):
+                  undisturbed, target_func, classifiers, inventory, pools,
+                  state_variables):
     """Computes a CBM rule based event by filtering and targeting a subset of
     the specified inventory.  In the case of merchantable or area targets
     splits may occur to meet a disturbance target exactly.
@@ -14,7 +14,8 @@ def process_event(filter_factory, classifiers_filter_factory, filter_data,
             See :py:mod:`libcbm.model.cbm.rule_based.rule_filter`
         classifiers_filter_factory (object): collection of methods to filter
             stands by classifier values using classifier sets.
-            See: :py:class:`libcbm.model.cbm.rule_based.classifier_filter.ClassifierFilter`
+            See:
+            :py:class:`libcbm.model.cbm.rule_based.classifier_filter.ClassifierFilter`
         filter_data (object): object containing pool and state variable filter
             expressions. See:
             :py:func:`libcbm.model.cbm.rule_based.rule_filter.create_filter`
@@ -37,34 +38,16 @@ def process_event(filter_factory, classifiers_filter_factory, filter_data,
             disturbance.
         target_func (func): a function for creating a disturbance target.
             See: :py:mod:`libcbm.model.cbm.rule_based.rule_target`
-        pools (pandas.DataFrame): CBM simulation pools
-        state_variables (pandas.DataFrame): CBM simulation state variables
         classifiers (pandas.DataFrame): CBM classifier values
         inventory (pandas.DataFrame): CBM inventory
+        pools (pandas.DataFrame): CBM simulation pools
+        state_variables (pandas.DataFrame): CBM simulation state variables
 
     Returns:
         tuple: The computed disturbance index and pools, state variables,
             classifiers and inventory which can be modified when splitting
             occurs. See the return value of :py:func:`apply_rule_based_event`
     """
-    filter_result = apply_filter(
-        filter_factory, classifiers_filter_factory, filter_data,
-        undisturbed, pools, state_variables, classifiers)
-
-    filtered_inventory = inventory[filter_result]
-    filtered_state_variables = state_variables[filter_result]
-    filtered_pools = pools[filter_result]
-
-    target = target_func(
-        filtered_pools, filtered_inventory, filtered_state_variables)
-
-    return apply_rule_based_event(
-        target, classifiers, inventory, pools, state_variables)
-
-
-def apply_filter(filter_factory, classifiers_filter_factory, filter_data,
-                 undisturbed, pools, state_variables, classifiers):
-
     classifier_filter = classifiers_filter_factory(
         filter_data.classifier_set, classifiers)
 
@@ -84,12 +67,43 @@ def apply_filter(filter_factory, classifiers_filter_factory, filter_data,
     # eligibility
     filter_result = np.logical_and(undisturbed, filter_result)
 
-    return filter_result
+    filtered_inventory = inventory[filter_result]
+    filtered_state_variables = state_variables[filter_result]
+    filtered_pools = pools[filter_result]
+
+    target = target_func(
+        filtered_pools, filtered_inventory, filtered_state_variables)
+
+    return apply_rule_based_event(
+        target, classifiers, inventory, pools, state_variables)
 
 
 def apply_rule_based_event(target, classifiers, inventory, pools,
                            state_variables):
+    """Apply the specified target to the CBM simulation variables,
+    splitting them if necessary.
 
+    Args:
+        target (pandas.DataFrame): dataframe describing the index of
+            records to disturbance and area split proportions.  See return
+            value of methods in
+            :py:mod:`libcbm.model.cbm.rule_based.rule_target`
+        classifiers (pandas.DataFrame): CBM classifier values
+        inventory (pandas.DataFrame): CBM inventory
+        pools (pandas.DataFrame): CBM simulation pools
+        state_variables (pandas.DataFrame): CBM simulation state variables
+
+    Returns:
+        tuple: tuple of copies of the 4 input variables with splits applied if
+            necessary:
+
+            1. classifiers (pandas.DataFrame): CBM classifier values
+            2. inventory (pandas.DataFrame): CBM inventory
+            3. pools (pandas.DataFrame): CBM simulation pools
+            4. state_variables (pandas.DataFrame): CBM simulation state
+               variables
+
+    """
     target_index = target["disturbed_index"]
     target_area_proportions = target["area_proportions"]
 
@@ -98,19 +112,20 @@ def apply_rule_based_event(target, classifiers, inventory, pools,
     updated_state_variables = state_variables.copy()
     updated_pools = pools.copy()
 
-    split_index = target_index[target_area_proportions < 1.0]
+    splits = target_area_proportions < 1.0
+    split_index = target_index[splits]
     split_inventory = updated_inventory.iloc[split_index].copy()
     if len(split_inventory.index) > 0:
         # reduce the area of the disturbed inventory by the disturbance area
         # proportion
         updated_inventory.area[split_index] = \
             updated_inventory.area[split_index] * \
-            target_area_proportions[target_area_proportions < 1.0].array
+            target_area_proportions[splits].array
 
         # set the split inventory as the remaining undisturbed area
         split_inventory.area = \
             split_inventory.area * \
-            (1.0 - target_area_proportions[target_area_proportions < 1.0].array)
+            (1.0 - target_area_proportions[splits].array)
 
         # create the updated inventory by appending the split records
         updated_inventory = updated_inventory.append(
--- test/model/cbm/rule_based/event_processor_test.py
+++ test/model/cbm/rule_based/event_processor_test.py
@@ -77,84 +77,16 @@ class EventProcessorTest(unittest.TestCase):
                 filter_data=mock_filter_data,
                 undisturbed=mock_undisturbed,
                 target_func=mock_target_func,
-                pools=mock_pools,
-                state_variables=mock_state_variables,
                 classifiers=mock_classifiers,
-                inventory=mock_inventory)
+                inventory=mock_inventory,
+                pools=mock_pools,
+                state_variables=mock_state_variables)
 
         # no splits occurred here, so the inputs are returned
-        self.assertTrue(inventory.equals(mock_inventory))
         self.assertTrue(classifiers.equals(mock_classifiers))
-        self.assertTrue(state_variables.equals(mock_state_variables))
+        self.assertTrue(inventory.equals(mock_inventory))
         self.assertTrue(pools.equals(mock_pools))
-
-    def test_apply_filter_expected_result(self):
-
-        mock_undisturbed = [False, True, False, True, True]
-
-        mock_pools = pd.DataFrame([
-            [1, 1, 1, 1],
-            [1, 1, 1, 1],
-            [1, 1, 1, 1],
-            [1, 1, 1, 1]
-        ], columns=["a", "b", "c", "d"])
-
-        mock_state_variables = pd.DataFrame([
-            [1, 1, 1],
-            [1, 1, 1],
-            [1, 1, 1],
-            [1, 1, 1]
-        ], columns=["i", "j", "k"])
-
-        mock_classifiers = pd.DataFrame([
-            [1, 1],
-            [1, 1],
-            [1, 1],
-            [1, 1]
-        ], columns=["c1", "c2"])
-
-        mock_filter_factory = Mock()
-        mock_filter_factory.merge_filters = Mock()
-
-        def mock_merge_filters(*args):
-            self.assertTrue(
-                list(args) == [
-                    "mock_created_filter", "mock_created_filter",
-                    "mock_classifier_filter"])
-
-        mock_filter_factory.merge_filters.side_effect = mock_merge_filters
-        mock_filter_factory.create_filter = Mock()
-        mock_filter_factory.create_filter.side_effect = \
-            lambda expression, data, columns: "mock_created_filter"
-
-        mock_filter_factory.evaluate_filter = Mock()
-        mock_evaluate_filter_return = [False, True, False, True, False]
-        mock_filter_factory.evaluate_filter.side_effect = \
-            lambda merged_filter: mock_evaluate_filter_return
-
-        mock_classifiers_filter_factory = Mock()
-        mock_classifiers_filter_factory.side_effect = \
-            lambda classifier_set, classifiers: "mock_classifier_filter"
-
-        mock_filter_data = SimpleNamespace(
-            classifier_set="mock_classifier_set",
-            pool_filter_expression="mock_pool_filter",
-            pool_filter_columns="mock_pool_filter_columns",
-            state_filter_expression="mock_tate_filter",
-            state_filter_columns="mock_tate_filter_columns",
-        )
-
-        result = event_processor.apply_filter(
-            filter_factory=mock_filter_factory,
-            classifiers_filter_factory=mock_classifiers_filter_factory,
-            filter_data=mock_filter_data,
-            undisturbed=mock_undisturbed,
-            pools=mock_pools,
-            state_variables=mock_state_variables,
-            classifiers=mock_classifiers)
-        self.assertTrue(
-            list(result) == list(
-                np.logical_and(mock_undisturbed, mock_evaluate_filter_return)))
+        self.assertTrue(state_variables.equals(mock_state_variables))
 
     def test_apply_rule_based_event_expected_result_with_no_split(self):
 
