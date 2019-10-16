395e72a9fea655799f88836b9eaef24d4a5031df
Scott Morken <smorken@nrcan.gc.ca>
2019-10-16 16:33:24 -0700

    sit_event_processor layout, decouple other modules for testability

--- libcbm/model/cbm/rule_based/sit/sit_event_processor.py
+++ libcbm/model/cbm/rule_based/sit/sit_event_processor.py
@@ -1,32 +1,7 @@
-
-from libcbm.model.cbm.rule_based import rule_filter
-from libcbm.model.cbm.rule_based import rule_target
-from libcbm.model.cbm.rule_based import event_processor
-from libcbm.model.cbm.rule_based.classifier_filter import ClassifierFilter
-
 from libcbm.model.cbm.rule_based.sit import sit_stand_filter
 from libcbm.model.cbm.rule_based.sit import sit_stand_target
 
 from libcbm.model.cbm import cbm_variables
-
-
-def get_compute_disturbance_production(model_functions, compute_functions,
-                                       eligible, flux_codes,
-                                       disturbance_type):
-
-    def compute_disturbance_production(pools, inventory,
-                                       disturbance_type_name):
-        rule_target.compute_disturbance_production(
-            model_functions=model_functions,
-            compute_functions=compute_functions,
-            pools=pools,
-            inventory=inventory,
-            disturbance_type=disturbance_type,
-            flux=cbm_variables.initialize_flux(
-                inventory.shape[0], flux_codes),
-            eligible=eligible
-            )
-    return compute_disturbance_production
 
 
 def event_iterator(time_step, sit_events):
@@ -41,29 +16,54 @@ def event_iterator(time_step, sit_events):
 
 
 class SITEventProcessor():
-    def __init__(self, model_functions, compute_functions, cbm_defaults_ref,
-                 classifiers_config, classifier_aggregates, random_generator):
+
+    def __init__(self, rule_filter_functions, rule_target_functions,
+                 event_processor_functions, model_functions,
+                 compute_functions, cbm_defaults_ref,
+                 classifier_filter_builder, random_generator):
         self.model_functions = model_functions
         self.compute_functions = compute_functions
 
-        self.classifier_filter_builder = ClassifierFilter(
-            classifiers_config, classifier_aggregates)
+        self.classifier_filter_builder = classifier_filter_builder
         self.cbm_defaults_ref = cbm_defaults_ref
         self.random_generator = random_generator
+
+        self.event_processor = event_processor_functions
+        self.rule_filter = rule_filter_functions
+        self.rule_target = rule_target_functions
+
+    def get_compute_disturbance_production(self, model_functions,
+                                           compute_functions,
+                                           eligible, flux_codes):
+
+        def compute_disturbance_production(pools, inventory,
+                                           disturbance_type):
+
+            lookup_func = self.cbm_defaults_ref.get_disturbance_type_id
+            self.rule_target.compute_disturbance_production(
+                model_functions=model_functions,
+                compute_functions=compute_functions,
+                pools=pools,
+                inventory=inventory,
+                disturbance_type=lookup_func(disturbance_type),
+                flux=cbm_variables.initialize_flux(
+                    inventory.shape[0], flux_codes),
+                eligible=eligible
+                )
+        return compute_disturbance_production
 
     def process_event(self, eligible, sit_event, classifiers, inventory, pools,
                       state_variables, on_unrealized):
 
-        compute_disturbance_production = get_compute_disturbance_production(
-            model_functions=self.model_functions,
-            compute_functions=self.compute_functions,
-            eligible=eligible,
-            flux_codes=self.cbm_defaults_ref.get_flux_indicators(),
-            disturbance_type=self.cbm_defaults_ref.get_disturbance_type_id(
-                sit_event["disturbance_type"])
-        )
+        compute_disturbance_production = \
+            self.get_compute_disturbance_production(
+                model_functions=self.model_functions,
+                compute_functions=self.compute_functions,
+                eligible=eligible,
+                flux_codes=self.cbm_defaults_ref.get_flux_indicators())
+
         target_factory = sit_stand_target.create_sit_event_target_factory(
-            rule_target=rule_target,
+            rule_target=self.rule_target,
             sit_event_row=sit_event,
             disturbance_production_func=compute_disturbance_production,
             eligible=eligible,
@@ -76,12 +76,14 @@ class SITEventProcessor():
         state_filter_expression, state_filter_cols = \
             sit_stand_filter.create_state_variable_filter_expression(
                 sit_event,
-                sit_stand_filter.get_state_variable_age_filter_mappings())
+                sit_stand_filter.get_state_variable_filter_mappings())
 
-        event_filter = rule_filter.merge_filters(
-            rule_filter.create_filter(
-                pool_filter_expression, pools, pool_filter_cols),
-            rule_filter.create_filter(
+        event_filter = self.rule_filter.merge_filters(
+            self.rule_filter.create_filter(
+                expression=pool_filter_expression,
+                data=pools,
+                columns=pool_filter_cols),
+            self.rule_filter.create_filter(
                 expression=state_filter_expression,
                 data=state_variables,
                 columns=state_filter_cols),
@@ -91,8 +93,8 @@ class SITEventProcessor():
                 classifiers))
 
         _classifiers, _inventory, _pools, _state_variables = \
-            event_processor.process_event(
-                filter_evaluator=rule_filter.evaluate_filter,
+            self.event_processor.process_event(
+                filter_evaluator=self.rule_filter.evaluate_filter,
                 event_filter=event_filter,
                 undisturbed=eligible,
                 target_func=target_factory,
@@ -100,3 +102,17 @@ class SITEventProcessor():
                 inventory=inventory,
                 pools=pools,
                 state_variables=state_variables)
+
+
+    def process_events(self, time_step, sit_events, classifiers, inventory,
+                       pools, state_variables, on_unrealized):
+        for sit_event in event_iterator(time_step, sit_events):
+            _classifiers, _inventory, _pools, _state_variables = \
+                self.process_event(
+                    eligible,
+                    sit_event,
+                    classifiers,
+                    inventory,
+                    pools,
+                    state_variables,
+                    on_unrealized)
