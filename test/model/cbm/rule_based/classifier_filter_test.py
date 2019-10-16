d421c5fb068cefda4d2cf98cb31f409943b6e650
Scott Morken <smorken@nrcan.gc.ca>
2019-10-16 10:40:52 -0700

    add test for wild card classifier set

--- test/model/cbm/rule_based/classifier_filter_test.py
+++ test/model/cbm/rule_based/classifier_filter_test.py
@@ -47,14 +47,16 @@ class ClassifierFilterTest(unittest.TestCase):
         c3 = get_classifier_value_index(3)
 
         classifier_values = pd.DataFrame(
-            [(c1[x[0]], c2[x[1]], c3[x[2]])
-             for x in [
-                ("c1_v1", "c2_v1", "c3_v3"),  # match
-                ("c1_v2", "c2_v1", "c3_v3"),  # non-match (c1_v2)
-                ("c1_v1", "c2_v2", "c3_v3"),  # match
-                ("c1_v1", "c2_v2", "c3_v1"),  # match
-                ("c1_v1", "c2_v2", "c3_v2"),  # non-match (aggregate)
-             ]], columns=["c1", "c2", "c3"])
+            [
+                (c1[x[0]], c2[x[1]], c3[x[2]])
+                for x in [
+                    ("c1_v1", "c2_v1", "c3_v3"),  # match
+                    ("c1_v2", "c2_v1", "c3_v3"),  # non-match (c1_v2)
+                    ("c1_v1", "c2_v2", "c3_v3"),  # match
+                    ("c1_v1", "c2_v2", "c3_v1"),  # match
+                    ("c1_v1", "c2_v2", "c3_v2")]  # non-match (aggregate)
+            ],
+            columns=["c1", "c2", "c3"])
 
         result = rule_filter.create_classifiers_filter(
             classifier_set, classifier_values)
@@ -63,6 +65,46 @@ class ClassifierFilterTest(unittest.TestCase):
         self.assertTrue(list(result.local_dict["c_0"]) == [1, 2, 1, 1, 1])
         self.assertTrue(list(result.local_dict["c_1"]) == [3, 3, 4, 4, 4])
         self.assertTrue(list(result.local_dict["c_2"]) == [7, 7, 7, 5, 6])
+
+    def test_create_classifiers_filter_expected_value_all_wildcards(self):
+
+        classifier_set = ["?", "?", "?"]
+        classifiers_config = get_mock_classifiers_config()
+        classifier_aggregates = [
+            {'classifier_id': 3,
+             'name': 'agg1',
+             'description': 'agg1',
+             'classifier_values': ['c3_v1', 'c3_v3']}]
+
+        rule_filter = ClassifierFilter(
+            classifiers_config, classifier_aggregates)
+
+        def get_classifier_value_index(classifier_id):
+            return {
+                x["name"]: x["id"] for x
+                in classifiers_config["classifier_values"]
+                if x["classifier_id"] == classifier_id}
+
+        c1 = get_classifier_value_index(1)
+        c2 = get_classifier_value_index(2)
+        c3 = get_classifier_value_index(3)
+
+        classifier_values = pd.DataFrame(
+            [
+                (c1[x[0]], c2[x[1]], c3[x[2]])
+                for x in [
+                    ("c1_v1", "c2_v1", "c3_v3"),  # match
+                    ("c1_v2", "c2_v1", "c3_v3"),  # non-match (c1_v2)
+                    ("c1_v1", "c2_v2", "c3_v3"),  # match
+                    ("c1_v1", "c2_v2", "c3_v1"),  # match
+                    ("c1_v1", "c2_v2", "c3_v2")]  # non-match (aggregate)
+            ],
+            columns=["c1", "c2", "c3"])
+
+        result = rule_filter.create_classifiers_filter(
+            classifier_set, classifier_values)
+        self.assertTrue(result.expression == "")
+        self.assertTrue(result.local_dict == {})
 
     def test_error_on_mismatching_classifiers(self):
         """check that an error is raised on mismatch in the number of
