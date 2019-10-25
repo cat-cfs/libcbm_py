f651f33876c89bbdb209329e22ff86116ac31226
Scott Morken <smorken@nrcan.gc.ca>
2019-10-25 14:35:32 -0700

    transition rule processor layout

--- libcbm/model/cbm/rule_based/transition_rule_processor.py
+++ libcbm/model/cbm/rule_based/transition_rule_processor.py
@@ -27,13 +27,24 @@ def create_split_proportions(tr_group_key, tr_group, group_error_max):
 
 class TransitionRuleProcessor(object):
 
-    def __init__(self, classifier_filter_builder):
+    def __init__(self, classifier_filter_builder, classifiers_config,
+                 grouped_percent_err_max):
         self.classifier_filter_builder = classifier_filter_builder
+        self.grouped_percent_err_max = grouped_percent_err_max
+        self.classifiers_config = classifiers_config
+        self.classifier_names = [
+            x["name"] for x in self.classifiers_config["classifiers"]]
+        self.classifier_value_lookup = {
+            x["name"]: self._get_classifier_value_index(x["id"])
+            for x in self.classifiers_config["classifiers"]}
 
-    def apply_transition_rule(self, tr_group_key, tr_group, transitioned,
-                              disturbance_type, classifiers, inventory, pools,
-                              state_variables):
+    def _get_classifier_value_index(self, classifier_id):
+        return {
+            x["value"]: x["id"] for x
+            in self.classifiers_config["classifier_values"]
+            if x["classifier_id"] == classifier_id}
 
+    def filter_stands(self, tr_group_key, inventory, disturbance_type):
         state_filter_expression, state_filter_cols = \
             sit_stand_filter.create_state_filter_expression(
                 tr_group_key, True)
@@ -55,6 +66,14 @@ class TransitionRuleProcessor(object):
                 columns=["disturbance_type"]))
 
         filter_result = rule_filter.evaluate_filter(tr_filter)
+        return filter_result
+
+    def apply_transition_rule(self, tr_group_key, tr_group, transitioned,
+                              disturbance_type, classifiers, inventory, pools,
+                              state_variables):
+
+        filter_result = self.filter_stands(
+            tr_group_key, inventory, disturbance_type)
 
         if np.logical_and(transitioned, filter_result).any():
             # this indicates that a transition rule has collided with another
@@ -62,11 +81,42 @@ class TransitionRuleProcessor(object):
             # specified (wildcards, age ranges etc.)  This is a simplistic,
             # but safe solution for this possible issue. Another approach might
             # be to prioritize overlapping groups instead.
-            raise ValueError("overlapping transition rule criteria detected: "
-                             f"{tr_group_key}")
+            raise ValueError(
+                "overlapping transition rule criteria detected: "
+                f"{tr_group_key}")
 
         # sets the transitioned array with the transition filter result
         transitioned = np.logical_or(transitioned, filter_result)
 
-        proportions = create_split_proportions(tr_group_key, tr_group)
+        proportions = create_split_proportions(
+            tr_group_key, tr_group, self.grouped_percent_err_max)
 
+        transitioned_classifier_rows = classifiers[filter_result].copy()
+        transition_classifier_append_result = pd.DataFrame()
+        for i_proportion, proportion in enumerate(proportions):
+            if i_proportion == 0:
+                # for the first index use the existing matched inventory record
+                self.transition_classifier_set(
+                    transition_rule=tr_group.iloc[i_proportion],
+                    filter_result=filter_result,
+                    classifiers=classifiers)
+            elif i_proportion == tr_group.shape[0]:
+                # if a proportion was created for the 100-grouped percentage
+                # remainder, then make a copy with no transition
+                transition_classifier_append_result = \
+                    transition_classifier_append_result.append(
+                        transitioned_classifier_rows)
+            else:
+                transition_copy_rows = transitioned_classifier_rows.copy()
+
+    def get_transition_classifier_set(self, transition_rule):
+        result = {}
+        for classifier_name in self.classifier_names:
+            transition_classifier_value = transition_rule[
+                classifier_name + "_tr"]
+            if transition_classifier_value == "?":
+                continue
+            transition_id = self.classifier_value_lookup[
+                classifier_name][transition_classifier_value]
+            result[classifier_name] = transition_id
+        return result
