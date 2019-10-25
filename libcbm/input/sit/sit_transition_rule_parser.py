c454ad9700588bdb8ed30c8bad9e1670afd9950e
Scott Morken <smorken@nrcan.gc.ca>
2019-10-24 17:15:15 -0700

    drop error margin on max sum of grouped transition rule percentage

--- libcbm/input/sit/sit_transition_rule_parser.py
+++ libcbm/input/sit/sit_transition_rule_parser.py
@@ -116,7 +116,7 @@ def parse(transition_rules, classifiers, classifier_values,
     group_cols = list(classifiers.name) + \
         ["min_age", "max_age", "disturbance_type"]
     grouped = transitions[group_cols + ["percent"]].groupby(group_cols).sum()
-    invalid_grouped = grouped[grouped.percent > 100.0001]
+    invalid_grouped = grouped[grouped.percent > 100]
     if len(invalid_grouped) > 0:
         invalid_percents = [x.Index for x in grouped.head().itertuples()]
         raise ValueError(
