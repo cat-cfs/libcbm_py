4cb525c2194ba8ba17c2bbce0163c5dc65836962
Scott Morken <smorken@nrcan.gc.ca>
2019-10-30 17:04:49 -0700

    test for SITTransitionRuleProcessor

--- test/model/cbm/rule_based/sit/sit_transition_rule_processor_test.py
+++ test/model/cbm/rule_based/sit/sit_transition_rule_processor_test.py
@@ -4,7 +4,8 @@ from unittest.mock import patch
 from types import SimpleNamespace
 import pandas as pd
 from libcbm.model.cbm.rule_based.sit import sit_transition_rule_processor
-
+from libcbm.model.cbm.rule_based.sit.sit_transition_rule_processor \
+    import SITTransitionRuleProcessor
 # used in patching (overriding) module imports in the module being tested
 PATCH_PATH = "libcbm.model.cbm.rule_based.sit.sit_transition_rule_processor"
 
@@ -83,7 +84,92 @@ class SITTransitionRuleProcessorTest(unittest.TestCase):
                 "min_age": 0,
                 "max_age": 10,
                 "disturbance_type_id": 1})
+        self.assertTrue(
+            list_result[0][1].equals(mock_sit_transitions.iloc[[0,1]])
+        )
 
+        self.assertTrue(
+            list_result[1][0] == {
+                "c1": "a2",
+                "c2": "b2",
+                "min_age": 1,
+                "max_age": 100,
+                "disturbance_type_id": 2})
+        self.assertTrue(
+            list_result[1][1].equals(mock_sit_transitions.iloc[[2,3]])
+        )
+
+        self.assertTrue(
+            list_result[2][0] == {
+                "c1": "a3",
+                "c2": "b3",
+                "min_age": 2,
+                "max_age": 5,
+                "disturbance_type_id": 3})
+        self.assertTrue(
+            list_result[2][1].equals(mock_sit_transitions.iloc[[4]])
+        )
 
     def test_sit_transition_rule_iterator_error(self):
-        pass
+        """check that an error is raised if a grouped set of
+        transition rules has a percent sum > 100
+        """
+        mock_sit_transitions = pd.DataFrame({
+            "c1": ["a1", "a1"],
+            "c2": ["b1", "b1"],
+            "min_age": [0, 0],
+            "max_age": [10, 10],
+            "disturbance_type_id": [1, 1],
+            "reset_age": [1, 2],
+            "percent": [51, 50]
+        })
+        classifier_names = ["c1", "c2"]
+        with self.assertRaises(ValueError):
+            list(
+                sit_transition_rule_processor.sit_transition_rule_iterator(
+                    mock_sit_transitions, classifier_names))
+
+    def test_process_transition_rules(self):
+
+        mock_cbm_vars = SimpleNamespace(
+            classifiers=pd.DataFrame({"c1": [1], "c2": [1]}))
+
+        mock_transition_rule_processor = Mock()
+        mock_apply_transition_rule = Mock()
+        mock_transition_rule_processor.apply_transition_rule = \
+            mock_apply_transition_rule
+
+
+        def test_apply_transition_rule(tr_group_key, tr_group,
+                                       transition_mask, cbm_vars):
+            self.assertTrue(
+                tr_group_key == {
+                    "c1": "a1",
+                    "c2": "b1",
+                    "min_age": 0,
+                    "max_age": 10,
+                    "disturbance_type_id": 1})
+            self.assertTrue(tr_group.equals(mock_sit_transitions))
+            self.assertTrue(list(transition_mask) == [False])
+            self.assertTrue(
+                cbm_vars.classifiers.equals(mock_cbm_vars.classifiers))
+
+            return "mock_mask", "mock_cbm_vars_result"
+
+        mock_apply_transition_rule.side_effect = test_apply_transition_rule
+        s = SITTransitionRuleProcessor(mock_transition_rule_processor)
+        mock_sit_transitions = pd.DataFrame({
+            "c1": ["a1", "a1"],
+            "c2": ["b1", "b1"],
+            "min_age": [0, 0],
+            "max_age": [10, 10],
+            "disturbance_type_id": [1, 1],
+            "reset_age": [1, 2],
+            "percent": [50, 50]
+        })
+
+        cbm_vars_result = \
+            s.process_transition_rules(
+                mock_sit_transitions, mock_cbm_vars)
+        self.assertTrue(cbm_vars_result == "mock_cbm_vars_result")
+        mock_apply_transition_rule.assert_called_once()
