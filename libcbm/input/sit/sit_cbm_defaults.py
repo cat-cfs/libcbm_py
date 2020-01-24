45b21ed081e7f225fe476db28b66c29baee300db
Scott Morken <smorken@nrcan.gc.ca>
2020-01-24 15:19:47 -0800

    start of sit_cbm_defaults

--- libcbm/input/sit/sit_cbm_defaults.py
+++ libcbm/input/sit/sit_cbm_defaults.py
@@ -1,10 +1,15 @@
+import numpy as np
 
 
 class SITCBMDefaults():
 
-    def __init__(self, cbm_defaults, cbm_defaults_ref, sit_disturbance_types):
+    def __init__(self, sit, cbm_defaults, cbm_defaults_ref):
+        self.sit = sit
         self.cbm_defaults = cbm_defaults
         self.cbm_defaults_ref = cbm_defaults_ref
+        self.sit_disturbance_type_id_lookup = {
+            x["name"]: x["sit_disturbance_type_id"]
+            for _, x in sit.sit_data.disturbance_types.iterrows()}
 
     def get_afforestation_pre_types(self):
         return self.cbm_defaults_ref.get_afforestation_pre_types()
@@ -13,19 +18,19 @@ class SITCBMDefaults():
         return self.cbm_defaults_ref.get_species_id(species_name)
 
     def get_species(self):
-        pass
+        return self.cbm_defaults.get_species()
 
     def get_spatial_unit_id(self, admin, eco):
-        pass
+        return self.cbm_defaults_ref.get_spatial_unit_id(admin, eco)
 
     def get_spatial_unit(self, default_spuid):
-        pass
+        return self.cbm_defaults_ref.get_spatial_unit(default_spuid)
 
     def get_land_classes(self):
-        pass
+        return self.cbm_defaults.get_land_classes()
 
-    def get_disturbance_type_id(self):
-        pass
+    def get_sit_disturbance_type_id(self):
+        return
 
     def get_configuration_factory(self):
         pass
