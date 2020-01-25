df28f032550d4f696acc15c0105c042e1f8b74c8
Scott Morken <smorken@nrcan.gc.ca>
2020-01-24 16:30:51 -0800

    clean up

--- libcbm/input/sit/sit_cbm_defaults.py
+++ libcbm/input/sit/sit_cbm_defaults.py
@@ -1,37 +1,20 @@
-import numpy as np
+from libcbm.model.cbm.cbm_defaults_reference import CBMDefaultsReference
+from libcbm.model.cbm import cbm_defaults
 
 
-class SITCBMDefaults():
+class SITCBMDefaults(CBMDefaultsReference):
 
-    def __init__(self, sit, cbm_defaults, cbm_defaults_ref):
+    def __init__(self, sit, db_path, locale_code="en-CA"):
+        super().__init__(db_path, locale_code)
         self.sit = sit
-        self.cbm_defaults = cbm_defaults
-        self.cbm_defaults_ref = cbm_defaults_ref
+        self.db_path = db_path
         self.sit_disturbance_type_id_lookup = {
             x["name"]: x["sit_disturbance_type_id"]
             for _, x in sit.sit_data.disturbance_types.iterrows()}
         self.default_disturbance_id_lookup = {
-            x["name"]: x[]
-            for x in self.cbm_defaults_ref.get_disturbance_types()
+            x["name"]: x["id"]
+            for x in self.get_disturbance_types()
         }
-
-    def get_afforestation_pre_types(self):
-        return self.cbm_defaults_ref.get_afforestation_pre_types()
-
-    def get_species_id(self, species_name):
-        return self.cbm_defaults_ref.get_species_id(species_name)
-
-    def get_species(self):
-        return self.cbm_defaults.get_species()
-
-    def get_spatial_unit_id(self, admin, eco):
-        return self.cbm_defaults_ref.get_spatial_unit_id(admin, eco)
-
-    def get_spatial_unit(self, default_spuid):
-        return self.cbm_defaults_ref.get_spatial_unit(default_spuid)
-
-    def get_land_classes(self):
-        return self.cbm_defaults.get_land_classes()
 
     def get_sit_disturbance_type_id(self, sit_dist_type_name):
         if sit_dist_type_name in self.sit_disturbance_type_id_lookup:
@@ -50,7 +33,7 @@ class SITCBMDefaults():
                 f"{default_dist_type_name} not found.")
 
     def get_configuration_factory(self):
-        pass
+        return cbm_defaults.get_libcbm_configuration_factory(self.db_path)
 
     def get_parameters_factory(self):
-        pass
+        return cbm_defaults.get_cbm_parameters_factory(self.db_path)
