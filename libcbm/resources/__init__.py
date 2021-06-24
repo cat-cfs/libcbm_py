a329dcd0035ffef6cc908d1a5eaf9ecefbdd7623
Scott Morken <scottm361@gmail.com>
2021-06-24 16:34:11 -0700

    update resources to handle both ubuntu 18.04 and 20.04

--- libcbm/resources/__init__.py
+++ libcbm/resources/__init__.py
@@ -4,8 +4,8 @@
 
 import os
 import sys
-import platform
 import warnings
+import platform
 
 
 def get_local_dir():
@@ -31,6 +31,20 @@ def get_test_resources_dir():
     return os.path.join(get_local_dir(), "test")
 
 
+def get_linux_os_release():
+    path = "/etc/os-release"
+    if not os.path.exists(path):
+        return None
+    with open(path) as fp:
+        d = {}
+        for line in fp:
+            k, v = line.rstrip().split('=')
+            if v.startswith('"'):
+                v = v[1:-1]
+            d[k] = v
+        return d
+            
+
 def get_libcbm_bin_path():
     """Returns bundled, OS specific compiled libcbm C/C++ library
 
@@ -41,19 +55,32 @@ def get_libcbm_bin_path():
     Returns:
         str: path to the bundled, compiled dll or so file
     """
+
     local_dir = get_local_dir()
     if sys.maxsize <= 2**32:
         raise RuntimeError("32 bit python not supported")
     system = platform.system()
-    if system == "Windows":
+    if os.name == "nt":
         return os.path.join(
             local_dir, "libcbm_bin", "win_x86_64", "libcbm.dll")
-    elif system == "Linux":
-        plat = platform.platform()
-        if "Ubuntu" not in plat or "18.04" not in plat:
+    elif os.name == "posix":
+        os_release = get_linux_os_release()
+        if not os_release:
+            raise RuntimeError("unsupported platform")
+        version_id = os_release["VERSION_ID"]
+        os_name = os_release["NAME"].lower()
+        if os_name == "ubuntu" and version_id == "18.04":
+            return os.path.join(
+                local_dir, "libcbm_bin", "ubuntu_18_04_x86_64", "libcbm.so")
+        elif os_name == "ubuntu" and version_id == "20.04":
+            return os.path.join(
+                local_dir, "libcbm_bin", "ubuntu_20_04_x86_64", "libcbm.so")
+        elif platform.system().lower() == "linux":
             message = f"unsupported linux distribution: {plat}"
             warnings.warn(message, RuntimeWarning)
-        return os.path.join(
-            local_dir, "libcbm_bin", "ubuntu_18_04_x86_64", "libcbm.so")
+            return os.path.join(
+                local_dir, "libcbm_bin", "ubuntu_18_04_x86_64", "libcbm.so")
+        else:
+            raise RuntimeError("unsupported platform")
     else:
         raise RuntimeError("unsupported platform")
