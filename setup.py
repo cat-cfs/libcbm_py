c376e9528da90cd3a5843be6e5132ce624508c35
Scott Morken <scottm361@gmail.com>
2021-07-15 16:01:49 -0700

    new ver: 0.4.2 update examples

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ cbm_defaults_db = [
               "cbm_defaults_2020.db"]]
 
 cbm_defaults_queries = [
-    os.path.join("resources", "cbm_defaults_queries", "*.sql")
+    os.path.join(resources_dir, "cbm_defaults_queries", "*.sql")
 ]
 
 win_x86_64_bin = [
@@ -31,27 +31,18 @@ ubuntu_18_04_x86_64_bin = [
     for x in ["cbm.a", "core.a", "libcbm.so", "volume_to_biomass.a"]
 ]
 
+test_resources = []
+for x in ["cbm3_tutorial2", "cbm3_tutorial6", "sit_rule_based_events"]:
+    test_resources.append(os.path.join(resources_dir, "test", x, "*.csv"))
+    test_resources.append(os.path.join(resources_dir, "test", x, "*.json"))
 
-test_resources = [
-    os.path.join(resources_dir, "test", "cbm3_tutorial2", x)
-    for x in [
-        "age_classes.csv", "classifiers.csv", "disturbance_events.csv",
-        "disturbance_types.csv", "growth_and_yield.csv", "inventory.csv",
-        "sit_config.json", "transition_rules.csv"]
-] + [
-    os.path.join(resources_dir, "test", "sit_rule_based_events", x)
-    for x in [
-        "sit_age_classes.csv", "sit_classifiers.csv", "sit_events.csv",
-        "sit_disturbance_types.csv", "sit_yield.csv", "sit_inventory.csv",
-        "sit_config.json", "sit_transition_rules.csv"]
-]
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name="libcbm",
-    version="0.4.2",
+    version="0.4.3",
     description="Carbon budget model library based on CBM-CFS3",
     keywords=["cbm-cfs3"],
     long_description=long_description,
