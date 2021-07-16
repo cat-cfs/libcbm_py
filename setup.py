431fb4164bd385f842f25e4b453487e576f6e413
Scott Morken <scottm361@gmail.com>
2021-07-16 16:06:41 -0700

    new version 0.4.7 simplified handing of CBM parameters

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="libcbm",
-    version="0.4.3",
+    version="0.4.7",
     description="Carbon budget model library based on CBM-CFS3",
     keywords=["cbm-cfs3"],
     long_description=long_description,
