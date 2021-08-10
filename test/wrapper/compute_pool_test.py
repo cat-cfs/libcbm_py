37b0077344870c5a06cb49d2b9e6b21213afe6c6
Scott Morken <scottm361@gmail.com>
2021-08-10 16:28:10 -0700

    testing new method

--- test/wrapper/compute_pool_test.py
+++ test/wrapper/compute_pool_test.py
@@ -171,3 +171,29 @@ class ComputePoolTests(unittest.TestCase):
         pools_expected = pools_working
         self.assertTrue(
             np.allclose(pools_expected, pools_test, rtol=1e-12, atol=1e-15))
+
+    def test_set_op_repeating(self):
+        pools = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
+        pooldef = pool_flux_helpers.create_pools(list(pools.keys()))
+        dll = pool_flux_helpers.load_dll({
+            "pools": pooldef,
+            "flux_indicators": []
+        })
+        n_stands = 10
+        pools_array = np.ones(shape=(n_stands, len(pools)))
+
+        matrices = [
+            [pools["a"], pools["a"], np.array([1.0, 1.0, 1.0])],
+            [pools["a"], pools["b"], np.array([1.5, 2.0, 3.0])],
+            [pools["a"], pools["c"], np.array([3.0, 2.0, 1.0])],
+            [pools["c"], pools["d"], np.array([1.0, 2.0, 1.0])],
+            [pools["d"], pools["d"], np.array([1.0, 2.0, 1.0])]
+        ]
+        coords = np.array([[x[0], x[1]] for x in matrices], dtype=int)
+        values = np.column_stack([x[2] for x in matrices])
+        matrix_index = \
+            np.array([0, 1, 2, 0, 1, 2, 0, 1, 2, 0], dtype=np.uint64)
+        op_id = dll.allocate_op(n_stands)
+        dll.set_op_repeating(op_id, coords, values, matrix_index)
+        dll.compute_pools(np.array([op_id]), pools_array)
+        self.assertTrue(pools)
