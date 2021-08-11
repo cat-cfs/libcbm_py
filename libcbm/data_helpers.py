0fad653eb3be9ed6260459dfca309bebeae1c4b0
Scott Morken <scottm361@gmail.com>
2021-08-11 16:01:19 -0700

    use np.full rather than multiplying by value

--- libcbm/data_helpers.py
+++ libcbm/data_helpers.py
@@ -126,7 +126,7 @@ def promote_scalar(value, size, dtype):
     elif isinstance(value, np.ndarray):
         return value
     else:
-        return np.ones(size, dtype=dtype) * value
+        return np.full(value, shape=size, dtype=dtype)
 
 
 def append_simulation_result(simulation_result, timestep_data, timestep):
