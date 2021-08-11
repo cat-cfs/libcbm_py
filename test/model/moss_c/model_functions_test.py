9222ef62d9e640d401c87ff780b0bb930b18d7f9
Scott Morken <scottm361@gmail.com>
2021-08-11 11:32:41 -0700

    remove expand matrix function

--- test/model/moss_c/model_functions_test.py
+++ test/model/moss_c/model_functions_test.py
@@ -10,60 +10,6 @@ from libcbm.model.moss_c.pools import Pool
 
 class ModelFunctionsTest(unittest.TestCase):
 
-    def test_expand_matrix(self):
-        result = model_functions.expand_matrix(mat=[
-            [1,  2,   np.array([3,   4,  5])],
-            [6,  7,   np.array([8,   9, 10])],
-            [11, 12,  np.array([13, 14, 15])],
-        ], identity_set={})
-
-        expected_output = [
-            np.array([
-                [1, 2, 3],
-                [6, 7, 8],
-                [11, 12, 13]], dtype=float),
-            np.array([
-                [1, 2, 4],
-                [6, 7, 9],
-                [11, 12, 14]], dtype=float),
-            np.array([
-                [1, 2, 5],
-                [6, 7, 10],
-                [11, 12, 15]], dtype=float),
-        ]
-        for i_mat, mat in enumerate(result):
-            self.assertTrue((mat == expected_output[i_mat]).all())
-
-    def test_expand_matrix_identity_set(self):
-        result = model_functions.expand_matrix(mat=[
-            [1,  1,   np.array([3,   4,  5])],
-            [6,  7,   np.array([8,   9, 10])],
-            [11, 12,  np.array([13, 14, 15])],
-        ], identity_set={1, 2, 3})
-
-        expected_output = [
-            np.array([
-                [2, 2, 1],
-                [3, 3, 1],
-                [1, 1, 3],
-                [6, 7, 8],
-                [11, 12, 13]], dtype=float),
-            np.array([
-                [2, 2, 1],
-                [3, 3, 1],
-                [1, 1, 4],
-                [6, 7, 9],
-                [11, 12, 14]], dtype=float),
-            np.array([
-                [2, 2, 1],
-                [3, 3, 1],
-                [1, 1, 5],
-                [6, 7, 10],
-                [11, 12, 15]], dtype=float),
-        ]
-        for i_mat, mat in enumerate(result):
-            self.assertTrue((mat == expected_output[i_mat]).all())
-
     def test_compute_with_pools_only(self):
         dll = Mock()
         dll.allocate_op.side_effect = lambda _: 12345
