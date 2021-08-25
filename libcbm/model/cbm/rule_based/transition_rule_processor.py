36f5cdf93d0459685f82d77234fb8390c23d45c3
Scott Morken <scottm361@gmail.com>
2021-08-25 13:13:52 -0700

    changed format for multiple filters

--- libcbm/model/cbm/rule_based/transition_rule_processor.py
+++ libcbm/model/cbm/rule_based/transition_rule_processor.py
@@ -76,7 +76,7 @@ class TransitionRuleProcessor(object):
         classifier_set = [
             tr_group_key[x]
             for x in cbm_vars.classifiers.columns.tolist()]
-        tr_filter = rule_filter.merge_filters(
+        tr_filters = [
             self.state_variable_filter_func(tr_group_key, cbm_vars.state),
             self.classifier_filter_builder.create_classifiers_filter(
                 classifier_set,
@@ -84,9 +84,9 @@ class TransitionRuleProcessor(object):
             rule_filter.create_filter(
                 expression=f"(disturbance_type_id == {dist_type_target})",
                 data={"disturbance_type_id": cbm_vars.params.disturbance_type},
-                columns=["disturbance_type_id"]))
+                columns=["disturbance_type_id"])]
 
-        filter_result = rule_filter.evaluate_filter(tr_filter)
+        filter_result = rule_filter.evaluate_filters(tr_filters)
         return filter_result
 
     def _get_transition_classifier_set(self, transition_rule):
