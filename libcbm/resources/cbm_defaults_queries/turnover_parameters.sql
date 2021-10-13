9a7c6e8d62879adf7088508063c64f0aa2f764f8
Scott <scottm361@gmail.com>
2021-10-13 14:34:28 -0700

    query sw/hw parameters separately

--- libcbm/resources/cbm_defaults_queries/turnover_parameters.sql
+++ libcbm/resources/cbm_defaults_queries/turnover_parameters.sql
@@ -9,8 +9,10 @@ turnover_parameter.coarse_root as CoarseRootTurnProp,
 turnover_parameter.fine_ag_split as FineRootAGSplit,
 turnover_parameter.fine_root as FineRootTurnProp,
 turnover_parameter.branch_snag_split as OtherToBranchSnagSplit,
-turnover_parameter.branch_snag as BranchSnagTurnoverRate,
-turnover_parameter.stem_snag as StemSnagTurnoverRate
+turnover_parameter.sw_branch_snag as SoftwoodBranchSnagTurnoverRate,
+turnover_parameter.sw_stem_snag as SoftwoodStemSnagTurnoverRate,
+turnover_parameter.hw_branch_snag as HardwoodBranchSnagTurnoverRate,
+turnover_parameter.hw_stem_snag as HardwoodStemSnagTurnoverRate
 from eco_boundary
 inner join turnover_parameter on
 eco_boundary.turnover_parameter_id = turnover_parameter.id;
