b9f8da71f090986fb8b9c593cde7cb06f21dbb39
Scott <scottm361@gmail.com>
2021-10-19 12:44:54 -0700

    improve integration test

--- test/model/cbm/integration_test.py
+++ test/model/cbm/integration_test.py
@@ -8,11 +8,13 @@ from libcbm.model.cbm.stand_cbm_factory import StandCBMFactory
 def test_integration():
 
     classifiers = {
-        "c1": ["c1_v1"]
+        "c1": ["c1_v1"],
+        "c2": ["c2_v1"],
+
     }
     merch_volumes = [
         {
-            "classifier_set": ["c1_v1"],
+            "classifier_set": ["c1_v1", "?"],
             "merch_volumes": [{
                 "species": "Spruce",
                 "age_volume_pairs": [
@@ -25,19 +27,21 @@ def test_integration():
         }
     ]
 
-    inventory = pd.DataFrame(
-        columns=[
-            "c1", "admin_boundary", "eco_boundary", "age", "area", "delay",
-            "landclass", "afforestation_pre_type",
-            "historic_disturbance_type", "last_pass_disturbance_type"],
-        data=[
-            ["c1_v1", "British Columbia", "Pacific Maritime", 15, 1.0, 0,
-             "UNFCCC_FL_FL", None, "Wildfire", "Wildfire"]])
-
     cbm_factory = StandCBMFactory(classifiers, merch_volumes)
 
-    csets, inv = cbm_factory.prepare_inventory(inventory)
     n_steps = 10
+    return_interval = 50
+    n_rotations = 5
+    age = 15
+    inventory = pd.DataFrame(
+        columns=[
+            "c1", "c2", "admin_boundary", "eco_boundary", "age", "area",
+            "delay", "land_class", "afforestation_pre_type",
+            "historic_disturbance_type", "last_pass_disturbance_type"],
+        data=[
+            ["c1_v1", "c2_v1", "British Columbia", "Pacific Maritime", age,
+             1.0, 0, "UNFCCC_FL_R_FL", None, "Wildfire", "Wildfire"]])
+    csets, inv = cbm_factory.prepare_inventory(inventory)
     n_stands = len(inv.index)
     with cbm_factory.initialize_cbm() as cbm:
 
@@ -45,7 +49,8 @@ def test_integration():
             cbm_simulator.create_in_memory_reporting_func(density=True)
         cbm_results, cbm_reporting_func = \
             cbm_simulator.create_in_memory_reporting_func(
-                classifier_map={1: "c1_v1"}, disturbance_type_map={1: "fires"})
+                classifier_map=cbm_factory.get_classifier_map(),
+                disturbance_type_map=cbm_factory.get_disturbance_type_map())
         cbm_simulator.simulate(
             cbm,
             n_steps=n_steps,
@@ -54,6 +59,9 @@ def test_integration():
             pre_dynamics_func=lambda t, cbm_vars: cbm_vars,
             reporting_func=cbm_reporting_func,
             spinup_params=cbm_variables.initialize_spinup_parameters(
-                n_stands, 50, 5, 5, -1),
+                n_stands, return_interval, n_rotations, n_rotations, -1),
             spinup_reporting_func=spinup_reporting_func)
         assert len(cbm_results.pools.index) == (n_steps + 1) * n_stands
+        assert (
+            len(spinup_results.pools.index) ==
+            (n_rotations * return_interval) + age - 1)
