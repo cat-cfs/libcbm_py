aee35bb54f83c6fdff8db1ca4ea923f9356689cb
Scott <scottm361@gmail.com>
2021-10-21 14:10:19 -0700

    convert example subdirs as well

--- convert_example_notebooks.py
+++ convert_example_notebooks.py
@@ -1,13 +1,12 @@
 import os
 import jupytext
-from glob import glob
+from pathlib import Path
 
 EXAMPLE_DIR = os.path.join(".", "examples")
-GLOB_PATTERN = os.path.abspath(os.path.join(EXAMPLE_DIR, "*.md"))
 
 
-for notebook_md_file in glob(GLOB_PATTERN):
+for notebook_md_file in Path(EXAMPLE_DIR).rglob("*.md"):
     md_notebook = jupytext.read(notebook_md_file)
     path_without_ext = os.path.splitext(notebook_md_file)[0]
-    nb_out_path = os.path.join(EXAMPLE_DIR, f"{path_without_ext}.ipynb")
+    nb_out_path = f"{path_without_ext}.ipynb"
     jupytext.write(md_notebook, nb_out_path)
