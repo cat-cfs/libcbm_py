c95b4b9d4383f40d9e690a0b2e0525768519048d
Scott <scottm361@gmail.com>
2021-10-21 13:36:30 -0700

    run the example notebooks

--- generate_coverage.bat
+++ generate_coverage.bat
@@ -1,5 +1,9 @@
 :: note to include numba routines in coverage, set disable it with the option
-:: DISABLE_JIT: True
-:: in the .numba_config.yml file in this dir
-pytest --cov=libcbm --cov-report html:cov_html
+SET DISABLE_JIT=True
+:: can also be done in the .numba_config.yml file in this dir
+pytest --cov=libcbm --cov-report html
+python ./convert_example_notebooks.py
+:: make the notebooks look at the local dir
+SET PYTHONPATH=%CD%
+pytest --nbval-lax ./examples --cov=libcbm --cov-report html  --cov-append
 coverage-badge -o coverage.svg -f
