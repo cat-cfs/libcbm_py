cbce57a6bac614232ac3115a74f060bd88f2ab18
Scott <scottm361@gmail.com>
2021-10-21 12:33:46 -0700

    extend create_sit_rule_based_processor can now specify sit formatted events or transition rules to make alternate rule based processors

--- libcbm/input/sit/sit_cbm_factory.py
+++ libcbm/input/sit/sit_cbm_factory.py
@@ -218,7 +218,7 @@ def initialize_inventory(sit):
     return classifiers_result, inventory_result
 
 
-def initialize_events(disturbance_events, sit_mapping):
+def _initialize_events(disturbance_events, sit_mapping):
     """Returns a copy of the parsed sit events with the disturbance type id
     resulting from the SIT configuration.
 
@@ -241,7 +241,7 @@ def initialize_events(disturbance_events, sit_mapping):
     return disturbance_events
 
 
-def initialize_transition_rules(sit):
+def _initialize_transition_rules(transition_rules, sit_mapping):
     """Returns a copy of the parsed sit transition rules with the disturbance
     type id resulting from the SIT configuration.
 
@@ -252,11 +252,11 @@ def initialize_transition_rules(sit):
         pandas.DataFrame: the transition rules with an added
             "disturbance_type_id" column.
     """
-    if sit.sit_data.transition_rules is None:
+    if transition_rules is None:
         return None
-    transition_rules = sit.sit_data.transition_rules.copy()
+    transition_rules = transition_rules.copy()
     transition_rules["disturbance_type_id"] = \
-        sit.sit_mapping.get_sit_disturbance_type_id(
+        sit_mapping.get_sit_disturbance_type_id(
             transition_rules.disturbance_type)
     return transition_rules
 
@@ -383,14 +383,70 @@ def create_sit_rule_based_processor(
         random_func (func, optional): A function of a single integer that
             returns a numeric 1d array whose length is the integer argument.
             Defaults to np.random.rand.
+        sit_events (pandas.DataFrame, optional): if specified the returned rule
+            base processor is based on the specified sit_events input.  The
+            value will be parsed and validated (sit_classifiers,
+            sit_disturbance_type etc.) based on the values in the specified sit
+            object.
+        sit_disturbance_eligibilities (pandas.DataFrame, optional): SIT
+            formatted disturbance eligibilities. Cannot be specified without
+            also specified sit_events using the disturbance-eligibility
+            formatting. Defaults to None.
+        sit_transition_rules (pandas.DataFrame, optional):  if specified the
+            returned rule base processor is based on  the specified
+            sit_transition_rules input.  The value will be parsed and validated
+            (sit_classifiers, sit_disturbance_type etc.) based on the values in
+            the specified sit object. Note if the sit_disturbance_events
+            parameter is set, but this parameter is not set the
+            transition_rules (if any) attached to the specified sit object will
+            be used by default.  If null transition rules are required with
+            non-null sit_events set this parameter to a dataframe with zero
+            rows `pandas.DataFrame()`.  Defaults to None.
+
+    Raises:
+        ValueError: cannot specify sit_disturbance_eligibilities with no
+            specified sit_events
 
     Returns:
         SITRuleBasedProcessor: an object for processing SIT rule based
             disturbances
     """
 
-    sit_events_parsed = None
-    if sit_events:
+    separate_eligibilities = sit_disturbance_eligibilities is not None
+    disturbance_events = None
+    disturbance_eligibilities = None
+    transition_rules = None
+    if sit_events is not None:
+
+        disturbance_events = sit_disturbance_event_parser.parse(
+            sit_events, sit.sit_data.classifiers,
+            sit.sit_data.classifier_values, sit.sit_data.classifier_aggregates,
+            sit.sit_data.disturbance_types, sit.sit_data.age_classes,
+            separate_eligibilities=separate_eligibilities)
+
+        if sit_disturbance_eligibilities is not None:
+            disturbance_eligibilities = \
+                sit_disturbance_event_parser.parse_eligibilities(
+                    sit_events, sit_disturbance_eligibilities)
+    else:
+        disturbance_events = sit.sit_data.disturbance_events
+        disturbance_eligibilities = sit.sit_data.disturbance_eligibilities
+        if separate_eligibilities:
+            raise ValueError(
+                "cannot specify sit_disturbance_eligibilities with no "
+                "specified sit_events")
+
+    if sit_transition_rules is not None:
+        if len(sit_transition_rules.index) == 0:
+            transition_rules = None
+        else:
+            transition_rules = sit_transition_rule_parser.parse(
+                sit_transition_rules, sit.sit_data.classifiers,
+                sit.sit_data.classifier_values,
+                sit.sit_data.classifier_aggregates,
+                sit.sit_data.disturbance_types, sit.sit_data.age_classes)
+    else:
+        transition_rules = sit.sit_data.transition_rules
 
     classifiers_config = get_classifiers(
         sit.sit_data.classifiers, sit.sit_data.classifier_values)
@@ -405,7 +461,8 @@ def create_sit_rule_based_processor(
         random_func=random_func,
         classifiers_config=classifiers_config,
         classifier_aggregates=sit.sit_data.classifier_aggregates,
-        sit_events=initialize_events(sit),
-        sit_transitions=initialize_transition_rules(sit),
+        sit_events=_initialize_events(disturbance_events, sit.sit_mapping),
+        sit_transitions=_initialize_transition_rules(
+            transition_rules, sit.sit_mapping),
         tr_constants=tr_constants,
-        sit_disturbance_eligibilities=sit.sit_data.disturbance_eligibilities)
+        sit_disturbance_eligibilities=disturbance_eligibilities)
