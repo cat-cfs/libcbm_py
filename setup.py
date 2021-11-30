089b3c647dc6834b239286ed9c931a885a269b32
Scott <scottm361@gmail.com>
2021-11-30 09:32:25 -0800

    new ver 0.9.3

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="libcbm",
-    version="0.9.0",
+    version="0.9.3",
     description="Carbon budget model library based on CBM-CFS3",
     keywords=["cbm-cfs3"],
     long_description=long_description,
