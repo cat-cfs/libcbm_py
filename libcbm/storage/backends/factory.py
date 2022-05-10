26b8d5b5bc33733a2390d276f740d68283a64588
Scott <scottm361@gmail.com>
2022-05-10 13:08:26 -0700

    backend factory

---
+++ libcbm/storage/backends/factory.py
@@ -0,0 +1,7 @@
+from libcbm.storage.backends import BackendType
+
+
+def get_backend(backend_type: BackendType):
+    if backend_type == BackendType.pandas:
+        from libcbm.storage.backends import pandas_backend
+        return pandas_backend
