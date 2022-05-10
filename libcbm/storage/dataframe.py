00e2d3b48db0fd429fe66c41c4a48a17b7b1a305
Scott <scottm361@gmail.com>
2022-05-10 13:15:02 -0700

    call backend

--- libcbm/storage/dataframe.py
+++ libcbm/storage/dataframe.py
@@ -6,6 +6,7 @@ from typing import Callable
 from libcbm.storage.backends import BackendType
 from libcbm.storage.series import Series
 from typing import Any
+from libcbm.storage.backends import factory
 
 
 class DataFrame:
@@ -20,46 +21,48 @@ class DataFrame:
         nrows: int = None,
         back_end: BackendType = None,
     ):
-        raise NotImplementedError()
+
+        self._data = data
+        self._back_end = factory.get_backend(back_end)
 
     def __getitem__(self, col_name: str) -> Series:
-        raise NotImplementedError()
+        return self._back_end.getitem(self._data, col_name)
 
-    def filter(self, arg) -> "DataFrame":
-        pass
+    def filter(self, arg: Series) -> "DataFrame":
+        return self._back_end.filter(self._data, arg)
 
-    def take(self, indices) -> "DataFrame":
-        pass
+    def take(self, indices: Series) -> "DataFrame":
+        return self._back_end.take(self._data, indices)
 
-    def at(self, index) -> dict:
+    def at(self, index: int) -> dict:
         """
         get the row at the specified 0 based sequential index as a row
         dictionary
         """
-        pass
+        return self._back_end.at(self._data, index)
 
     def assign(self, col_name: str, value: Any, indices: Series = None):
-        pass
+        self._back_end.assign(self._data, col_name, value, indices)
 
     @property
     def n_rows(self) -> int:
-        raise NotImplementedError()
+        return self._back_end.n_rows(self._data)
 
     @property
     def n_cols(self) -> int:
-        raise NotImplementedError()
+        return self._back_end.n_cols(self._data)
 
     @property
     def columns(self) -> list[str]:
-        raise NotImplementedError()
+        return self._back_end.columns(self._data)
 
     @property
     def backend_type(self) -> BackendType:
-        raise NotImplementedError()
+        return self._back_end.backend_type()
 
-    def copy() -> "DataFrame":
+    def copy(self) -> "DataFrame":
         """produce a new in-memory copy of this dataframe"""
-        raise NotImplementedError()
+        return self._back_end.copy(self._data)
 
     def multiply(self, series: Series) -> "DataFrame":
         """
@@ -67,29 +70,29 @@ class DataFrame:
         row axis. An error is raised if the series length is not the same as
         the number of rows in this dataframe.  Returns new DataFrame
         """
-        raise NotImplementedError()
+        return self._back_end.multiply(self._data, series)
 
     def add_column(self, series: Series, index: int) -> None:
-        raise NotImplementedError()
+        self._back_end.add_column(self._data, series, index)
 
     def to_c_contiguous_numpy_array(self) -> np.ndarray:
-        raise NotImplementedError()
+        return self._back_end.to_c_contiguous_numpy_array(self._data)
 
     def to_pandas(self) -> pd.DataFrame:
-        raise NotImplementedError()
+        return self._back_end.to_pandas(self._data)
 
     def zero(self):
         """
         Set all values in this dataframe to zero
         """
-        raise NotImplementedError()
+        self._back_end.zero(self._data)
 
     def map(self, arg: Union[dict, Callable]) -> "DataFrame":
         """Apply the specified mapping arg on every element of this dataframe
         to project a new dataframe with updated values. The results has the
         same number of rows, columns and same column names
         """
-        raise NotImplementedError()
+        return self._back_end.map(self._data, arg)
 
 
 def numeric_dataframe(
