9a9be841dd75234e08deec07d3064723536319e3
Scott <scottm361@gmail.com>
2022-05-12 15:30:58 -0700

    fix abstract methods, add abstract dataframe and series methods

--- libcbm/storage/dataframe.py
+++ libcbm/storage/dataframe.py
@@ -44,27 +44,27 @@ class DataFrame(ABC):
     @property
     @abstractmethod
     def n_rows(self) -> int:
-        return self._back_end.n_rows(self._data)
+        pass
 
     @property
     @abstractmethod
     def n_cols(self) -> int:
-        return self._back_end.n_cols(self._data)
+        pass
 
     @property
     @abstractmethod
     def columns(self) -> list[str]:
-        return self._back_end.columns(self._data)
+        pass
 
     @property
     @abstractmethod
     def backend_type(self) -> BackendType:
-        return self._back_end.backend_type()
+        pass
 
     @abstractmethod
     def copy(self) -> "DataFrame":
         """produce a new in-memory copy of this dataframe"""
-        return self._back_end.copy(self._data)
+        pass
 
     @abstractmethod
     def multiply(self, series: Series) -> "DataFrame":
@@ -73,26 +73,26 @@ class DataFrame(ABC):
         row axis. An error is raised if the series length is not the same as
         the number of rows in this dataframe.  Returns new DataFrame
         """
-        return self._back_end.multiply(self._data, series)
+        pass
 
     @abstractmethod
     def add_column(self, series: Series, index: int) -> None:
-        self._back_end.add_column(self._data, series, index)
+        pass
 
     @abstractmethod
     def to_c_contiguous_numpy_array(self) -> np.ndarray:
-        return self._back_end.to_c_contiguous_numpy_array(self._data)
+        pass
 
     @abstractmethod
     def to_pandas(self) -> pd.DataFrame:
-        return self._back_end.to_pandas(self._data)
+        pass
 
     @abstractmethod
     def zero(self):
         """
         Set all values in this dataframe to zero
         """
-        self._back_end.zero(self._data)
+        pass
 
     @abstractmethod
     def map(self, arg: Union[dict, Callable]) -> "DataFrame":
@@ -100,7 +100,143 @@ class DataFrame(ABC):
         to project a new dataframe with updated values. The results has the
         same number of rows, columns and same column names
         """
-        return self._back_end.map(self._data, arg)
+        pass
+
+
+def get_backend(backend_type: BackendType):
+
+    if backend_type == BackendType.numpy:
+        from libcbm.storage.backends import numpy_backend
+
+        return numpy_backend
+    elif backend_type == BackendType.pandas:
+        from libcbm.storage.backends import pandas_backend
+
+        return pandas_backend
+    else:
+        raise NotImplementedError()
+
+
+def convert_series_backend(
+    series: Series, backend_type: BackendType
+) -> Series:
+    if series.backend_type == backend_type:
+        return series
+
+    elif backend_type == BackendType.numpy:
+        from libcbm.storage.backends import numpy_backend
+
+        return numpy_backend.NumpySeriesBackend(series.name, series.to_numpy())
+    elif backend_type == BackendType.pandas:
+        from libcbm.storage.backends import pandas_backend
+
+        return pandas_backend.PandasSeriesBackend(
+            series.name, pd.Series(series.to_numpy())
+        )
+    else:
+        raise NotImplementedError()
+
+
+def convert_dataframe_backend(
+    df: DataFrame, backend_type: BackendType
+) -> DataFrame:
+    if df.backend_type == backend_type:
+        return df
+    elif backend_type == BackendType.numpy:
+        from libcbm.storage.backends import numpy_backend
+
+        return numpy_backend.NumpyDataFrameFrameBackend(
+            {col: df[col].to_numpy() for col in df.columns}
+        )
+    elif backend_type == BackendType.pandas:
+        from libcbm.storage.backends import pandas_backend
+
+        return pandas_backend.PandasDataFrameBackend(
+            pd.DataFrame({col: df[col].to_numpy() for col in df.columns})
+        )
+    else:
+        raise NotImplementedError()
+
+
+def _get_uniform_backend(
+    data: list[Union[DataFrame, Series]], backend_type: BackendType = None
+) -> tuple[BackendType, list[Union[DataFrame, Series]]]:
+    if backend_type is None:
+        inferred_backend = None
+        for _d in data:
+            if inferred_backend is None:
+                inferred_backend = _d.backend_type
+            elif inferred_backend != _d.backend_type:
+                raise ValueError(
+                    "backend type must be specified with non-uniform list of "
+                    "dataframes backends"
+                )
+
+        backend_type = inferred_backend
+    output = []
+    for _d in data:
+        if isinstance(_d, DataFrame):
+            output.append(convert_dataframe_backend(_d, backend_type))
+        else:
+            output.append(convert_series_backend(_d, backend_type))
+    return backend_type, output
+
+
+def concat_data_frame(
+    data: list[Union[DataFrame, Series]], backend_type: BackendType = None
+) -> Union[DataFrame, Series]:
+
+    backend_type, uniform_dfs = _get_uniform_backend(data, backend_type)
+    return get_backend(backend_type).concat_data_frame(uniform_dfs)
+
+
+def concat_series(
+    series: list[Series], backend_type: BackendType = None
+) -> Series:
+    backend_type, uniform_dfs = _get_uniform_backend(series, backend_type)
+    return get_backend(backend_type).concat_series(uniform_dfs)
+
+
+def logical_and(
+    s1: Series, s2: Series, backend_type: BackendType = None
+) -> Series:
+    backend_type, uniform_dfs = _get_uniform_backend([s1, s2], backend_type)
+    return get_backend(backend_type).logical_and(
+        uniform_dfs[0], uniform_dfs[1]
+    )
+
+
+def logical_not(series: Series) -> Series:
+    return get_backend(series.backend_type).logical_not(series)
+
+
+def logical_or(
+    s1: Series, s2: Series, backend_type: BackendType = None
+) -> Series:
+    backend_type, uniform_dfs = _get_uniform_backend([s1, s2], backend_type)
+    return get_backend(backend_type).logical_or(uniform_dfs[0], uniform_dfs[1])
+
+
+def make_boolean_series(
+    init: bool, size: int, backend_type: BackendType.numpy
+) -> Series:
+    return get_backend(backend_type).make_boolean_series(init, size)
+
+
+def is_null(series: Series) -> Series:
+    """
+    returns an Series of True where any value in the specified series
+    is null, None, or NaN, and otherwise False
+    """
+    return get_backend(series.backend_type).is_null(series)
+
+
+def indices_nonzero(series: Series) -> Series:
+    """
+    return a series of the 0 based sequential index of non-zero values in
+    the specfied series
+    """
+    return get_backend(series.backend_type).indices_nonzero(series)
 
 
 def numeric_dataframe(
@@ -112,5 +248,13 @@ def numeric_dataframe(
     return factory.get_backend(back_end).numeric_dataframe(cols, nrows, init)
 
 
-def from_pandas(df: pd.DataFrame, back_end: BackendType) -> DataFrame:
-    return factory.get_backend(back_end).from_pandas(df)
+def from_pandas(df: pd.DataFrame) -> DataFrame:
+    from libcbm.storage.backends import pandas_backend
+
+    return pandas_backend.PandasDataFrameBackend(df)
+
+
+def from_numpy(data: dict[str, np.ndarray]) -> DataFrame:
+    from libcbm.storage.backends import numpy_backend
+
+    return numpy_backend.NumpyDataFrameFrameBackend(data)
