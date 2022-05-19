4e27d70cc8ea7e379242d6d6a529b7f7e57dac22
Scott <scottm361@gmail.com>
2022-05-19 11:02:19 -0700

    add get_backend method

--- libcbm/storage/backends/__init__.py
+++ libcbm/storage/backends/__init__.py
@@ -5,3 +5,17 @@ class BackendType(Enum):
     numpy = 1
     pandas = 2
     pyarrow = 3
+
+
+def get_backend(backend_type: BackendType):
+
+    if backend_type == BackendType.numpy:
+        from libcbm.storage.backends import numpy_backend
+
+        return numpy_backend
+    elif backend_type == BackendType.pandas:
+        from libcbm.storage.backends import pandas_backend
+
+        return pandas_backend
+    else:
+        raise NotImplementedError()
