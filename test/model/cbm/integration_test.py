47996391602b10084c8d1f51d6e3999ac716de88
Scott <scottm361@gmail.com>
2022-06-16 16:26:11 -0700

    test repairs

--- test/model/cbm/integration_test.py
+++ test/model/cbm/integration_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from libcbm.storage import dataframe
+from libcbm.storage import series
 from libcbm.model.cbm import cbm_variables
 from libcbm.model.cbm import cbm_simulator
 from libcbm.model.cbm.stand_cbm_factory import StandCBMFactory
@@ -35,38 +36,40 @@ def test_integration():
     return_interval = 50
     n_rotations = 5
     age = 15
-    inventory = pd.DataFrame(
-        columns=[
-            "c1",
-            "c2",
-            "admin_boundary",
-            "eco_boundary",
-            "age",
-            "area",
-            "delay",
-            "land_class",
-            "afforestation_pre_type",
-            "historic_disturbance_type",
-            "last_pass_disturbance_type",
-        ],
-        data=[
-            [
-                "c1_v1",
-                "c2_v1",
-                "British Columbia",
-                "Pacific Maritime",
-                age,
-                1.0,
-                0,
-                "UNFCCC_FL_R_FL",
-                None,
-                "Wildfire",
-                "Wildfire",
-            ]
-        ],
+    inventory = dataframe.from_pandas(
+        pd.DataFrame(
+            columns=[
+                "c1",
+                "c2",
+                "admin_boundary",
+                "eco_boundary",
+                "age",
+                "area",
+                "delay",
+                "land_class",
+                "afforestation_pre_type",
+                "historic_disturbance_type",
+                "last_pass_disturbance_type",
+            ],
+            data=[
+                [
+                    "c1_v1",
+                    "c2_v1",
+                    "British Columbia",
+                    "Pacific Maritime",
+                    age,
+                    1.0,
+                    0,
+                    "UNFCCC_FL_R_FL",
+                    None,
+                    "Wildfire",
+                    "Wildfire",
+                ]
+            ],
+        )
     )
     csets, inv = cbm_factory.prepare_inventory(inventory)
-    n_stands = len(inv.index)
+    n_stands = inv.n_rows
     with cbm_factory.initialize_cbm() as cbm:
 
         spinup_results = InMemoryCBMOutput(density=True)
@@ -84,12 +87,41 @@ def test_integration():
             pre_dynamics_func=lambda t, cbm_vars: cbm_vars,
             reporting_func=cbm_results.append_simulation_result,
             spinup_params=cbm_variables.initialize_spinup_parameters(
-                n_stands, return_interval, n_rotations, n_rotations, -1
+                n_stands,
+                inventory.backend_type,
+                series.allocate(
+                    "return_inverval",
+                    n_stands,
+                    return_interval,
+                    "int32",
+                    inventory.backend_type,
+                ),
+                series.allocate(
+                    "min_rotations",
+                    n_stands,
+                    n_rotations,
+                    "int32",
+                    inventory.backend_type,
+                ),
+                series.allocate(
+                    "max_rotations",
+                    n_stands,
+                    n_rotations,
+                    "int32",
+                    inventory.backend_type,
+                ),
+                series.allocate(
+                    "mean_annual_temp",
+                    n_stands,
+                    -1,
+                    "float",
+                    inventory.backend_type,
+                ),
             ),
             spinup_reporting_func=spinup_results.append_simulation_result,
         )
-        assert len(cbm_results.pools.index) == (n_steps + 1) * n_stands
+        assert cbm_results.pools.n_rows == (n_steps + 1) * n_stands
         assert (
-            len(spinup_results.pools.index)
+            spinup_results.pools.n_rows
             == (n_rotations * return_interval) + age - 1
         )
