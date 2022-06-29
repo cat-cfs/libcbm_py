e000cfa8191763dc5a3440f302f60ac43dc82cd7
Scott <scottm361@gmail.com>
2022-06-29 10:57:51 -0700

    name refactor

--- libcbm/model/cbm/rule_based/sit/sit_rule_based_sort.py
+++ libcbm/model/cbm/rule_based/sit/sit_rule_based_sort.py
@@ -1,7 +1,33 @@
+
+from typing import Callable
 from libcbm.input.sit import sit_disturbance_event_parser
 from libcbm.storage.series import Series
 from libcbm.storage.dataframe import DataFrame
 from libcbm.model.cbm.cbm_variables import CBMVariables
+
+
+def get_sort_value(
+    sort_type: str,
+    cbm_vars: CBMVariables,
+    random_generator: Callable[[int], Series],
+) -> Series:
+    if sort_type == "SORT_BY_SW_AGE" or sort_type == "SORT_BY_HW_AGE":
+        return cbm_vars.state["age"]
+    elif sort_type == "TOTALSTEMSNAG":
+        return (
+            cbm_vars.pools["SoftwoodStemSnag"]
+            + cbm_vars.pools["HardwoodStemSnag"]
+        )
+    elif sort_type == "SWSTEMSNAG":
+        return cbm_vars.pools["SoftwoodStemSnag"]
+    elif sort_type == "HWSTEMSNAG":
+        return cbm_vars.pools["HardwoodStemSnag"]
+    elif sort_type == "RANDOMSORT":
+        return random_generator(cbm_vars.pools.n_rows)
+    else:
+        raise ValueError(
+            f"specified sort_type '{sort_type}' is not sort value"
+        )
 
 
 def is_production_sort(sit_event_row: dict) -> bool:
