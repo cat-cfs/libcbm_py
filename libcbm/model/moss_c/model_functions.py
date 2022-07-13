620ada7bcc6c1847bc8874bfa736094254821bbe
Scott <scottm361@gmail.com>
2022-07-13 11:17:19 -0700

    make class for return type, type annotations

--- libcbm/model/moss_c/model_functions.py
+++ libcbm/model/moss_c/model_functions.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from enum import IntEnum
-from types import SimpleNamespace
 from libcbm.model.moss_c.pools import Pool
 
 import numba
@@ -17,7 +16,7 @@ class SpinupState(IntEnum):
     End = 5
 
 
-def np_map(a, m, dtype):
+def np_map(a: np.ndarray, m: dict, dtype: str):
     """Return the mapped value of a according to the dictionary m.
     The occurence of any value in a that is not present as a key
     in m will result in a value error.
@@ -25,7 +24,7 @@ def np_map(a, m, dtype):
     Args:
         a (numpy.ndarray): a numpy array
         m (dict): a dictionary to map values in the resulting array
-        dtype (object): numpy type assigned as type of returned array
+        dtype (str): numpy type assigned as type of returned array
     Returns:
         numpy.ndarray: the numpy array with replaced mapped values
     """
@@ -37,7 +36,7 @@ def np_map(a, m, dtype):
 
 
 @numba.njit()
-def _np_map(a, m, out):
+def _np_map(a: np.ndarray, m: numba.typed.Dict, out: np.ndarray):
     for index, value in np.ndenumerate(a):
         if value in m:
             out[index] = m[value]
@@ -46,8 +45,23 @@ def _np_map(a, m, out):
     return out
 
 
-def initialize_dm(disturbance_matrix_data):
-    dm_data = disturbance_matrix_data
+class DMData:
+    def __init__(
+        self, dm_dist_type_index: dict[int, int], dm_list: list[np.ndarray]
+    ):
+        self._dm_dist_type_index = dm_dist_type_index
+        self._dm_list = dm_list
+
+    @property
+    def dm_dist_type_index(self) -> dict[int, int]:
+        return self._dm_dist_type_index
+
+    @property
+    def dm_list(self) -> list[np.ndarray]:
+        return self._dm_list
+
+
+def initialize_dm(dm_data: pd.DataFrame) -> DMData:
     proportions_valid = np.allclose(
         1.0,
         dm_data[["disturbance_type_id", "source", "proportion"]]
@@ -66,14 +80,16 @@ def initialize_dm(disturbance_matrix_data):
     )
     dm_list = [identity_matrix]
     dm_dist_type_index = {0: 0}
-    for dist_type_id in dm_data.disturbance_type_id.unique():
-        dm_values = dm_data[dm_data.disturbance_type_id == dist_type_id].copy()
+    for dist_type_id in dm_data["disturbance_type_id"].unique():
+        dm_values: pd.DataFrame = dm_data[
+            dm_data["disturbance_type_id"] == dist_type_id
+        ].copy()
 
         identity_set = {p for p in Pool}
 
         for _, row in dm_values.iterrows():
-            if Pool[row.source] == Pool[row.sink]:
-                identity_set.remove(Pool[row.source])
+            if Pool[row["source"]] == Pool[row["sink"]]:
+                identity_set.remove(Pool[row["source"]])
 
         dm_values = pd.concat(
             [
@@ -91,23 +107,23 @@ def initialize_dm(disturbance_matrix_data):
                 ),
             ]
         )
-        mat = np.column_stack(
+        mat: np.ndarray = np.column_stack(
             [
-                np.array([Pool[p] for p in dm_values.source], dtype=float),
-                np.array([Pool[p] for p in dm_values.sink], dtype=float),
-                dm_values.proportion.to_numpy(),
+                np.array([Pool[p] for p in dm_values["source"]], dtype=float),
+                np.array([Pool[p] for p in dm_values["sink"]], dtype=float),
+                dm_values["proportion"].to_numpy(),
             ]
         )
         dm_dist_type_index[dist_type_id] = len(dm_list)
         dm_list.append(mat)
 
-    return SimpleNamespace(
-        dm_dist_type_index=dm_dist_type_index, dm_list=dm_list
-    )
+    return DMData(dm_dist_type_index=dm_dist_type_index, dm_list=dm_list)
 
 
 @numba.njit()
-def _small_slow_diff(last_rotation_slow, this_rotation_slow):
+def _small_slow_diff(
+    last_rotation_slow: np.ndarray, this_rotation_slow: np.ndarray
+) -> np.ndarray:
     return (
         abs(
             (last_rotation_slow - this_rotation_slow)
@@ -119,15 +135,15 @@ def _small_slow_diff(last_rotation_slow, this_rotation_slow):
 
 
 def advance_spinup_state(
-    spinup_state,
-    age,
-    final_age,
-    return_interval,
-    rotation_num,
-    max_rotations,
-    last_rotation_slow,
-    this_rotation_slow,
-):
+    spinup_state: np.ndarray,
+    age: np.ndarray,
+    final_age: np.ndarray,
+    return_interval: np.ndarray,
+    rotation_num: np.ndarray,
+    max_rotations: np.ndarray,
+    last_rotation_slow: np.ndarray,
+    this_rotation_slow: np.ndarray,
+) -> np.ndarray:
 
     out_state = spinup_state.copy()
     _advance_spinup_state(
@@ -147,17 +163,17 @@ def advance_spinup_state(
 
 @numba.njit()
 def _advance_spinup_state(
-    n_stands,
-    spinup_state,
-    age,
-    final_age,
-    return_interval,
-    rotation_num,
-    max_rotations,
-    last_rotation_slow,
-    this_rotation_slow,
-    out_state,
-):
+    n_stands: int,
+    spinup_state: np.ndarray,
+    age: np.ndarray,
+    final_age: np.ndarray,
+    return_interval: np.ndarray,
+    rotation_num: np.ndarray,
+    max_rotations: np.ndarray,
+    last_rotation_slow: np.ndarray,
+    this_rotation_slow: np.ndarray,
+    out_state: np.ndarray,
+) -> np.ndarray:
 
     for i in range(0, n_stands):
         state = spinup_state[i]
