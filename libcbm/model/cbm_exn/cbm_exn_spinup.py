b0dbb28c8b15291f36608c09f7af43abd515c36f
Scott <scottm361@gmail.com>
2022-11-17 15:04:12 -0800

    function signature layout

--- libcbm/model/cbm_exn/cbm_exn_spinup.py
+++ libcbm/model/cbm_exn/cbm_exn_spinup.py
@@ -1,7 +1,20 @@
+from typing import Callable
 from libcbm.model.cbm_exn.model import CBMEXNModel
 from libcbm.model.cbm_exn.cbm_variables import CBMVariables
 from libcbm.model.cbm_exn.cbm_variables import SpinupInput
+from libcbm.model.cbm_exn.cbm_variables import SpinupVariables
 
 
-def spinup(model: CBMEXNModel, input: SpinupInput) -> CBMVariables:
+def init_cbm_vars(
+    model: CBMEXNModel, spinup_vars: SpinupVariables
+) -> CBMVariables:
+    pass
+
+
+def spinup(
+    model: CBMEXNModel,
+    input: SpinupInput,
+    reporting_func: Callable[[int, SpinupVariables], None] = None,
+    include_flux: bool = False
+) -> CBMVariables:
     pass
