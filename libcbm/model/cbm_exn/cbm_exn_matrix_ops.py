40998c3929c37625cddebf5da9f2a36869629318
Scott <scottm361@gmail.com>
2022-11-17 14:37:27 -0800

    matrix ops layout

--- libcbm/model/cbm_exn/cbm_exn_matrix_ops.py
+++ libcbm/model/cbm_exn/cbm_exn_matrix_ops.py
@@ -1,3 +1,4 @@
+from enum import IntEnum
 import numpy as np
 from libcbm.model.cbm_exn.model import CBMEXNModel
 from libcbm.wrapper.libcbm_operation import Operation
@@ -5,30 +6,61 @@ from libcbm.model.cbm_exn.cbm_variables import CBMVariables
 from libcbm.model.cbm_exn.cbm_exn_parameters import CBMEXNParameters
 
 
-def net_growth(model: CBMEXNModel, cbm_vars: CBMVariables) -> Operation:
-    pass
+class OpProcesses(IntEnum):
+    """
+    grouping categories for flux indicators
+    """
+
+    growth = 1
+    decay = 2
+    disturbance = 3
 
 
-def overmature_decline(
-    model: CBMEXNModel, cbm_vars: CBMVariables
+def disturbance(
+    model: CBMEXNModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
 ) -> Operation:
     pass
 
 
-def snag_turnover(model: CBMEXNModel, cbm_vars: CBMVariables) -> Operation:
+def net_growth(
+    model: CBMEXNModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
+) -> Operation:
     pass
 
 
-def dom_decay(model: CBMEXNModel, cbm_vars: CBMVariables) -> Operation:
+def overmature_decline(
+    model: CBMEXNModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
+) -> Operation:
     pass
 
 
-def slow_decay(model: CBMEXNModel, cbm_vars: CBMVariables) -> Operation:
+def snag_turnover(
+    model: CBMEXNModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
+) -> Operation:
     pass
 
 
-def slow_mixing(model: CBMEXNModel, cbm_vars: CBMVariables) -> Operation:
-    parameters = CBMEXNParameters(model.parameters)
+def biomass_turnover(
+    model: CBMEXNModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
+) -> Operation:
+    pass
+
+
+def dom_decay(
+    model: CBMEXNModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
+) -> Operation:
+    pass
+
+
+def slow_decay(
+    model: CBMEXNModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
+) -> Operation:
+    pass
+
+
+def slow_mixing(
+    model: CBMEXNModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
+) -> Operation:
     rate: float = parameters.get_slow_mixing_rate()
     op = model._model_handle.create_operation(
         matrices=[
