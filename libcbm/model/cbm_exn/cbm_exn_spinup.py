7b35025df1f19527a109199b04fcce9c70f0f58c
Scott <scottm361@gmail.com>
2022-11-18 11:46:19 -0800

    refactor according to previous commit

--- libcbm/model/cbm_exn/cbm_exn_spinup.py
+++ libcbm/model/cbm_exn/cbm_exn_spinup.py
@@ -1,20 +1,16 @@
 from typing import Callable
 from libcbm.model.model_definition.model import CBMModel
 from libcbm.model.model_definition.cbm_variables import CBMVariables
-from libcbm.model.model_definition.cbm_variables import SpinupInput
-from libcbm.model.model_definition.cbm_variables import SpinupVariables
 
 
-def init_cbm_vars(
-    model: CBMModel, spinup_vars: SpinupVariables
-) -> CBMVariables:
+def init_cbm_vars(model: CBMModel, spinup_vars: CBMVariables) -> CBMVariables:
     pass
 
 
 def spinup(
     model: CBMModel,
-    input: SpinupInput,
-    reporting_func: Callable[[int, SpinupVariables], None] = None,
+    input: CBMVariables,
+    reporting_func: Callable[[int, CBMVariables], None] = None,
     include_flux: bool = False,
 ) -> CBMVariables:
     pass
