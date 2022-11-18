abd646b3e9c40cbf190de3a67c1a2ddd311f4bf0
Scott <scottm361@gmail.com>
2022-11-18 15:50:27 -0800

    docs

--- libcbm/model/model_definition/cbm_variables.py
+++ libcbm/model/model_definition/cbm_variables.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 
 class CBMVariables:
+    """
+    Container for multiple named dataframes
+    """
+
     def __init__(self, data: dict[str, DataFrame]):
         self._data = data
 
@@ -14,13 +18,25 @@ class CBMVariables:
         return name in self._data
 
     def get_collection(self) -> dict[str, DataFrame]:
+        """
+        Get a dictionary containing named references to the
+        dataframes stored in this collection
+        """
         return {k: v for k, v in self._data.items()}
 
     @staticmethod
     def from_pandas(frames: dict[str, pd.DataFrame]) -> "CBMVariables":
+        """
+        Assemble a CBMVariables instance from a collection of pandas DataFrames
+        """
         return CBMVariables(
             {k: dataframe.from_pandas(v) for k, v in frames.items()}
         )
 
     def to_pandas(self) -> dict[str, pd.DataFrame]:
+        """
+        return the dataframes in this collection as a dictionary
+        of named pandas dataframes.  This may result in a copy
+        if the underlying dataframe storage backend is not pandas
+        """
         return {k: v.to_pandas() for k, v in self._data.items()}
