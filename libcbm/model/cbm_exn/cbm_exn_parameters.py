4b52b8dad0524a013b72c519a57cc0666013f2f3
Scott <scottm361@gmail.com>
2022-11-23 10:15:09 -0800

    use pandas for parameter tables

--- libcbm/model/cbm_exn/cbm_exn_parameters.py
+++ libcbm/model/cbm_exn/cbm_exn_parameters.py
@@ -1,6 +1,12 @@
+import pandas as pd
+
+
 class CBMEXNParameters:
     def __init__(self, parameters: dict):
         self._parameters = parameters
 
     def get_slow_mixing_rate(self) -> float:
+        pass
+
+    def get_turnover_parameters(self) -> pd.DataFrame:
         pass
