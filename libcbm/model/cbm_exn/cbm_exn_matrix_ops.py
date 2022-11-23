82c8ce0926cad366ac67a48946ee5f1ceaed2576
Scott <scottm361@gmail.com>
2022-11-23 10:15:43 -0800

    use matrix ops class to manage matrix allocations, indexing

--- libcbm/model/cbm_exn/cbm_exn_matrix_ops.py
+++ libcbm/model/cbm_exn/cbm_exn_matrix_ops.py
@@ -4,6 +4,7 @@ from libcbm.model.model_definition.model import CBMModel
 from libcbm.model.model_definition.cbm_variables import CBMVariables
 from libcbm.wrapper.libcbm_operation import Operation
 from libcbm.model.cbm_exn.cbm_exn_parameters import CBMEXNParameters
+from libcbm.storage.series import Series
 
 
 class OpProcesses(IntEnum):
@@ -32,53 +33,193 @@ class SpinupMatrixOps:
         pass
 
 
-def disturbance(
+class MatrixOps:
+    def __init__(self, model: CBMModel, parameters: CBMEXNParameters):
+        self._parameters = parameters
+
+        self._model = model
+        self._turnover_parameters = self._parameters.get_turnover_parameters()
+        self._turnover_parameter_rates: dict[str, np.ndarray] = None
+        self._slow_mixing_rate: float = self._parameters.get_slow_mixing_rate()
+
+        # dictionary of spuid, turnover parameter matrix index
+        self._turnover_parameter_idx: dict[int, int] = None
+        self._get_turnover_rates()
+        self._biomass_turnover_op: Operation = None
+        self._snag_turnover_op: Operation = None
+
+        self._slow_mixing_op: Operation = None
+
+    def _get_turnover_rates(self):
+        parameter_names = [
+            "StemAnnualTurnoverRate",
+            "FoliageFallRate",
+            "BranchTurnoverRate",
+            "CoarseRootTurnProp",
+            "FineRootTurnProp",
+            "OtherToBranchSnagSplit",
+            "CoarseRootAGSplit",
+            "FineRootAGSplit",
+            "StemSnag",
+            "BranchSnag",
+        ]
+        self._turnover_parameter_rates = {
+            str(col): self._turnover_parameters[col].to_numpy()
+            for col in parameter_names
+        }
+        duplicates = self._turnover_parameters["spuid"].duplicated()
+        if duplicates.any():
+            duplicate_values = self._turnover_parameters["spuid"][duplicates]
+            raise ValueError(
+                "duplicated spuids detected in turnover paramters "
+                f"{duplicate_values}"
+            )
+        self._turnover_parameter_idx = {
+            int(spuid): int(idx)
+            for idx, spuid in enumerate(
+                self._turnover_parameters["spuid"].to_numpy()
+            )
+        }
+
+    def snag_turnover(self, spuid: Series) -> Operation:
+        if not self._snag_turnover_op:
+            self._snag_turnover_op = _snag_turnover(
+                self._model, self._turnover_parameter_rates
+            )
+            self._snag_turnover_op.set_op(
+                spuid.map(self._turnover_parameter_idx)
+            )
+        else:
+            self._snag_turnover_op.update_index(
+                spuid.map(self._turnover_parameter_idx)
+            )
+        return self._snag_turnover_op
+
+    def biomass_turnover(self, spuid: Series) -> Operation:
+        if not self._biomass_turnover_op:
+            self._biomass_turnover_op = _biomass_turnover(
+                self._model, self._turnover_parameter_rates
+            )
+            self._biomass_turnover_op.set_op(
+                spuid.map(self._turnover_parameter_idx)
+            )
+        else:
+            self._biomass_turnover_op.update_index(
+                spuid.map(self._turnover_parameter_idx)
+            )
+        return self._biomass_turnover_op
+
+    def slow_mixing(self, n_rows: int) -> Operation:
+        if not self._slow_mixing_op:
+            self._slow_mixing_op = _slow_mixing(
+                self._model, self._slow_mixing_rate
+            )
+            self._biomass_turnover_op.set_op(np.arange(0, n_rows))
+        else:
+            self._biomass_turnover_op.update_index(np.arange(0, n_rows))
+
+
+def _disturbance(
     model: CBMModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
 ) -> Operation:
     pass
 
 
-def net_growth(
+def _net_growth(
     model: CBMModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
 ) -> Operation:
     pass
 
 
-def overmature_decline(
+def _overmature_decline(
     model: CBMModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
 ) -> Operation:
     pass
 
 
-def snag_turnover(
+def _snag_turnover(model: CBMModel, rates: dict[str, np.ndarray]) -> Operation:
+
+    op = model.create_operation(
+        matrices=[
+            ["StemSnag", "StemSnag", 1 - rates["StemSnag"]],
+            ["StemSnag", "MediumSoil", rates["StemSnag"]],
+            ["BranchSnag", "BranchSnag", 1 - rates["BranchSnag"]],
+            ["BranchSnag", "AboveGroundFastSoil", rates["BranchSnag"]],
+        ],
+        fmt="repeating_coordinates",
+        process_id=OpProcesses.growth,
+    )
+    return op
+
+
+def _biomass_turnover(
+    model: CBMModel, rates: dict[str, np.ndarray]
+) -> Operation:
+
+    op = model.create_operation(
+        matrices=[
+            [
+                "Merch",
+                "SoftwoodStemSnag",
+                rates["StemAnnualTurnoverRate"],
+            ],
+            [
+                "Foliage",
+                "AboveGroundVeryFastSoil",
+                rates["FoliageFallRate"],
+            ],
+            [
+                "Other",
+                "SoftwoodBranchSnag",
+                rates["OtherToBranchSnagSplit"] * rates["BranchTurnoverRate"],
+            ],
+            [
+                "Other",
+                "AboveGroundFastSoil",
+                (1 - rates["OtherToBranchSnagSplit"])
+                * rates["BranchTurnoverRate"],
+            ],
+            [
+                "CoarseRoots",
+                "AboveGroundFastSoil",
+                rates["CoarseRootAGSplit"] * rates["CoarseRootTurnProp"],
+            ],
+            [
+                "CoarseRoots",
+                "BelowGroundFastSoil",
+                (1 - rates["CoarseRootAGSplit"]) * rates["CoarseRootTurnProp"],
+            ],
+            [
+                "FineRoots",
+                "AboveGroundVeryFastSoil",
+                rates["FineRootAGSplit"] * rates["FineRootTurnProp"],
+            ],
+            [
+                "FineRoots",
+                "BelowGroundVeryFastSoil",
+                (1 - rates["FineRootAGSplit"]) * rates["FineRootTurnProp"],
+            ],
+        ],
+        fmt="repeating_coordinates",
+        process_id=OpProcesses.growth,
+    )
+    return op
+
+
+def _dom_decay(
     model: CBMModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
 ) -> Operation:
     pass
 
 
-def biomass_turnover(
+def _slow_decay(
     model: CBMModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
 ) -> Operation:
     pass
 
 
-def dom_decay(
-    model: CBMModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
-) -> Operation:
-    pass
+def _slow_mixing(model: CBMModel, rate: float) -> Operation:
 
-
-def slow_decay(
-    model: CBMModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
-) -> Operation:
-    pass
-
-
-def slow_mixing(
-    model: CBMModel, cbm_vars: CBMVariables, parameters: CBMEXNParameters
-) -> Operation:
-
-    rate: float = parameters.get_slow_mixing_rate()
     op = model.create_operation(
         matrices=[
             ["AboveGroundSlowSoil", "BelowGroundSlowSoil", rate],
@@ -87,5 +228,4 @@ def slow_mixing(
         fmt="repeating_coordinates",
         process_id=OpProcesses.decay,
     )
-    # create a single matrix shared by all stands to compute slow mixing
-    op.set_op(np.zeros(cbm_vars["pools"].n_rows))
+    return op
