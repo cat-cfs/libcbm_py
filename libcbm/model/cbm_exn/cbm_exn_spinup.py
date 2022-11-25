4cfa05edf1afe27c39ec744c345728006d021176
Scott <scottm361@gmail.com>
2022-11-25 15:21:31 -0800

    use MatrixOps class

--- libcbm/model/cbm_exn/cbm_exn_spinup.py
+++ libcbm/model/cbm_exn/cbm_exn_spinup.py
@@ -20,31 +20,50 @@ def spinup(
 ) -> CBMVariables:
 
     parameters = CBMEXNParameters(model.parameters)
-    matrix_ops = cbm_exn_matrix_ops.SpinupMatrixOps(model, parameters)
+    matrix_ops = cbm_exn_matrix_ops.MatrixOps(model, parameters)
 
     spinup_vars = prepare_spinup_vars(input, include_flux)
     n_stands = spinup_vars["pools"].n_rows
+    snag_turnover = matrix_ops.snag_turnover(
+        spinup_vars["state"]["spatial_unit_id"]
+    )
+    biomass_turnover = matrix_ops.biomass_turnover(
+        spinup_vars["state"]["spatial_unit_id"]
+    )
+    dom_decay = matrix_ops.dom_decay(
+        spinup_vars["parameters"]["mean_annual_temperature"]
+    )
+    slow_decay = matrix_ops.slow_decay(
+        spinup_vars["parameters"]["mean_annual_temperature"]
+    )
+    slow_mixing = matrix_ops.slow_mixing(spinup_vars["parameters"].n_rows)
     t: int = 0
     while True:
         n_finished = cbm_exn_land_state.advance_spinup_state(spinup_vars)
         if n_finished == n_stands:
             break
 
-        step_operations = matrix_ops.get_spinup_matrix_ops(spinup_vars)
+        disturbance = matrix_ops.disturbance(
+            spinup_vars["state"]["disturbance_type"],
+            spinup_vars["state"]["spatial_unit_id"],
+        )
+        growth, overmature_decline = matrix_ops.spinup_net_growth(spinup_vars)
+        matrix_ops.net_growth()
         ops = [
-            step_operations["growth"],
-            step_operations["snag_turnover"],
-            step_operations["biomass_turnover"],
-            step_operations["overmature_decline"],
-            step_operations["growth"],
-            step_operations["dom_decay"],
-            step_operations["slow_decay"],
-            step_operations["slow_mixing"],
+            growth,
+            snag_turnover,
+            biomass_turnover,
+            overmature_decline,
+            growth,
+            dom_decay,
+            slow_decay,
+            slow_mixing,
+            disturbance,
         ]
         op_process_ids = (
-            [cbm_exn_matrix_ops.OpProcesses.disturbance]
-            + [cbm_exn_matrix_ops.OpProcesses.growth] * 5
+            [cbm_exn_matrix_ops.OpProcesses.growth] * 5
             + [cbm_exn_matrix_ops.OpProcesses.decay] * 3
+            + [cbm_exn_matrix_ops.OpProcesses.disturbance]
         )
         model.compute(spinup_vars, ops, op_process_ids)
         spinup_vars = cbm_exn_land_state.end_spinup_step(spinup_vars)
