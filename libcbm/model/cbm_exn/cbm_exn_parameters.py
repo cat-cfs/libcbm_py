67426b3e6067103a7ca28053568ede8bce96fd86
Scott <scottm361@gmail.com>
2022-11-25 11:03:16 -0800

    add more function stubs

--- libcbm/model/cbm_exn/cbm_exn_parameters.py
+++ libcbm/model/cbm_exn/cbm_exn_parameters.py
@@ -10,3 +10,12 @@ class CBMEXNParameters:
 
     def get_turnover_parameters(self) -> pd.DataFrame:
         pass
+
+    def get_sw_hw_map(self) -> dict[int, int]:
+        """
+        returns a map of speciesid: sw_hw where sw_hw is either 0: sw or 1: hw
+        """
+        pass
+
+    def get_root_parameters(self) -> dict[str, float]:
+        pass
