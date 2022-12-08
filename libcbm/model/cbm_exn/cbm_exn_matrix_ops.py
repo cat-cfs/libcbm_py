7cb41a3ff47e230ce48830da8287f407652871d5
Scott <scottm361@gmail.com>
2022-12-08 14:00:42 -0800

    optimization: use numba tuple key, rather than nested dictionary

--- libcbm/model/cbm_exn/cbm_exn_matrix_ops.py
+++ libcbm/model/cbm_exn/cbm_exn_matrix_ops.py
@@ -136,8 +136,8 @@ class MatrixOps:
         matrix_idx = np.zeros(n_rows, dtype="uintp")
         for i in range(n_rows):
             if disturbance_type[i] > 0:
-                matrix_idx[i] = dm_index[disturbance_type[i]][spuid[i]][
-                    sw_hw[i]
+                matrix_idx[i] = dm_index[
+                    (disturbance_type[i], spuid[i], sw_hw[i])
                 ]
         return matrix_idx
 
@@ -309,55 +309,80 @@ def _disturbance(
 ) -> tuple[list, Dict]:
     disturbance_matrices = parameters.get_disturbance_matrices()
 
-    matrix_data = []
-    dmid_index = {}
-    # append the null matrix
-    matrix_data.append([[p, p, 1.0] for p in model.pool_names])
-    for idx, dmid in enumerate(
-        disturbance_matrices["disturbance_matrix_id"].unique()
-    ):
+    matrix_data_by_dmid: dict[int, list[list]] = {}
+    dmid_index = Dict.empty(key_type=types.int64, value_type=types.int64)
+
+    dmid = disturbance_matrices["disturbance_matrix_id"].to_numpy()
+    source = disturbance_matrices["source_pool"].to_list()
+    sink = disturbance_matrices["sink_pool"].to_list()
+    proportion = disturbance_matrices["proportion"].to_numpy()
+    for i in range(dmid.shape[0]):
+        dmid_i = dmid[i]
+        if dmid_i not in matrix_data_by_dmid:
+            matrix_data_by_dmid[dmid_i] = []
+        matrix_data_by_dmid[dmid_i].append([source[i], sink[i], proportion[i]])
+    dmids = list(matrix_data_by_dmid.keys())
+    for i_dmid, dmid in enumerate(dmids):
         pool_set = set(model.pool_names)
-        dmid_loc = disturbance_matrices["disturbance_matrix_id"] == dmid
-        dmid_mat_data = []
-        for i, row in disturbance_matrices.loc[dmid_loc].iterrows():
-            dmid_mat_data.append(
-                [row["source_pool"], row["sink_pool"], row["proportion"]]
-            )
-            pool_set.discard(row["source_pool"])
+        dmid_index[dmid] = i_dmid + 1
+        for row in matrix_data_by_dmid[dmid]:
+            pool_set.discard(row[0])
         for pool in pool_set:
-            dmid_mat_data.append([pool, pool, 1.0])
-        matrix_data.append(dmid_mat_data)
-        dmid_index[int(dmid)] = idx + 1
+            matrix_data_by_dmid[dmid].append([pool, pool, 1.0])
 
+    dm_assocatiations = parameters.get_disturbance_matrix_associations()
     _dm_op_index = Dict.empty(
-        key_type=types.int64,
-        value_type=numba.types.DictType(
-            types.int64, numba.types.DictType(types.int64, types.int64)
-        ),
+        key_type=types.UniTuple(types.int64, 3),
+        value_type=types.int64,
     )
-    for _, row in parameters.get_disturbance_matrix_associations().iterrows():
-        dist_type = int(row["disturbance_type_id"])
-        spuid = int(row["spatial_unit_id"])
-        sw_hw = int(row["sw_hw"])
-        dm_idx = dmid_index[int(row["disturbance_matrix_id"])]
-        if dist_type not in _dm_op_index:
-            _dm_op_index[dist_type] = Dict.empty(
-                key_type=types.int64,
-                value_type=numba.types.DictType(types.int64, types.int64),
-            )
-            _dm_op_index[dist_type][spuid] = Dict.empty(
-                key_type=types.int64, value_type=types.int64
-            )
-            _dm_op_index[dist_type][spuid][sw_hw] = dm_idx
-        elif spuid not in _dm_op_index[dist_type]:
-            _dm_op_index[dist_type][spuid] = Dict.empty(
-                key_type=types.int64, value_type=types.int64
-            )
-            _dm_op_index[dist_type][spuid][sw_hw] = dm_idx
-        elif sw_hw not in _dm_op_index[dist_type][spuid]:
-            _dm_op_index[dist_type][spuid][sw_hw] = dm_idx
+    _dm_op_index = _build_dm_op_index(
+        _dm_op_index,
+        dmid_index,
+        dm_assocatiations["disturbance_matrix_id"].to_numpy(dtype="int"),
+        dm_assocatiations["disturbance_type_id"].to_numpy(dtype="int"),
+        dm_assocatiations["spatial_unit_id"].to_numpy(dtype="int"),
+        dm_assocatiations["sw_hw"].to_numpy(dtype="int"),
+    )
+    # append the null matrix
+    matrix_list = [[[p, p, 1.0] for p in model.pool_names]] + list(
+        matrix_data_by_dmid.values()
+    )
+    return (matrix_list, _dm_op_index)
 
-    return (matrix_data, _dm_op_index)
+@numba.njit()
+def _build_dm_op_index(
+    _dm_op_index: Dict,
+    dmid_idx: Dict,
+    disturbance_matrix_id: np.ndarray,
+    disturbance_type_id: np.ndarray,
+    spatial_unit_ids: np.ndarray,
+    sw_hw: np.ndarray,
+):
+
+    for i in range(disturbance_type_id.shape[0]):
+        dist_type = disturbance_type_id[i]
+        spuid = spatial_unit_ids[i]
+        _sw_hw = sw_hw[i]
+        key = (dist_type, spuid, _sw_hw)
+        dm_idx = dmid_idx[disturbance_matrix_id[i]]
+        _dm_op_index[key] = dm_idx
+        # if dist_type not in _dm_op_index:
+        #    _dm_op_index[dist_type] = Dict.empty(
+        #        key_type=types.int64,
+        #        value_type=numba.types.DictType(types.int64, types.int64),
+        #    )
+        #    _dm_op_index[dist_type][spuid] = Dict.empty(
+        #        key_type=types.int64, value_type=types.int64
+        #    )
+        #    _dm_op_index[dist_type][spuid][_sw_hw] = dm_idx
+        # elif spuid not in _dm_op_index[dist_type]:
+        #    _dm_op_index[dist_type][spuid] = Dict.empty(
+        #        key_type=types.int64, value_type=types.int64
+        #    )
+        #    _dm_op_index[dist_type][spuid][_sw_hw] = dm_idx
+        # elif _sw_hw not in _dm_op_index[dist_type][spuid]:
+        #    _dm_op_index[dist_type][spuid][_sw_hw] = dm_idx
+    return _dm_op_index
 
 
 def _net_growth(
