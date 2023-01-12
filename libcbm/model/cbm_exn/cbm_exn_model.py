240b689e3a7d5c38bec9a485bd8802433b592ca0
Scott <scottm361@gmail.com>
2023-01-12 14:17:33 -0800

    code docs

--- libcbm/model/cbm_exn/cbm_exn_model.py
+++ libcbm/model/cbm_exn/cbm_exn_model.py
@@ -20,20 +20,39 @@ cbm_vars_type = Union[CBMVariables, Dict[str, pd.DataFrame]]
 
 
 class SpinupReporter:
+    """Tracks step-by-step results during spinup for debugging purposes.
+    """
     def __init__(self, pandas_interface: bool):
+        """initialize a SpinupReporter
+
+        Args:
+            pandas_interface (bool): if true CBMVariables members are assumed
+                to be pandas dataframes, and otherwise the internal DataFrame
+                interfaces
+        """
         self._output_processor = ModelOutputProcessor()
         self._pandas_interface = pandas_interface
 
     def append_spinup_output(
         self, timestep: int, spinup_vars: CBMVariables
     ) -> None:
+        """Append a set of results to the spinup reporter
+
+        Args:
+            timestep (int): the step number
+            spinup_vars (CBMVariables): the spinup variables and state
+        """
         self._output_processor.append_results(timestep, spinup_vars)
 
     def get_output(self) -> cbm_vars_type:
+        """Gets the accumulated spinup results"""
         return self._output_processor.get_results()
 
 
 class CBMEXNModel:
+    """The CBMEXNModel: CBM-CFS3 with external net Aboveground Carbon biomass
+    increments
+    """
     def __init__(
         self,
         cbm_model: CBMModel,
@@ -41,6 +60,18 @@ class CBMEXNModel:
         pandas_interface=True,
         spinup_reporter: SpinupReporter = None,
     ):
+        """initialize the CBMEXNModel
+
+        Args:
+            cbm_model (CBMModel): abstraction of CBM model
+            parameters (CBMEXNParameters): cbm_constant parameter
+            pandas_interface (bool, optional): If set to true then all members
+                of CBMVariables are assumed to be of type pd.DataFrame, if
+                false then the internally defined libcbm DataFrame is used.
+                Defaults to True.
+            spinup_reporter (SpinupReporter, optional): If specified, spinup
+                results are tracked for debugging purposes. Defaults to None.
+        """
         self._cbm_model = cbm_model
         self._pandas_interface = pandas_interface
         self._spinup_reporter = spinup_reporter
@@ -49,6 +80,12 @@ class CBMEXNModel:
 
     @property
     def pool_names(self) -> list[str]:
+        """Get the list of pool names, also used as the column headers for
+        the cbm_vars pools dataframe
+
+        Returns:
+            list[str]: the pool names
+        """
         return self._cbm_model.pool_names
 
     @property
@@ -63,13 +100,31 @@ class CBMEXNModel:
 
     @property
     def matrix_ops(self) -> MatrixOps:
+        """MatrixOps instance which computes and caches C matrix flows
+
+        Returns:
+            MatrixOps: instance of MatrixOps
+        """
         return self._matrix_ops
 
     @property
     def parameters(self) -> CBMEXNParameters:
+        """CBM constant parameters
+
+        Returns:
+            CBMEXNParameters: CBM constant parameters
+        """
         return self._parameters
 
     def step(self, cbm_vars: cbm_vars_type) -> cbm_vars_type:
+        """Perform one timestep of the CBMEXNModel
+
+        Args:
+            cbm_vars (cbm_vars_type): _description_
+
+        Returns:
+            cbm_vars_type: _description_
+        """
         if self._pandas_interface:
             return cbm_exn_step.step(
                 self, CBMVariables.from_pandas(cbm_vars)
@@ -127,6 +182,16 @@ class CBMEXNModel:
 def initialize(
     config_path: str, pandas_interface=True, include_spinup_debug=False
 ) -> Iterator[CBMEXNModel]:
+    """_summary_
+
+    Args:
+        config_path (str): _description_
+        pandas_interface (bool, optional): _description_. Defaults to True.
+        include_spinup_debug (bool, optional): _description_. Defaults to False.
+
+    Yields:
+        Iterator[CBMEXNModel]: _description_
+    """
 
     if not config_path:
         config_path = resources.get_cbm_exn_parameters_dir()
