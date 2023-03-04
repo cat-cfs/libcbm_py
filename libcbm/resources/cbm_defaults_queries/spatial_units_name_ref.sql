4dc17598c0b6a7db33337a01bba086181aa5d28d
Scott <scottm361@gmail.com>
2023-03-03 16:10:30 -0800

    include identifiers in query

--- libcbm/resources/cbm_defaults_queries/spatial_units_name_ref.sql
+++ libcbm/resources/cbm_defaults_queries/spatial_units_name_ref.sql
@@ -1,5 +1,5 @@
-select spatial_unit.id as spatial_unit_id, admin_boundary_tr.name as admin_boundary_name,
-eco_boundary_tr.name as eco_boundary_name from spatial_unit
+select spatial_unit.id as spatial_unit_id, admin_boundary.id as admin_boundary_id, admin_boundary_tr.name as admin_boundary_name,
+eco_boundary.id as eco_boundary_id, eco_boundary_tr.name as eco_boundary_name from spatial_unit
 inner join eco_boundary on eco_boundary.id = spatial_unit.eco_boundary_id
 inner join admin_boundary on admin_boundary.id = spatial_unit.admin_boundary_id
 inner join eco_boundary_tr on eco_boundary_tr.eco_boundary_id = eco_boundary.id
