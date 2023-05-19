a85e473f7c04508a550f0a9aec38cdf5a52e57cd
Scott <scottm361@gmail.com>
2023-05-19 10:08:56 -0700

    move transtion rlue filtering out of transition_rule_processor, now accepts a list of filter objects

--- libcbm/model/cbm/rule_based/transition_rule_processor.py
+++ libcbm/model/cbm/rule_based/transition_rule_processor.py
@@ -3,7 +3,6 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from __future__ import annotations
-from typing import Callable
 from typing import Iterable
 from typing import Tuple
 import numpy as np
@@ -14,24 +13,17 @@ from libcbm.storage.series import Series
 from libcbm.model.cbm.cbm_variables import CBMVariables
 from libcbm.model.cbm.rule_based import rule_filter
 from libcbm.model.cbm.rule_based.rule_filter import RuleFilter
-from libcbm.model.cbm.rule_based.classifier_filter import ClassifierFilter
 
 
 class TransitionRuleProcessor(object):
     def __init__(
         self,
-        classifier_filter_builder: ClassifierFilter,
-        state_variable_filter_func: Callable[[dict, DataFrame], RuleFilter],
         classifiers_config: dict[str, list],
-        grouped_percent_err_max: float,
         wildcard: str,
         transition_classifier_postfix: str,
     ):
-        self.wildcard = wildcard
-        self.transition_classifier_postfix = transition_classifier_postfix
-        self.state_variable_filter_func = state_variable_filter_func
-        self.classifier_filter_builder = classifier_filter_builder
-        self.grouped_percent_err_max = grouped_percent_err_max
+        self._wildcard = wildcard
+        self._transition_classifier_postfix = transition_classifier_postfix
         self.classifiers_config = classifiers_config
         self.classifier_names = [
             x["name"] for x in self.classifiers_config["classifiers"]
@@ -48,35 +40,14 @@ class TransitionRuleProcessor(object):
             if x["classifier_id"] == classifier_id
         }
 
-    def _filter_stands(
-        self, tr_group_key: dict, cbm_vars: CBMVariables
-    ) -> Series:
-        dist_type_target = tr_group_key["disturbance_type_id"]
-        classifier_set = [
-            tr_group_key[x] for x in cbm_vars.classifiers.columns
-        ]
-        tr_filters = [
-            self.state_variable_filter_func(tr_group_key, cbm_vars.state),
-            self.classifier_filter_builder.create_classifiers_filter(
-                classifier_set, cbm_vars.classifiers
-            ),
-            rule_filter.create_filter(
-                expression=f"(disturbance_type == {dist_type_target})",
-                data=cbm_vars.parameters,
-            ),
-        ]
-
-        filter_result = rule_filter.evaluate_filters(*tr_filters)
-        return filter_result
-
     def _get_transition_classifier_set(
         self, transition_rule: dict
     ) -> Iterable[Tuple[str, int]]:
         for classifier_name in self.classifier_names:
             transition_classifier_value = transition_rule[
-                classifier_name + self.transition_classifier_postfix
+                classifier_name + self._transition_classifier_postfix
             ]
-            if transition_classifier_value == self.wildcard:
+            if transition_classifier_value == self._wildcard:
                 continue
             transition_id = self.classifier_value_lookup[classifier_name][
                 transition_classifier_value
@@ -86,6 +57,7 @@ class TransitionRuleProcessor(object):
     def apply_transition_rule(
         self,
         tr_group: DataFrame,
+        rule_filters: list[RuleFilter],
         proportions: list[float],
         transition_mask: Series,
         cbm_vars: CBMVariables,
@@ -98,8 +70,6 @@ class TransitionRuleProcessor(object):
         the member transition rules.
 
         Args:
-            tr_group_key (dict): the common key for the grouped transition
-                rules.
             tr_group (DataFrame): the grouped transition rules, where
                 each row is a member.
             transition_mask (Series): a boolean mask indicating when
@@ -121,7 +91,7 @@ class TransitionRuleProcessor(object):
                     state
 
         """
-        filtered = self._filter_stands(tr_group_key, cbm_vars)
+        filtered = rule_filter.evaluate_filters(*rule_filters)
 
         # sets the transitioned array with the transition filter result
         eligible = dataframe.logical_and(
