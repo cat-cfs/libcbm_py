cc7b44fa4345c9523abc230e15d873e9e48415a4
Scott <scottm361@gmail.com>
2023-05-19 14:18:07 -0700

    accept disturbance event id for tracking of areas by that id

--- libcbm/model/cbm/rule_based/event_processor.py
+++ libcbm/model/cbm/rule_based/event_processor.py
@@ -47,6 +47,7 @@ def process_event(
     target_func: Callable[[CBMVariables, Series], RuleTargetResult],
     disturbance_type_id: int,
     cbm_vars: CBMVariables,
+    disturbance_event_id: int = None,
 ) -> ProcessEventResult:
     """Computes a CBM rule based event by filtering and targeting a subset of
     the specified inventory.  In the case of merchantable or area targets
@@ -63,6 +64,8 @@ def process_event(
             processed.
         cbm_vars (CBMVariables): an object containing dataframes that store cbm
             simulation state and variables
+        disturbance_event_id (int, optional): an identifier for the disturbance
+            event being processed.
 
     Returns:
         ProcessEventResult: instance of class containing results for the
@@ -79,14 +82,20 @@ def process_event(
 
     if rule_target_result.target is not None:
         cbm_vars = apply_rule_based_event(
-            rule_target_result.target, disturbance_type_id, cbm_vars
+            rule_target_result.target,
+            disturbance_type_id,
+            cbm_vars,
+            disturbance_event_id,
         )
 
     return ProcessEventResult(cbm_vars, filter_result, rule_target_result)
 
 
 def apply_rule_based_event(
-    target: DataFrame, disturbance_type_id: int, cbm_vars: CBMVariables
+    target: DataFrame,
+    disturbance_type_id: int,
+    cbm_vars: CBMVariables,
+    disturbance_event_id: int = None,
 ) -> CBMVariables:
     """Apply the specified target to the CBM simulation variables,
     splitting them if necessary.
@@ -98,6 +107,8 @@ def apply_rule_based_event(
             applied.
         cbm_vars (CBMVariables): an object containing dataframes that store cbm
             simulation state and variables
+        disturbance_event_id (int, optional): an identifier for the disturbance
+            event being processed.
 
     Returns:
         CBMVariables: updated and expanded cbm_vars
@@ -110,11 +121,6 @@ def apply_rule_based_event(
     splits = target_area_proportions < 1.0
     split_index = target_index.filter(splits)
     split_inventory = cbm_vars.inventory.take(split_index)
-    # set the disturbance types for the disturbed indices, based on
-    # the sit_event disturbance_type field.
-    cbm_vars.parameters["disturbance_type"].assign(
-        np.int32(disturbance_type_id), target_index
-    )
 
     if split_inventory.n_rows > 0:
         # reduce the area of the disturbed inventory by the disturbance area
@@ -172,13 +178,17 @@ def apply_rule_based_event(
             [cbm_vars.flux, cbm_vars.flux.take(split_index)]
         )
 
-        new_params = cbm_vars.parameters.take(split_index)
-        new_params["disturbance_type"].assign(np.int32(0))
         parameters = dataframe.concat_data_frame(
-            [cbm_vars.parameters, new_params]
+            [cbm_vars.parameters, cbm_vars.parameters.take(split_index)]
         )
 
         cbm_vars = CBMVariables(
             pools, flux, classifiers, state, inventory, parameters
         )
+
+    # set the disturbance types for the disturbed indices, based on
+    # the sit_event disturbance_type field.
+    cbm_vars.parameters["disturbance_type"].assign(
+        np.int32(disturbance_type_id), target_index
+    )
     return cbm_vars
