7f81f6657b1b041d5935892dde8ec58a8413e281
Scott Morken <scottm361@gmail.com>
2023-10-10 12:26:08 -0700

    refactor

--- libcbm/model/model_definition/model.py
+++ libcbm/model/model_definition/model.py
@@ -5,6 +5,7 @@ import numpy as np
 from libcbm.model.model_definition.model_variables import ModelVariables
 from libcbm.model.model_definition import model_handle
 from libcbm.model.model_definition.model_handle import ModelHandle
+from libcbm.model.model_definition.model_matrix_ops import ModelMatrixOps
 from libcbm.wrapper.libcbm_operation import Operation
 
 
@@ -18,6 +19,7 @@ class CBMModel:
         model_handle: ModelHandle,
         pool_config: list[str],
         flux_config: list[dict],
+        op_processes: dict[str, int],
     ):
         invalid_pool_names = []
         for p in pool_config:
@@ -30,6 +32,9 @@ class CBMModel:
         self._model_handle = model_handle
         self._pool_config = pool_config
         self._flux_config = flux_config
+        self._model_matrix_ops = ModelMatrixOps(
+            self._model_handle, pool_config, op_processes
+        )
 
     @property
     def pool_names(self) -> list[str]:
@@ -39,33 +44,9 @@ class CBMModel:
     def flux_names(self) -> list[str]:
         return [f["name"] for f in self._flux_config]
 
-    def create_operation(
-        self,
-        matrices: list,
-        fmt: str,
-        process_id: int,
-        matrix_index: np.ndarray,
-        init_value: int = 1,
-    ) -> Operation:
-        """Create a set of matrix operations for C dynamics along the row axis
-        of cbm_vars. The relationship of matrices to stands is 1:m
-
-        Args:
-            matrices (list): a list of matrix information
-            fmt (str): one of "repeating_coordinates" or "matrix_list"
-            process_id (int): the process_id for flux indicator categorization
-            matrix_index (np.ndarray): a 1d numpy array of the same length as
-                the dataframes in cbm_vars where each value is the index to a
-                matrix in the specified matrices list
-            init_value (int): the value to set on the diagonal of the matrix
-                initially, defaults to 1
-
-        Returns:
-            Operation: an `Operation` object
-        """
-        return self._model_handle.create_operation(
-            matrices, fmt, process_id, matrix_index, init_value
-        )
+    @property
+    def matrix_ops(self) -> ModelMatrixOps:
+        return self._model_matrix_ops
 
     def compute(
         self,
