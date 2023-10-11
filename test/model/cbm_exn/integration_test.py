c242e232d233ce673a7bef81981eb8ec21a343f6
Scott Morken <scottm361@gmail.com>
2023-10-11 10:48:31 -0700

    drop pandas interface

--- test/model/cbm_exn/integration_test.py
+++ test/model/cbm_exn/integration_test.py
@@ -41,7 +41,6 @@ def test_cbm_exn_integration():
         )
         with cbm_exn_model.initialize(
             config_path=tempdir,
-            pandas_interface=False,
             include_spinup_debug=True,
         ) as model:
             cbm_vars = model.spinup(spinup_input)
