f211ae06d46aeed474587182f0e37a0cb40d0e3b
Scott Morken <scottm361@gmail.com>
2023-10-12 09:56:10 -0700

    default ops for stepping

--- libcbm/model/cbm_exn/cbm_exn_step.py
+++ libcbm/model/cbm_exn/cbm_exn_step.py
@@ -3,6 +3,7 @@ from typing import Union
 
 if TYPE_CHECKING:
     from libcbm.model.cbm_exn.cbm_exn_model import CBMEXNModel
+from libcbm.model.cbm_exn.cbm_exn_parameters import CBMEXNParameters
 from libcbm.model.model_definition.model_variables import ModelVariables
 from libcbm.model.cbm_exn import cbm_exn_land_state
 from libcbm.model.cbm_exn import cbm_exn_annual_process_dynamics
@@ -10,19 +11,114 @@ from libcbm.model.cbm_exn import cbm_exn_disturbance_dynamics
 from libcbm.model.cbm_exn import cbm_exn_growth_functions
 
 
-def get_default_disturbance_ops(model: "CBMEXNModel") -> list[dict]:
+def get_default_ops(
+    parameters: CBMEXNParameters, cbm_vars: ModelVariables, which: str = "all"
+) -> list[dict]:
+    if which not in ["disturbance", "annual_process", "all"]:
+        raise ValueError(f"uknown parameter value for which '{which}'")
+
+    output = []
+
+    if which in ["disturbance", "all"]:
+        output.append(
+            {
+                "name": "disturbance",
+                "op_process_name": "Disturbance",
+                "op_data": cbm_exn_disturbance_dynamics.disturbance(
+                    parameters.pool_configuration(),
+                    parameters.get_disturbance_matrices(),
+                    parameters.get_disturbance_matrix_associations(),
+                    False,
+                ),
+                "requires_reindexing": True,
+            }
+        )
+
+    if which in ["annual_process", "all"]:
+        growth_info = cbm_exn_growth_functions.prepare_growth_info(
+            cbm_vars,
+            parameters.get_turnover_parameters(),
+            parameters.get_root_parameters(),
+        )
+        annual_process_ops = [
+            {
+                "name": "snag_turnover",
+                "op_process_name": "Growth and Turnover",
+                "op_data": cbm_exn_annual_process_dynamics.snag_turnover(
+                    parameters.get_turnover_parameters(), False
+                ),
+                "requires_reindexing": False,
+            },
+            {
+                "name": "biomass_turnover",
+                "op_process_name": "Growth and Turnover",
+                "op_data": cbm_exn_annual_process_dynamics.biomass_turnover(
+                    parameters.get_turnover_parameters(), False
+                ),
+                "requires_reindexing": False,
+            },
+            {
+                "name": "dom_decay",
+                "op_process_name": "Decay",
+                "op_data": cbm_exn_annual_process_dynamics.dom_decay(
+                    cbm_vars["parameters"][
+                        "mean_annual_temperature"
+                    ].to_numpy(),
+                    parameters.get_decay_parameters(),
+                ),
+                "requires_reindexing": True,
+            },
+            {
+                "name": "slow_decay",
+                "op_process_name": "Decay",
+                "op_data": cbm_exn_annual_process_dynamics.slow_decay(
+                    cbm_vars["parameters"][
+                        "mean_annual_temperature"
+                    ].to_numpy(),
+                    parameters.get_decay_parameters(),
+                ),
+                "requires_reindexing": True,
+            },
+            {
+                "name": "slow_mixing",
+                "op_process_name": "Decay",
+                "op_data": cbm_exn_annual_process_dynamics.slow_mixing(
+                    parameters.get_slow_mixing_rate(),
+                ),
+                "requires_reindexing": False,
+            },
+            {
+                "name": "growth",
+                "op_process_name": "Growth and Turnover",
+                "op_data": cbm_exn_annual_process_dynamics.net_growth(
+                    growth_info,
+                ),
+                "requires_reindexing": True,
+            },
+            {
+                "name": "overmature_decline",
+                "op_process_name": "Growth and Turnover",
+                "op_data": cbm_exn_annual_process_dynamics.overmature_decline(
+                    growth_info,
+                ),
+                "requires_reindexing": True,
+            },
+        ]
+        output.extend(annual_process_ops)
+
+    return output
+
+
+def get_default_annual_process_op_sequence() -> list[str]:
     return [
-        {
-            "name": "disturbance",
-            "op_process_name": "Disturbance",
-            "op_data": cbm_exn_disturbance_dynamics.disturbance(
-                model.pool_names,
-                model.parameters.get_disturbance_matrices(),
-                model.parameters.get_disturbance_matrix_associations(),
-                False,
-            ),
-            "requires_reindexing": True,
-        }
+        "growth",
+        "snag_turnover",
+        "biomass_turnover",
+        "overmature_decline",
+        "growth",
+        "dom_decay",
+        "slow_decay",
+        "slow_mixing",
     ]
 
 
@@ -52,98 +148,16 @@ def step_disturbance(
     Returns:
         ModelVariables: updated cbm_variables and state
     """
-    if ops is None:
-        ops = get_default_disturbance_ops(model)
-    for op_def in ops:
-        model.matrix_ops.create_operation(**op_def)
     if op_sequence is None:
         op_sequence = get_default_disturbance_op_sequence()
+    if ops is None:
+        ops = get_default_ops(model.parameters, cbm_vars, "disturbance")
+    for op_def in ops:
+        if op_def["name"] in op_sequence:
+            model.matrix_ops.create_operation(**op_def)
+
     model.compute(cbm_vars, op_sequence)
     return cbm_vars
-
-
-def get_default_annual_process_ops(
-    model: "CBMEXNModel", cbm_vars: ModelVariables
-) -> list[dict]:
-    growth_info = cbm_exn_growth_functions.prepare_growth_info(
-        cbm_vars,
-        model.parameters.get_turnover_parameters(),
-        model.parameters.get_root_parameters(),
-    )
-
-    return [
-        {
-            "name": "snag_turnover",
-            "op_process_name": "Growth and Turnover",
-            "op_data": cbm_exn_annual_process_dynamics.snag_turnover(
-                model.parameters.get_turnover_parameters(), False
-            ),
-            "requires_reindexing": False,
-        },
-        {
-            "name": "biomass_turnover",
-            "op_process_name": "Growth and Turnover",
-            "op_data": cbm_exn_annual_process_dynamics.biomass_turnover(
-                model.parameters.get_turnover_parameters(), False
-            ),
-            "requires_reindexing": False,
-        },
-        {
-            "name": "dom_decay",
-            "op_process_name": "Decay",
-            "op_data": cbm_exn_annual_process_dynamics.dom_decay(
-                cbm_vars["parameters"]["mean_annual_temperature"].to_numpy(),
-                model.parameters.get_decay_parameters(),
-            ),
-            "requires_reindexing": True,
-        },
-        {
-            "name": "slow_decay",
-            "op_process_name": "Decay",
-            "op_data": cbm_exn_annual_process_dynamics.slow_decay(
-                cbm_vars["parameters"]["mean_annual_temperature"].to_numpy(),
-                model.parameters.get_decay_parameters(),
-            ),
-            "requires_reindexing": True,
-        },
-        {
-            "name": "slow_mixing",
-            "op_process_name": "Decay",
-            "op_data": cbm_exn_annual_process_dynamics.slow_mixing(
-                model.parameters.get_slow_mixing_rate(),
-            ),
-            "requires_reindexing": False,
-        },
-        {
-            "name": "growth",
-            "op_process_name": "Growth and Turnover",
-            "op_data": cbm_exn_annual_process_dynamics.net_growth(
-                growth_info,
-            ),
-            "requires_reindexing": True,
-        },
-        {
-            "name": "overmature_decline",
-            "op_process_name": "Growth and Turnover",
-            "op_data": cbm_exn_annual_process_dynamics.net_growth(
-                growth_info,
-            ),
-            "requires_reindexing": True,
-        },
-    ]
-
-
-def get_default_annual_process_op_sequence() -> list[str]:
-    return [
-        "growth",
-        "snag_turnover",
-        "biomass_turnover",
-        "overmature_decline",
-        "growth",
-        "dom_decay",
-        "slow_decay",
-        "slow_mixing",
-    ]
 
 
 def step_annual_process(
@@ -161,12 +175,14 @@ def step_annual_process(
     Returns:
         CBMVariables: updated cbm_vars
     """
-    if ops is None:
-        ops = get_default_annual_process_ops(model, cbm_vars)
-    for op_def in ops:
-        model.matrix_ops.create_operation(**op_def)
     if op_sequence is None:
         op_sequence = get_default_annual_process_op_sequence()
+    if ops is None:
+        ops = get_default_ops(model.parameters, cbm_vars, "annual_process")
+    for op_def in ops:
+        if op_def["name"] in op_sequence:
+            model.matrix_ops.create_operation(**op_def)
+
     model.compute(cbm_vars, op_sequence)
     return cbm_vars
 
@@ -174,10 +190,9 @@ def step_annual_process(
 def step(
     model: "CBMEXNModel",
     cbm_vars: ModelVariables,
-    disturbance_ops: Union[list[dict], None] = None,
-    disturbance_op_sequence: Union[list[str], None] = None,
-    step_ops: Union[list[dict], None] = None,
+    ops: Union[list[dict], None] = None,
     step_op_sequence: Union[list[str], None] = None,
+    disturbance_op_sequence: Union[list[str], None] = None,
 ) -> ModelVariables:
     """Advance CBM state by one timestep, and track results.
 
@@ -193,12 +208,14 @@ def step(
     Returns:
         ModelVariables: updated cbm_vars
     """
+    if ops is None:
+        ops = get_default_ops(model.parameters, cbm_vars, "all")
+    for op_def in ops:
+        model.matrix_ops.create_operation(**op_def)
 
     cbm_vars["flux"].zero()
     cbm_vars = cbm_exn_land_state.start_step(cbm_vars, model.parameters)
-    cbm_vars = step_disturbance(
-        model, cbm_vars, disturbance_ops, disturbance_op_sequence
-    )
-    cbm_vars = step_annual_process(model, cbm_vars, step_ops, step_op_sequence)
+    cbm_vars = step_disturbance(model, cbm_vars, ops, disturbance_op_sequence)
+    cbm_vars = step_annual_process(model, cbm_vars, ops, step_op_sequence)
     cbm_vars = cbm_exn_land_state.end_step(cbm_vars, model.parameters)
     return cbm_vars
