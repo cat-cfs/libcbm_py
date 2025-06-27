11a941724de0f59d7b6a648836286270aa12978c
Ryder <ryder.bergerud@gmail.com>
2025-06-26 17:29:44 -0700

    Support OSX <= 16
    Running test script run_1_1_simulate.ipynb shows the existing osx build works on Sonoma OSX16, so taking away the check that prevents OS versions <= 12 from using libcbm.

--- libcbm/resources/__init__.py
+++ libcbm/resources/__init__.py
@@ -123,8 +123,7 @@ def get_libcbm_bin_path():
         minor = version_tokens[1]
         matched_ver = (
             (int(major) == 10 and int(minor) >= 12)
-            or (int(major) == 11)
-            or (int(major) == 12)
+            or (int(major) >= 11 int(major) <= 16)
         )
         # Get the full path to the dylib #
         dylib = os.path.join(
